8576b634ca3969ca4d093d00d8a65a90d2c61651
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2011-01-24 21:18:50 +0000

    Adding a -version option to Opus to display the version on the command line.

---
+++ Opus/Actions/ShowVersionAction.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="ShowVersionAction.cs" company="Mark Final">
+//  Opus
+// </copyright>
+// <summary>Opus main application.</summary>
+// <author>Mark Final</author>
+
+[assembly: Opus.Core.RegisterAction(typeof(Opus.ShowVersionAction))]
+
+namespace Opus
+{
+    [Core.TriggerAction]
+    internal class ShowVersionAction : Core.IAction
+    {
+        public string CommandLineSwitch
+        {
+            get
+            {
+                return "-version";
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return "Display the version of Opus";
+            }
+        }
+
+        public bool Execute()
+        {
+            Core.Log.MessageAll(Core.State.OpusVersionString);
+
+            return true;
+        }
+    }
+}
