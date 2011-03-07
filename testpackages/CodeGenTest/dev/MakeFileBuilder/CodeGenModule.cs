33b7822a75decd761f7824546e4dd6a9e92c035b
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2011-03-07 21:15:36 +0000

    [makefiles] Test package updates for recent makefile changes.

---
+++ testpackages/CodeGenTest/dev/MakeFileBuilder/CodeGenModule.cs
@@ -0,0 +1,53 @@
+namespace MakeFileBuilder
+{
+    public partial class MakeFileBuilder
+    {
+        public object Build(CodeGenTest.CodeGenModule codeGenModule, Opus.Core.DependencyNode node, out System.Boolean success)
+        {
+            Opus.Core.Target target = node.Target;
+            CodeGenTest.CodeGenTool tool = new CodeGenTest.CodeGenTool();
+            CodeGenTest.CodeGenOptions toolOptions = codeGenModule.Options as CodeGenTest.CodeGenOptions;
+            string toolExePath = tool.Executable(target);
+
+            Opus.Core.StringArray inputFiles = new Opus.Core.StringArray();
+            inputFiles.Add(tool.Executable(target));
+            Opus.Core.StringArray outputFiles = codeGenModule.Options.OutputPaths.Paths;
+
+            System.Text.StringBuilder commandLineBuilder = new System.Text.StringBuilder();
+            Opus.Core.DirectoryCollection directoriesToCreate = null;
+            if (toolOptions is CommandLineProcessor.ICommandLineSupport)
+            {
+                CommandLineProcessor.ICommandLineSupport commandLineOption = toolOptions as CommandLineProcessor.ICommandLineSupport;
+                commandLineOption.ToCommandLineArguments(commandLineBuilder, target);
+
+                directoriesToCreate = commandLineOption.DirectoriesToCreate();
+            }
+            else
+            {
+                throw new Opus.Core.Exception("Moc options does not support command line translation");
+            }
+
+            Opus.Core.StringArray recipes = new Opus.Core.StringArray();
+            recipes.Add("\"" + toolExePath + "\" " + commandLineBuilder.ToString());
+
+            string makeFilePath = MakeFileBuilder.GetMakeFilePathName(node);
+            System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(makeFilePath));
+            Opus.Core.Log.DebugMessage("Makefile : '{0}'", makeFilePath);
+
+            MakeFile makeFile = new MakeFile(node, this.topLevelMakeFilePath);
+
+            MakeFileRule rule = new MakeFileRule(codeGenModule.Options.OutputPaths, CodeGenTest.OutputFileFlags.GeneratedSourceFile, node.UniqueModuleName, directoriesToCreate, null, inputFiles, recipes);
+            makeFile.RuleArray.Add(rule);
+
+            using (System.IO.TextWriter makeFileWriter = new System.IO.StreamWriter(makeFilePath))
+            {
+                makeFile.Write(makeFileWriter);
+            }
+
+            success = true;
+
+            MakeFileData returnData = new MakeFileData(makeFilePath, makeFile.ExportedTargets, makeFile.ExportedVariables, tool.EnvironmentPaths(target));
+            return returnData;
+        }
+    }
+}
---
+++ testpackages/CodeGenTest2/dev/MakeFileBuilder/CodeGenModule.cs
@@ -0,0 +1,53 @@
+namespace MakeFileBuilder
+{
+    public partial class MakeFileBuilder
+    {
+        public object Build(CodeGenTest2.CodeGenModule codeGenModule, Opus.Core.DependencyNode node, out System.Boolean success)
+        {
+            Opus.Core.Target target = node.Target;
+            CodeGenTest2.CodeGenTool tool = new CodeGenTest2.CodeGenTool();
+            CodeGenTest2.CodeGenOptions toolOptions = codeGenModule.Options as CodeGenTest2.CodeGenOptions;
+            string toolExePath = tool.Executable(target);
+
+            Opus.Core.StringArray inputFiles = new Opus.Core.StringArray();
+            inputFiles.Add(tool.Executable(target));
+            Opus.Core.StringArray outputFiles = codeGenModule.Options.OutputPaths.Paths;
+
+            System.Text.StringBuilder commandLineBuilder = new System.Text.StringBuilder();
+            Opus.Core.DirectoryCollection directoriesToCreate = null;
+            if (toolOptions is CommandLineProcessor.ICommandLineSupport)
+            {
+                CommandLineProcessor.ICommandLineSupport commandLineOption = toolOptions as CommandLineProcessor.ICommandLineSupport;
+                commandLineOption.ToCommandLineArguments(commandLineBuilder, target);
+
+                directoriesToCreate = commandLineOption.DirectoriesToCreate();
+            }
+            else
+            {
+                throw new Opus.Core.Exception("Moc options does not support command line translation");
+            }
+
+            Opus.Core.StringArray recipes = new Opus.Core.StringArray();
+            recipes.Add("\"" + toolExePath + "\" " + commandLineBuilder.ToString());
+
+            string makeFilePath = MakeFileBuilder.GetMakeFilePathName(node);
+            System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(makeFilePath));
+            Opus.Core.Log.DebugMessage("Makefile : '{0}'", makeFilePath);
+
+            MakeFile makeFile = new MakeFile(node, this.topLevelMakeFilePath);
+
+            MakeFileRule rule = new MakeFileRule(codeGenModule.Options.OutputPaths, CodeGenTest2.OutputFileFlags.GeneratedSourceFile, node.UniqueModuleName, directoriesToCreate, null, inputFiles, recipes);
+            makeFile.RuleArray.Add(rule);
+
+            using (System.IO.TextWriter makeFileWriter = new System.IO.StreamWriter(makeFilePath))
+            {
+                makeFile.Write(makeFileWriter);
+            }
+
+            success = true;
+
+            MakeFileData returnData = new MakeFileData(makeFilePath, makeFile.ExportedTargets, makeFile.ExportedVariables, tool.EnvironmentPaths(target));
+            return returnData;
+        }
+    }
+}
