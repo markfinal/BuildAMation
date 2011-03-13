c0270028388a266e6a8f3096e6b41a991100a006
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2011-03-13 09:41:31 +0000

    [trunk] Set Opus version from 0.1 to 0.10.

--- Opus.Core/Properties/AssemblyInfo.cs
+++ Opus.Core/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.10.0.0")]
 
 // Because it exposes externally visible types.
 [assembly: CLSCompliant(true)]
