d69b3b37ef6b6205971c9b710c1a8dbffdb0b6a8
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2011-04-24 15:30:24 +0000

    [qtcreator] Headers are now included in .pro files

--- packages/C/dev/QMakeBuilder/CDynamicLibrary.cs
+++ packages/C/dev/QMakeBuilder/CDynamicLibrary.cs
@@ -72,6 +72,32 @@ namespace QMakeBuilder
                     proFileWriter.WriteLine(sourcesStatement.ToString());
                 }
 
+                // headers
+                {
+                    System.Reflection.BindingFlags fieldBindingFlags = System.Reflection.BindingFlags.Instance |
+                                                                       System.Reflection.BindingFlags.Public |
+                                                                       System.Reflection.BindingFlags.NonPublic;
+                    System.Reflection.FieldInfo[] fields = dynamicLibrary.GetType().GetFields(fieldBindingFlags);
+                    foreach (System.Reflection.FieldInfo field in fields)
+                    {
+                        var headerFileAttributes = field.GetCustomAttributes(typeof(C.HeaderFilesAttribute), false);
+                        if (headerFileAttributes.Length > 0)
+                        {
+                            Opus.Core.FileCollection headerFileCollection = field.GetValue(dynamicLibrary) as Opus.Core.FileCollection;
+                            if (headerFileCollection.Count > 0)
+                            {
+                                System.Text.StringBuilder headersStatement = new System.Text.StringBuilder();
+                                headersStatement.AppendFormat("{0}:HEADERS += ", nodeData.Configuration);
+                                foreach (string headerPath in headerFileCollection)
+                                {
+                                    headersStatement.AppendFormat("\\\n\t{0}", headerPath.Replace('\\', '/'));
+                                }
+                                proFileWriter.WriteLine(headersStatement.ToString());
+                            }
+                        }
+                    }
+                }
+
                 // cflags, include paths and defines
                 {
                     Opus.Core.StringArray cflags = nodeData["CFLAGS"];
