f7ba8b82a30ca8b8592dfbd28d3d6dca38b0ee49
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2011-04-24 15:38:55 +0000

    [qtcreator] HeaderLibraries now output a subdirs pro file with the headers listed.

--- packages/C/dev/QMakeBuilder/HeaderLibrary.cs
+++ packages/C/dev/QMakeBuilder/HeaderLibrary.cs
@@ -7,12 +7,55 @@ namespace QMakeBuilder
 {
     public sealed partial class QMakeBuilder
     {
-        [Opus.Core.EmptyBuildFunction]
         public object Build(C.HeaderLibrary headerLibrary, Opus.Core.DependencyNode node, out bool success)
         {
-            // TODO: add a subdirs pro file with just HEADERS?
+            NodeData nodeData = new NodeData();
+            nodeData.Configuration = GetQtConfiguration(node.Target);
+
+            string proFilePath = QMakeBuilder.GetProFilePath(node);
+            System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(proFilePath));
+            nodeData.ProFilePathName = proFilePath;
+
+            using (System.IO.TextWriter proFileWriter = new System.IO.StreamWriter(proFilePath))
+            {
+                proFileWriter.WriteLine("# --- Written by Opus");
+
+                {
+                    string relativePriPathName = Opus.Core.RelativePathUtilities.GetPath(this.DisableQtPriPathName, proFilePath);
+                    proFileWriter.WriteLine("include({0})", relativePriPathName.Replace('\\', '/'));
+                }
+
+                proFileWriter.WriteLine("TEMPLATE = subdirs");
+
+                // headers
+                {
+                    System.Reflection.BindingFlags fieldBindingFlags = System.Reflection.BindingFlags.Instance |
+                                                                       System.Reflection.BindingFlags.Public |
+                                                                       System.Reflection.BindingFlags.NonPublic;
+                    System.Reflection.FieldInfo[] fields = headerLibrary.GetType().GetFields(fieldBindingFlags);
+                    foreach (System.Reflection.FieldInfo field in fields)
+                    {
+                        var headerFileAttributes = field.GetCustomAttributes(typeof(C.HeaderFilesAttribute), false);
+                        if (headerFileAttributes.Length > 0)
+                        {
+                            Opus.Core.FileCollection headerFileCollection = field.GetValue(headerLibrary) as Opus.Core.FileCollection;
+                            if (headerFileCollection.Count > 0)
+                            {
+                                System.Text.StringBuilder headersStatement = new System.Text.StringBuilder();
+                                headersStatement.AppendFormat("{0}:HEADERS += ", nodeData.Configuration);
+                                foreach (string headerPath in headerFileCollection)
+                                {
+                                    headersStatement.AppendFormat("\\\n\t{0}", headerPath.Replace('\\', '/'));
+                                }
+                                proFileWriter.WriteLine(headersStatement.ToString());
+                            }
+                        }
+                    }
+                }
+            }
+
             success = true;
-            return null;
+            return nodeData;
         }
     }
 }
