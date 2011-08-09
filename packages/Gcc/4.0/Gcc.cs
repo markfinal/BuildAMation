8a8448da2dc61dbed509acc6ef47a10479c43aaa
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2011-08-09 21:29:32 +0000

    [xml] Adding Gcc 4.0 (clone of 4.1 hopefully)

---
+++ packages/Gcc/4.0/Gcc.cs
@@ -0,0 +1,16 @@
+// <copyright file="Gcc.cs" company="Mark Final">
+//  Opus package
+// </copyright>
+// <summary>Gcc package</summary>
+// <author>Mark Final</author>
+[assembly: Opus.Core.RegisterTargetToolChain("C", "gcc", "Gcc.Toolchain.VersionString")]
+
+[assembly: Opus.Core.MapToolChainClassTypes("C", "gcc", C.ClassNames.ArchiverTool, typeof(GccCommon.Archiver), typeof(Gcc.ArchiverOptionCollection))]
+[assembly: Opus.Core.MapToolChainClassTypes("C", "gcc", C.ClassNames.CCompilerTool, typeof(Gcc.CCompiler), typeof(Gcc.CCompilerOptionCollection))]
+[assembly: Opus.Core.MapToolChainClassTypes("C", "gcc", C.ClassNames.CPlusPlusCompilerTool, typeof(Gcc.CPlusPlusCompiler), typeof(Gcc.CPlusPlusCompilerOptionCollection))]
+[assembly: Opus.Core.MapToolChainClassTypes("C", "gcc", C.ClassNames.LinkerTool, typeof(Gcc.Linker), typeof(Gcc.LinkerOptionCollection))]
+[assembly: Opus.Core.MapToolChainClassTypes("C", "gcc", C.ClassNames.Toolchain, typeof(Gcc.Toolchain), typeof(Gcc.ToolchainOptionCollection))]
+
+namespace Gcc
+{
+}
