de499dcd121a461fd63694d392b4d6976a42e868
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2011-08-10 12:56:51 +0000

    [xml] Added -dependent=<package>-<version> command to specify a dependent package to work on.

---
+++ Opus/Actions/SetDependentAction.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="SetDependentAction.cs" company="Mark Final">
+//  Opus
+// </copyright>
+// <summary>Opus main application.</summary>
+// <author>Mark Final</author>
+
+[assembly: Opus.Core.RegisterAction(typeof(Opus.SetDependentAction))]
+
+namespace Opus
+{
+    [Core.PreambleAction]
+    internal class SetDependentAction : Core.IActionWithArguments
+    {
+        public string CommandLineSwitch
+        {
+            get
+            {
+                return "-dependent";
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return "Specify the dependent package to act upon";
+            }
+        }
+
+        public void AssignArguments(string arguments)
+        {
+            this.DependentPackageAndVersion = arguments;
+        }
+
+        public string DependentPackageAndVersion
+        {
+            get;
+            private set;
+        }
+
+        public bool Execute()
+        {
+            return true;
+        }
+    }
+}
