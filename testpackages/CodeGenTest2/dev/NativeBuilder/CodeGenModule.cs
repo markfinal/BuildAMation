d5bc6958cd7b6b6eb152cf836d2f8fb2af76198f
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2011-09-04 17:04:43 +0000

    [trunk] Adding Gcc-4.0.1 to CodeGenTest2-dev, and fixing running C# assemblies on OSX

--- testpackages/CodeGenTest2/dev/NativeBuilder/CodeGenModule.cs
+++ testpackages/CodeGenTest2/dev/NativeBuilder/CodeGenModule.cs
@@ -8,9 +8,10 @@ namespace NativeBuilder
             Opus.Core.Target target = node.Target;
             CodeGenTest2.CodeGenTool tool = new CodeGenTest2.CodeGenTool();
             CodeGenTest2.CodeGenOptions toolOptions = codeGenModule.Options as CodeGenTest2.CodeGenOptions;
-
+            
+            string toolExecutable = tool.Executable(target);
             Opus.Core.StringArray inputFiles = new Opus.Core.StringArray();
-            inputFiles.Add(tool.Executable(target));
+            inputFiles.Add(toolExecutable);
             Opus.Core.StringArray outputFiles = codeGenModule.Options.OutputPaths.Paths;
             if (!RequiresBuilding(outputFiles, inputFiles))
             {
@@ -24,7 +25,14 @@ namespace NativeBuilder
             {
                 CommandLineProcessor.ICommandLineSupport commandLineOption = toolOptions as CommandLineProcessor.ICommandLineSupport;
                 commandLineOption.ToCommandLineArguments(commandLineBuilder, target);
-
+                
+                // OSX insists on running C# assemblies through mono
+                if (target.HasPlatform(Opus.Core.EPlatform.OSX))
+                {
+                    commandLineBuilder.Insert(0, toolExecutable);
+                    toolExecutable = "mono";
+                }
+    
                 Opus.Core.DirectoryCollection directoriesToCreate = commandLineOption.DirectoriesToCreate();
                 foreach (string directoryPath in directoriesToCreate)
                 {
@@ -36,7 +44,7 @@ namespace NativeBuilder
                 throw new Opus.Core.Exception("CodeGen options does not support command line translation");
             }
 
-            int exitCode = CommandLineProcessor.Processor.Execute(node, tool, tool.Executable(target), commandLineBuilder);
+            int exitCode = CommandLineProcessor.Processor.Execute(node, tool, toolExecutable, commandLineBuilder);
             success = (0 == exitCode);
 
             return null;
