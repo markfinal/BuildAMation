c8116859d11982addef6c8e689a65bf12592d32c
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2011-09-08 21:42:38 +0000

    [trunk] Adding a Batch file to the root source directory to set up the environment for running Opus.

---
+++ env.bat
@@ -0,0 +1,23 @@
+@ECHO OFF
+
+SET DefaultFlavour=Release
+
+IF NOT "%1"=="" (
+  SET flavour=%1
+) ELSE (
+  SET flavour=%DefaultFlavour%
+)
+
+SET OpusPath=%CD%\bin\%flavour%
+
+REM Export the PATH
+IF NOT EXIST %OpusPath% (
+  ECHO Opus directory '%OpusPath%' does not exist
+) ELSE (
+  ECHO Adding '%OpusPath%' to the start of PATH
+  SET PATH=%OPUSPATH%;%PATH%
+)
+
+REM Unset local variables
+SET OpusPath=
+SET DefaultFlavour=
