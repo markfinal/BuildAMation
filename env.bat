34c72c9d64839e159075abcbef54e7aba8f052d7
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2011-09-20 20:08:47 +0000

    [trunk] Issue 46. Fixing env.bat for Windows to expand the original PATH correctly.

--- env.bat
+++ env.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 
+SETLOCAL ENABLEDELAYEDEXPANSION
+
 SET DefaultFlavour=Release
 
 IF NOT "%1"=="" (
@@ -9,16 +11,16 @@ IF NOT "%1"=="" (
 )
 
 SET OpusPath=%CD%\bin\%flavour%
+REM Using delayed expansion in case PATH has some spaces in
+SET NewPath=!PATH!
 
 REM Export the PATH
 IF NOT EXIST %OpusPath% (
   ECHO Opus directory '%OpusPath%' does not exist
 ) ELSE (
   ECHO Adding '%OpusPath%' to the start of PATH
-  REM Using delayed expansion in case PATH has some spaces in
-  SET PATH=%OPUSPATH%;!PATH!
+  SET NewPath=%OPUSPATH%;!NewPath!
 )
 
-REM Unset local variables
-SET OpusPath=
-SET DefaultFlavour=
+REM Pass the local variable out to the global
+ENDLOCAL&SET PATH=%NewPath%
