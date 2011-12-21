de467cebbe52251172c2600742ca84d951548ae4
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2011-12-21 20:24:45 +0000

    [trunk] Adding Gcc-4.5 support for Linux. A new include path under /usr/include needed to be added.

---
+++ packages/Gcc/4.5/Scripts/CPlusPlusCompilerOptionCollection.cs
@@ -0,0 +1,43 @@
+// <copyright file="CPlusPlusCompilerOptionCollection.cs" company="Mark Final">
+//  Opus package
+// </copyright>
+// <summary>Gcc package</summary>
+// <author>Mark Final</author>
+namespace Gcc
+{
+    public sealed partial class CPlusPlusCompilerOptionCollection : GccCommon.CPlusPlusCompilerOptionCollection
+    {
+        protected override void InitializeDefaults(Opus.Core.DependencyNode node)
+        {
+            base.InitializeDefaults(node);
+
+            Opus.Core.Target target = node.Target;
+
+            CCompiler compilerInstance = C.CompilerFactory.GetTargetInstance(target, C.ClassNames.CCompilerTool) as CCompiler;
+
+            string cppIncludePath = compilerInstance.GxxIncludePath(target);
+            if (!System.IO.Directory.Exists(cppIncludePath))
+            {
+                throw new Opus.Core.Exception(System.String.Format("Gcc C++ include path '{0}' does not exist. Is g++ installed?", cppIncludePath), false);
+            }
+            string cppIncludePath2 = System.String.Format("{0}/{1}", cppIncludePath, compilerInstance.MachineType(target));
+            if (!System.IO.Directory.Exists(cppIncludePath2))
+            {
+                throw new Opus.Core.Exception(System.String.Format("Gcc C++ include path '{0}' does not exist. Is g++ installed?", cppIncludePath2), false);
+            }
+
+            this.SystemIncludePaths.Add(null, cppIncludePath);
+            this.SystemIncludePaths.Add(null, cppIncludePath2);
+        }
+
+        public CPlusPlusCompilerOptionCollection()
+            : base()
+        {
+        }
+
+        public CPlusPlusCompilerOptionCollection(Opus.Core.DependencyNode node)
+            : base(node)
+        {
+        }
+    }
+}
