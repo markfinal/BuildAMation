a167b802ad2ea72dcd853698c834eac671a39ca8
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2012-02-02 22:01:38 +0000

    [trunk] Fixes to solution generation given recent additions to VC compiler options.

--- packages/VSSolutionBuilder/dev/VSSolutionBuilder.cs
+++ packages/VSSolutionBuilder/dev/VSSolutionBuilder.cs
@@ -146,7 +146,8 @@ namespace VSSolutionBuilder
         {
             if (System.String.IsNullOrEmpty(path))
             {
-                throw new Opus.Core.Exception("Cannot refactor an empty path for VisualStudio projects", false);
+                Opus.Core.Log.DebugMessage("Cannot refactor an empty path for VisualStudio projects");
+                return path;
             }
 
             string refactoredPath = path;
