fadc862c1a5931695ecc07a175c7c25fc1a1428a
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2012-03-04 09:58:29 +0000

    [trunk] Moving resource compiler setup for VisualC to the VisualCCommon package.

--- packages/WindowsSDKCommon/dev/Scripts/Win32ResourceCompiler.cs
+++ packages/WindowsSDKCommon/dev/Scripts/Win32ResourceCompiler.cs
@@ -1,4 +1,4 @@
-// <copyright file="Win32ResoureCompiler.cs" company="Mark Final">
+// <copyright file="Win32ResoureCompilerBase.cs" company="Mark Final">
 //  Opus package
 // </copyright>
 // <summary>WindowsSDKCommon package</summary>
@@ -27,45 +27,6 @@ namespace C
         public abstract string OutputFileSwitch
         {
             get;
-        }
-    }
-}
-
-namespace VisualCCommon
-{
-    sealed class Win32ResourceCompiler : C.Win32ResourceCompilerBase
-    {
-        private string platformBinFolder;
-
-        public Win32ResourceCompiler(Opus.Core.Target target)
-        {
-            if (!Opus.Core.OSUtilities.IsWindowsHosting)
-            {
-                return;
-            }
-
-            this.platformBinFolder = WindowsSDK.WindowsSDK.BinPath(target);
-        }
-
-        public override string Executable(Opus.Core.Target target)
-        {
-            return System.IO.Path.Combine(this.platformBinFolder, "rc.exe");
-        }
-
-        public override string InputFileSwitch
-        {
-            get
-            {
-                return string.Empty;
-            }
-        }
-
-        public override string OutputFileSwitch
-        {
-            get
-            {
-                return "/fo ";
-            }
         }
     }
 }
