449d8dbd6ed714fadc31462b634be09ff1c7adef
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2012-03-05 21:38:06 +0000

    [trunk] Removing now obsolete WindowsSDKCommon package.

--- packages/WindowsSDKCommon/dev/WindowsSDKCommon.cs
+++
@@ -1,9 +0,0 @@
-// <copyright file="WindowsSDKCommon.cs" company="Mark Final">
-//  Opus package
-// </copyright>
-// <summary>WindowsSDKCommon package</summary>
-// <author>Mark Final</author>
-
-namespace WindowsSDKCommon
-{
-}
