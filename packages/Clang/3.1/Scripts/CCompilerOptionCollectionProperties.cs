bef9bd14d0294ac6f2caea523b7695dded43ec17
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2012-10-15 21:56:18 +0000

    [050dev] Regenerating some files without an additional space at the end of the arguments.

--- packages/Clang/3.1/Scripts/CCompilerOptionCollectionProperties.cs
+++ packages/Clang/3.1/Scripts/CCompilerOptionCollectionProperties.cs
@@ -1,6 +1,6 @@
 // Automatically generated file from OpusOptionInterfacePropertyGenerator. DO NOT EDIT.
 // Command line:
-// -i=..\..\..\C\dev\Scripts\ICCompilerOptions.cs -n=Clang -c=CCompilerOptionCollection -p -d -dd=..\..\..\CommandLineProcessor\dev\Scripts\CommandLineDelegate.cs -pv=PrivateData 
+// -i=..\..\..\C\dev\Scripts\ICCompilerOptions.cs -n=Clang -c=CCompilerOptionCollection -p -d -dd=..\..\..\CommandLineProcessor\dev\Scripts\CommandLineDelegate.cs -pv=PrivateData
 namespace Clang
 {
     public partial class CCompilerOptionCollection
