c29291c3159342dea4a0fa4cf980e6827a68c811
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2012-10-18 18:33:13 +0000

    [050dev] Some fixes for the CSharp package

--- packages/CSharp/dev/CSharp.cs
+++ packages/CSharp/dev/CSharp.cs
@@ -5,8 +5,34 @@
 // <author>Mark Final</author>
 [assembly: Opus.Core.MapToolChainClassTypes("CSharp", "dotnet", "ClassCsc", typeof(CSharp.Csc), typeof(CSharp.OptionCollection))]
 
-[assembly: CSharp.RegisterToolchain("dotnet")]
+[assembly: CSharp.RegisterToolchain("dotnet", typeof(CSharp.ToolsetInfo))]
 
 namespace CSharp
 {
+    public sealed class ToolsetInfo : Opus.Core.IToolsetInfo
+    {
+        #region IToolsetInfo Members
+
+        string Opus.Core.IToolsetInfo.BinPath(Opus.Core.Target target)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        Opus.Core.StringArray Opus.Core.IToolsetInfo.Environment
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        string Opus.Core.IToolsetInfo.InstallPath(Opus.Core.Target target)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        string Opus.Core.IToolsetInfo.Version(Opus.Core.Target target)
+        {
+            return "dev";
+        }
+
+        #endregion
+    }
 }
--- packages/CSharp/dev/Scripts/Attributes/RegisterToolchainAttribute.cs
+++ packages/CSharp/dev/Scripts/Attributes/RegisterToolchainAttribute.cs
@@ -3,8 +3,13 @@ namespace CSharp
     [System.AttributeUsage(System.AttributeTargets.Assembly, AllowMultiple=true)]
     public sealed class RegisterToolchainAttribute : Opus.Core.RegisterToolchainAttribute
     {
-        public RegisterToolchainAttribute(string name)
+        public RegisterToolchainAttribute(string name, System.Type infoType)
         {
+            if (!typeof(Opus.Core.IToolsetInfo).IsAssignableFrom(infoType))
+            {
+                throw new Opus.Core.Exception(System.String.Format("Toolset information type '{0}' does not implement the interface {1}", infoType.ToString(), typeof(Opus.Core.IToolsetInfo).ToString()), false);
+            }
+
             if (!typeof(IOptions).IsAssignableFrom(typeof(OptionCollection)))
             {
                 throw new Opus.Core.Exception(System.String.Format("C Compiler option type '{0}' does not implement the interface {1}", typeof(OptionCollection).ToString(), typeof(IOptions).ToString()), false);
@@ -20,6 +25,15 @@ namespace CSharp
                     Opus.Core.State.AddCategory("ToolchainTypeMap");
                 }
                 Opus.Core.State.Add("ToolchainTypeMap", name, map);
+            }
+
+            // define where toolset information can be located
+            {
+                if (!Opus.Core.State.HasCategory("ToolsetInfo"))
+                {
+                    Opus.Core.State.AddCategory("ToolsetInfo");
+                }
+                Opus.Core.State.Add("ToolsetInfo", name, Opus.Core.ToolsetInfoFactory.CreateToolsetInfo(infoType));
             }
         }
     }
