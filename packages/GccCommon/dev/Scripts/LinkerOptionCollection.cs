4e248ee4eed23595f2fb9f1a1565b363e19249e7
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2012-10-22 20:37:27 +0000

    [050dev] Make sure Gcc links against libstdc++.a if C++ object code is linked against

--- packages/GccCommon/dev/Scripts/LinkerOptionCollection.cs
+++ packages/GccCommon/dev/Scripts/LinkerOptionCollection.cs
@@ -45,6 +45,16 @@ namespace GccCommon
             (this as ILinkerOptions).AllowUndefinedSymbols = (node.Module is C.DynamicLibrary);
             (this as ILinkerOptions).RPath = new Opus.Core.StringArray();
 
+            // we use gcc as the linker - if there is C++ code included, link against libstdc++
+            foreach (Opus.Core.DependencyNode child in node.Children)
+            {
+                if (child.Module is C.CPlusPlus.ObjectFile || child.Module is C.CPlusPlus.ObjectFileCollection)
+                {
+                    (this as C.ILinkerOptions).Libraries.Add("-lstdc++");
+                    break;
+                }
+            }
+
             /*
              This is an example link line using gcc with -v
 
