f87385c9d2149b3e1d1af88684a49e90b0200ea1
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2012-10-23 13:17:40 +0000

    [050dev] Shared the majority of the ToolsetInfo code with the VisualCCommon package - only individual VisualC packages need to implement is a function to find the install path

---
+++ packages/VisualCCommon/dev/Scripts/ToolsetInfo.cs
@@ -0,0 +1,232 @@
+// <copyright file="ToolsetInfo.cs" company="Mark Final">
+//  Opus package
+// </copyright>
+// <summary>VisualCCommon package</summary>
+// <author>Mark Final</author>
+namespace VisualCCommon
+{
+    public abstract class ToolsetInfo : Opus.Core.IToolsetInfo, C.ICompilerInfo, C.ILinkerInfo, C.IArchiverInfo, C.IWinResourceCompilerInfo, VisualStudioProcessor.IVisualStudioTargetInfo
+    {
+        protected string installPath;
+        protected string bin32Folder;
+        protected string bin64Folder;
+        protected string bin6432Folder;
+        protected Opus.Core.StringArray lib32Folder = new Opus.Core.StringArray();
+        protected Opus.Core.StringArray lib64Folder = new Opus.Core.StringArray();
+        protected Opus.Core.StringArray environment = new Opus.Core.StringArray();
+
+        protected abstract void GetInstallPath();
+
+        #region IToolsetInfo Members
+
+        string Opus.Core.IToolsetInfo.BinPath(Opus.Core.Target target)
+        {
+            this.GetInstallPath();
+
+            if (target.HasPlatform(Opus.Core.EPlatform.Win64))
+            {
+                if (Opus.Core.OSUtilities.Is64BitHosting)
+                {
+                    return this.bin64Folder;
+                }
+                else
+                {
+                    return this.bin6432Folder;
+                }
+            }
+            else
+            {
+                return this.bin32Folder;
+            }
+        }
+
+        Opus.Core.StringArray Opus.Core.IToolsetInfo.Environment
+        {
+            get
+            {
+                this.GetInstallPath();
+                return this.environment;
+            }
+        }
+
+        string Opus.Core.IToolsetInfo.InstallPath(Opus.Core.Target target)
+        {
+            this.GetInstallPath();
+            return this.installPath;
+        }
+
+        string Opus.Core.IToolsetInfo.Version(Opus.Core.Target target)
+        {
+            this.GetInstallPath();
+            return "9.0";
+        }
+
+        #endregion
+
+        #region ICompilerInfo Members
+
+        string C.ICompilerInfo.PreprocessedOutputSuffix
+        {
+            get
+            {
+                return ".i";
+            }
+        }
+
+        string C.ICompilerInfo.ObjectFileSuffix
+        {
+            get
+            {
+                return ".obj";
+            }
+        }
+
+        string C.ICompilerInfo.ObjectFileOutputSubDirectory
+        {
+            get
+            {
+                return "obj";
+            }
+        }
+
+        Opus.Core.StringArray C.ICompilerInfo.IncludePaths(Opus.Core.Target target)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        #region ILinkerInfo Members
+
+        string C.ILinkerInfo.ExecutableSuffix
+        {
+            get
+            {
+                return ".exe";
+            }
+        }
+
+        string C.ILinkerInfo.MapFileSuffix
+        {
+            get
+            {
+                return ".map";
+            }
+        }
+
+        string C.ILinkerInfo.ImportLibraryPrefix
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+        string C.ILinkerInfo.ImportLibrarySuffix
+        {
+            get
+            {
+                return ".lib";
+            }
+        }
+
+        string C.ILinkerInfo.DynamicLibraryPrefix
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+        string C.ILinkerInfo.DynamicLibrarySuffix
+        {
+            get
+            {
+                return ".dll";
+            }
+        }
+
+        string C.ILinkerInfo.ImportLibrarySubDirectory
+        {
+            get
+            {
+                return "lib";
+            }
+        }
+
+        string C.ILinkerInfo.BinaryOutputSubDirectory
+        {
+            get
+            {
+                return "bin";
+            }
+        }
+
+        Opus.Core.StringArray C.ILinkerInfo.LibPaths(Opus.Core.Target target)
+        {
+            this.GetInstallPath();
+            if (target.HasPlatform(Opus.Core.EPlatform.Win64))
+            {
+                return this.lib64Folder;
+            }
+            else
+            {
+                return this.lib32Folder;
+            }
+        }
+
+        #endregion
+
+        #region IArchiverInfo Members
+
+        string C.IArchiverInfo.StaticLibraryPrefix
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+        string C.IArchiverInfo.StaticLibrarySuffix
+        {
+            get
+            {
+                return ".lib";
+            }
+        }
+
+        string C.IArchiverInfo.StaticLibraryOutputSubDirectory
+        {
+            get
+            {
+                return "lib";
+            }
+        }
+
+        #endregion
+
+        #region IWinResourceCompilerInfo Members
+
+        string C.IWinResourceCompilerInfo.CompiledResourceSuffix
+        {
+            get
+            {
+                return ".res";
+            }
+        }
+
+        #endregion
+
+        #region IVisualStudioTargetInfo Members
+
+        VisualStudioProcessor.EVisualStudioTarget VisualStudioProcessor.IVisualStudioTargetInfo.VisualStudioTarget
+        {
+            get
+            {
+                return VisualStudioProcessor.EVisualStudioTarget.VCPROJ;
+            }
+        }
+
+        #endregion
+    }
+}
