920c964e4fbb988ef343f2a3615aecf615fe2e93
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2012-11-03 11:34:10 +0000

    [050dev] Windows resource compiler support for Mingw with the new toolset API

--- packages/Mingw/3.4.5/Scripts/Toolset.cs
+++ packages/Mingw/3.4.5/Scripts/Toolset.cs
@@ -14,16 +14,16 @@ namespace Mingw
             this.toolMap[typeof(C.ILinkerTool)] = new Linker(this);
 #if false
             this.toolMap[typeof(C.IArchiverTool)] = new MingwCommon.Archiver(this);
-            this.toolMap[typeof(C.IWinResourceCompilerTool)] = new MingwCommon.Win32ResourceCompiler(this);
 #endif
+            this.toolMap[typeof(C.IWinResourceCompilerTool)] = new MingwCommon.Win32ResourceCompiler(this);
 
             this.toolOptionsMap[typeof(C.ICompilerTool)] = typeof(Mingw.CCompilerOptionCollection);
             this.toolOptionsMap[typeof(C.ICxxCompilerTool)] = typeof(Mingw.CPlusPlusCompilerOptionCollection);
             this.toolOptionsMap[typeof(C.ILinkerTool)] = typeof(Mingw.LinkerOptionCollection);
 #if false
             this.toolOptionsMap[typeof(C.IArchiverTool)] = typeof(Mingw.ArchiverOptionCollection);
-            this.toolOptionsMap[typeof(C.IWinResourceCompilerTool)] = typeof(C.Win32ResourceCompilerOptionCollection);
 #endif
+            this.toolOptionsMap[typeof(C.IWinResourceCompilerTool)] = typeof(C.Win32ResourceCompilerOptionCollection);
         }
 
         protected override void GetInstallPath()
--- packages/MingwCommon/dev/Scripts/Win32ResourceCompiler.cs
+++ packages/MingwCommon/dev/Scripts/Win32ResourceCompiler.cs
@@ -5,6 +5,67 @@
 // <author>Mark Final</author>
 namespace MingwCommon
 {
+    // NEW STYLE
+#if true
+    public sealed class Win32ResourceCompiler : C.IWinResourceCompilerTool, Opus.Core.IToolEnvironmentPaths
+    {
+        private Opus.Core.IToolset toolset;
+        private Opus.Core.StringArray pathEnvironment = new Opus.Core.StringArray();
+
+        public Win32ResourceCompiler(Opus.Core.IToolset toolset)
+        {
+            this.toolset = toolset;
+            this.pathEnvironment.Add(@"c:\windows\system32");
+        }
+
+        #region IWinResourceCompilerTool Members
+
+        string C.IWinResourceCompilerTool.CompiledResourceSuffix
+        {
+            get
+            {
+                return ".obj";
+            }
+        }
+
+        string C.IWinResourceCompilerTool.InputFileSwitch
+        {
+            get
+            {
+                return "--input=";
+            }
+        }
+
+        string C.IWinResourceCompilerTool.OutputFileSwitch
+        {
+            get
+            {
+                return "--output=";
+            }
+        }
+
+        #endregion
+
+        #region ITool Members
+
+        string Opus.Core.ITool.Executable(Opus.Core.Target target)
+        {
+            string platformBinFolder = this.toolset.BinPath((Opus.Core.BaseTarget)target);
+            return System.IO.Path.Combine(platformBinFolder, "windres.exe");
+        }
+
+        #endregion
+
+        #region IToolEnvironmentPaths Members
+
+        Opus.Core.StringArray Opus.Core.IToolEnvironmentPaths.Paths(Opus.Core.Target target)
+        {
+            return this.pathEnvironment;
+        }
+
+        #endregion
+    }
+#else
     sealed class Win32ResourceCompiler : C.Win32ResourceCompilerBase, Opus.Core.IToolEnvironmentPaths
     {
         private Opus.Core.StringArray pathEnvironment = new Opus.Core.StringArray();
@@ -56,4 +117,5 @@ namespace MingwCommon
             return this.pathEnvironment;
         }
     }
+#endif
 }
