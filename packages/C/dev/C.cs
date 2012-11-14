ed8e52aeadde3c983210c440c8d65629b1a99311
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2012-11-14 22:38:03 +0000

    [trunk] Removed C.ClassNames as it has been superceded by Toolsets and tool interfaces.

--- packages/C/dev/C.cs
+++ packages/C/dev/C.cs
@@ -5,26 +5,4 @@
 // <author>Mark Final</author>
 namespace C
 {
-    public static class ClassNames
-    {
-        public const string CCompilerTool = "ClassCCompiler";
-        public const string CCompilerToolOptions = "ClassCCompilerOptions";
-
-        public const string CPlusPlusCompilerTool = "ClassCPlusPlusCompiler";
-        public const string CPlusPlusCompilerToolOptions = "ClassCPlusPlusCompilerOptions";
-
-        public const string ArchiverTool = "ClassArchiver";
-        public const string ArchiverToolOptions = "ClassArchiverOptions";
-
-        public const string LinkerTool = "ClassLinker";
-        public const string LinkerToolOptions = "ClassLinkerOptions";
-
-#if false
-        public const string Toolchain = "ClassToolchain";
-        public const string ToolchainOptions = "ClassToolchainOptions";
-#endif
-
-        public const string Win32ResourceCompilerTool = "ClassWin32ResourceCompiler";
-        public const string Win32ResourceCompilerToolOptions = "ClassWin32ResourceCompilerOptions";
-    }
 }
