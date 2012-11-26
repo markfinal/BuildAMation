8d789dce84a887c26c55fd43ca6cea14df9991fe
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2012-11-26 09:14:39 +0000

    [trunk] Qt 4.6.3 - moved Qt thirdparty module to a Scripts file

---
+++ packages/Qt/4.6.3/Scripts/Qt.cs
@@ -0,0 +1,31 @@
+// <copyright file="Qt.cs" company="Mark Final">
+//  Opus package
+// </copyright>
+// <summary>Qt package</summary>
+// <author>Mark Final</author>
+namespace Qt
+{
+    public sealed class Qt : QtCommon.QtCommon
+    {
+        public Qt(Opus.Core.Target target)
+        {
+            // The target here will have a null IToolset because this module is a Thirdparty
+            // module, which has no need for a tool, so no toolset is configured
+            // However, we do need to know where Qt was installed, which is in the Toolset
+            // so just grab the instance
+            Opus.Core.IToolset toolset = Opus.Core.ToolsetFactory.GetInstance(typeof(Toolset));
+            string installPath = toolset.InstallPath((Opus.Core.BaseTarget)target);
+            if (Opus.Core.OSUtilities.IsOSXHosting)
+            {
+                this.BinPath = installPath;
+            }
+            else
+            {
+                this.BinPath = System.IO.Path.Combine(installPath, "bin");
+            }
+
+            this.LibPath = System.IO.Path.Combine(installPath, "lib");
+            this.includePaths.Add(System.IO.Path.Combine(installPath, "include"));
+        }
+    }
+}
