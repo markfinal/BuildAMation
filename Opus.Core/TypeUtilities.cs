abab757f7ea1f128a2323e44c085c38b0834213e
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2012-11-26 20:11:31 +0000

    [trunk] Adding a TypeUtilities static class

---
+++ Opus.Core/TypeUtilities.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="TypeUtilities.cs" company="Mark Final">
+//  Opus
+// </copyright>
+// <summary>Opus Core</summary>
+// <author>Mark Final</author>
+namespace Opus.Core
+{
+    public static class TypeUtilities
+    {
+        public static void CheckTypeImplementsInterface(System.Type type)
+        {
+            if (!typeof(IModule).IsAssignableFrom(type))
+            {
+                throw new Exception("Type '{0}' does not implement the interface {1}", type.ToString(), typeof(IModule).ToString());
+            }
+        }
+
+        public static void CheckTypeIsNotAbstract(System.Type type)
+        {
+            if (type.IsAbstract)
+            {
+                throw new Exception("Type '{0}' is abstract", type.ToString());
+            }
+        }
+    }
+}
