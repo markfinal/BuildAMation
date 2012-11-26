3efed2351f09b2f1913edd1d56cf3ca74f707199
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2012-11-26 09:11:26 +0000

    [trunk] MakeFileBuilder - moved some class definitions around

---
+++ packages/MakeFileBuilder/dev/Scripts/MakeFileBuilder.cs
@@ -0,0 +1,18 @@
+// <copyright file="MakeFileBuilder.cs" company="Mark Final">
+//  Opus package
+// </copyright>
+// <summary>MakeFileBuilder package</summary>
+// <author>Mark Final</author>
+namespace MakeFileBuilder
+{
+    public sealed partial class MakeFileBuilder : Opus.Core.IBuilder
+    {
+        public static string GetMakeFilePathName(Opus.Core.DependencyNode node)
+        {
+            string makeFileDirectory = System.IO.Path.Combine(node.GetModuleBuildDirectory(), "Makefiles");
+            string makeFilePathName = System.IO.Path.Combine(makeFileDirectory, System.String.Format("{0}_{1}.mak", node.UniqueModuleName, node.Target));
+            Opus.Core.Log.DebugMessage("Makefile : '{0}'", makeFilePathName);
+            return makeFilePathName;
+        }
+    }
+}
