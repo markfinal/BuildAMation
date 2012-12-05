7b3cd181f542449a534b490bc3c4de8d15f7c08f
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2012-12-05 14:19:53 +0000

    [trunk] Moved Gcc-4.5 to the new delegate and properties layout

---
+++ packages/Gcc/4.5/Scripts/CCompilerOptionCollectionDelegates.cs
@@ -0,0 +1,43 @@
+// Automatically generated file from OpusOptionInterfacePropertyGenerator.
+// Command line:
+// -i=ICCompilerOptions.cs -n=Gcc -c=CCompilerOptionCollection -p -d -dd=..\..\..\CommandLineProcessor\dev\Scripts\CommandLineDelegate.cs -pv=GccCommon.PrivateData -e
+
+namespace Gcc
+{
+    public partial class CCompilerOptionCollection
+    {
+        #region ICCompilerOptions Option delegates
+        private static void VisibilityCommandLineProcessor(object sender, Opus.Core.StringArray commandLineBuilder, Opus.Core.Option option, Opus.Core.Target target)
+        {
+            // requires gcc 4.0
+            Opus.Core.ValueTypeOption<EVisibility> enumOption = option as Opus.Core.ValueTypeOption<EVisibility>;
+            switch (enumOption.Value)
+            {
+                case EVisibility.Default:
+                    commandLineBuilder.Add("-fvisibility=default");
+                    break;
+
+                case EVisibility.Hidden:
+                    commandLineBuilder.Add("-fvisibility=hidden");
+                    break;
+
+                case EVisibility.Internal:
+                    commandLineBuilder.Add("-fvisibility=internal");
+                    break;
+
+                case EVisibility.Protected:
+                    commandLineBuilder.Add("-fvisibility=protected");
+                    break;
+
+                default:
+                    throw new Opus.Core.Exception("Unrecognized visibility option");
+            }
+        }
+        #endregion
+        protected override void SetDelegates(Opus.Core.DependencyNode node)
+        {
+            base.SetDelegates(node);
+            this["Visibility"].PrivateData = new GccCommon.PrivateData(VisibilityCommandLineProcessor);
+        }
+    }
+}
---
+++ packages/Gcc/4.5/Scripts/GenerateFiles.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+import os
+import string
+import subprocess
+
+def ExecuteProcess(args, verbose=False):
+    if verbose:
+        print "Executing: '%s'" % " ".join(args)
+    process = subprocess.Popen(args, stdout=subprocess.PIPE)
+    output = process.communicate()
+    if process.returncode != 0:
+        raise RuntimeError("Command '%s' failed" % (" ".join(args)))
+    return output
+
+get_opus_dir_command = [
+    "Opus",
+    "-verbosity=0",
+    "-showdirectory"
+]
+(stdout,stderr) = ExecuteProcess(get_opus_dir_command)
+opusBinDir = string.strip(stdout, os.linesep)
+opusPackageDir = os.path.abspath(os.path.join(opusBinDir, os.pardir, os.pardir, "packages"))
+
+# C compiler options
+cCompiler_options = [
+    os.path.join(opusBinDir, "OpusOptionInterfacePropertyGenerator.exe"),
+    "-i=" + os.path.relpath(os.path.join(opusPackageDir, "Gcc", "4.5", "Scripts", "ICCompilerOptions.cs")),
+    "-n=Gcc",
+    "-c=CCompilerOptionCollection",
+    "-p", # generate properties
+    "-d", # generate delegates
+    "-dd=" + os.path.relpath(os.path.join(opusPackageDir, "CommandLineProcessor", "dev", "Scripts", "CommandLineDelegate.cs")),
+    "-pv=GccCommon.PrivateData",
+    "-e" # this option set derives from the GccCommon option set
+]
+(stdout,stderr) = ExecuteProcess(cCompiler_options, True)
+print stdout
+
+# C++ compiler options
+cxxCompiler_options = [
+    os.path.join(opusBinDir, "OpusOptionInterfacePropertyGenerator.exe"),
+    "-i=" + os.path.relpath(os.path.join(opusPackageDir, "C", "dev", "Scripts", "ICxxCompilerOptions.cs")),
+    "-n=Gcc",
+    "-c=CxxCompilerOptionCollection",
+    "-p", # generate properties
+    "-d", # generate delegates
+    "-dd=" + os.path.relpath(os.path.join(opusPackageDir, "CommandLineProcessor", "dev", "Scripts", "CommandLineDelegate.cs")),
+    "-pv=GccCommon.PrivateData",
+    "-e" # this option set derives from the C option set
+]
+(stdout,stderr) = ExecuteProcess(cxxCompiler_options, True)
+print stdout
