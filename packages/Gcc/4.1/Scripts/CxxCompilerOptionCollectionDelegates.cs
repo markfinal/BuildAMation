caf949b8c3d0a7c3edeb005309b8492607088f33
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2012-12-05 14:04:02 +0000

    [trunk] Moving Gcc-4.1 to the new delegate and properties layout

---
+++ packages/Gcc/4.1/Scripts/CxxCompilerOptionCollectionDelegates.cs
@@ -0,0 +1,21 @@
+// Automatically generated file from OpusOptionInterfacePropertyGenerator.
+// Command line:
+// -i=..\..\..\C\dev\Scripts\ICxxCompilerOptions.cs -n=Gcc -c=CxxCompilerOptionCollection -p -d -dd=..\..\..\CommandLineProcessor\dev\Scripts\CommandLineDelegate.cs -pv=GccCommon.PrivateData -e
+
+namespace Gcc
+{
+    public partial class CxxCompilerOptionCollection
+    {
+        #region C.ICxxCompilerOptions Option delegates
+        private static void ExceptionHandlerCommandLineProcessor(object sender, Opus.Core.StringArray commandLineBuilder, Opus.Core.Option option, Opus.Core.Target target)
+        {
+            GccCommon.CxxCompilerOptionCollection.ExceptionHandlerCommandLine(sender, commandLineBuilder, option, target);
+        }
+        #endregion
+        protected override void SetDelegates(Opus.Core.DependencyNode node)
+        {
+            base.SetDelegates(node);
+            this["ExceptionHandler"].PrivateData = new GccCommon.PrivateData(ExceptionHandlerCommandLineProcessor);
+        }
+    }
+}
