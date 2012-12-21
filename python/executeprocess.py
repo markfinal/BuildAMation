28198f5f200fa655b1b509d0c744391eaa714577
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2012-12-21 23:05:15 +0000

    [trunk] Convert back slashes to forward slashes before executing Python processes

--- python/executeprocess.py
+++ python/executeprocess.py
@@ -2,10 +2,14 @@ import subprocess
 import sys
 
 def ExecuteProcess(args, verbose=False, isCSharp=False):
+    if sys.platform.startswith("win"):
+        for index,item in enumerate(args):
+            if '\\' in item:
+                args[index] = item.replace('\\', '/')
     if isCSharp and sys.platform.startswith("darwin"):
-      newArgs = ["mono"]
-      newArgs.extend(args)
-      args = newArgs
+        newArgs = ["mono"]
+        newArgs.extend(args)
+        args = newArgs
     if verbose:
         print "Executing: '%s'" % " ".join(args)
     process = subprocess.Popen(args, stdout=subprocess.PIPE)
