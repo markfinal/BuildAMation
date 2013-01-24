d1958419ec554d56e6888b40df77e9068d6c7fcd
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-01-24 19:05:19 +0000

    [trunk] Removing old code from WindowsSDK-6.0A

--- packages/WindowsSDK/6.0A/WindowsSDK.cs
+++ packages/WindowsSDK/6.0A/WindowsSDK.cs
@@ -25,8 +25,6 @@ namespace WindowsSDK
             {
                 if (null == key)
                 {
-                    // NEW STYLE
-#if true
                     // TODO: do I want to hard code VisualC here?
                     Opus.Core.IToolset toolset = Opus.Core.State.Get("Toolset", "visualc") as Opus.Core.IToolset;
                     if (null == toolset)
@@ -52,27 +50,6 @@ namespace WindowsSDK
                     
                     lib32Path = System.IO.Path.Combine(installPath, "lib");
                     lib64Path = System.IO.Path.Combine(lib32Path, "AMD64");
-#else
-                    C.Toolchain tc = C.ToolchainFactory.GetTargetInstance(target);
-                    string platformSDKPath = System.IO.Path.Combine(tc.InstallPath(target), "PlatformSDK");
-
-                    if (System.IO.Directory.Exists(platformSDKPath))
-                    {
-                        installPath = platformSDKPath;
-                    }
-                    else
-                    {
-                        throw new Opus.Core.Exception("WindowsSDK 6.0A was not installed");
-                    }
-                    Opus.Core.Log.DebugMessage("Windows SDK installation folder is from the MSVC PlatformSDK: {0}", installPath);
-
-                    bin32Path = System.IO.Path.Combine(installPath, "bin");
-                    bin64Path = System.IO.Path.Combine(bin32Path, "win64");
-                    bin64Path = System.IO.Path.Combine(bin64Path, "AMD64");
-
-                    lib32Path = System.IO.Path.Combine(installPath, "lib");
-                    lib64Path = System.IO.Path.Combine(lib32Path, "AMD64");
-#endif
                 }
                 else
                 {
