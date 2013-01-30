e4f6538fe5f0b9cf8269c7319263b8bec85cb20c
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-01-30 20:18:13 +0000

    [trunk] Missed this file from the last commit

--- packages/Clang/3.1/Scripts/GenerateFiles.py
+++ packages/Clang/3.1/Scripts/GenerateFiles.py
@@ -2,6 +2,18 @@
 
 import os
 import sys
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-f", "--force", dest="force", action="store_true", default=False, help="Force writing")
+parser.add_option("-u", "--updateheader", dest="updateheader", action="store_true", default=False, help="Update headers")
+(options,args) = parser.parse_args()
+
+extra_args = []
+if options.force:
+  extra_args.append("-f")
+if options.updateheader:
+  extra_args.append("-uh")
 
 sys.path.append("../../../../python")
 from executeprocess import ExecuteProcess
@@ -20,6 +32,7 @@ cCompiler_options = [
     "-dd=" + os.path.relpath(os.path.join(opusPackageDir, "CommandLineProcessor", "dev", "Scripts", "CommandLineDelegate.cs")),
     "-pv=PrivateData"
 ]
+cCompiler_options.extend(extra_args)
 (stdout,stderr) = ExecuteProcess(cCompiler_options, True, True)
 print stdout
 
@@ -35,5 +48,6 @@ cxxCompiler_options = [
     "-pv=PrivateData",
     "-e" # this option set derives from the C option set
 ]
+cxxCompiler_options.extend(extra_args)
 (stdout,stderr) = ExecuteProcess(cxxCompiler_options, True, True)
 print stdout
