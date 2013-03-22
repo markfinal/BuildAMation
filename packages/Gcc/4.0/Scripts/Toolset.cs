e76d2bac0d94a227a7d26a7a8d9528cf204021d0
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-03-22 05:53:09 +0000

    [trunk] Updating GCC-4.4 with Objective C tools

--- packages/Gcc/4.0/Scripts/Toolset.cs
+++ packages/Gcc/4.0/Scripts/Toolset.cs
@@ -9,12 +9,12 @@ namespace Gcc
     {
         public Toolset()
         {
-            this.toolConfig[typeof(C.ICompilerTool)] = new Opus.Core.ToolAndOptionType(new CCompiler(this), typeof(CCompilerOptionCollection));
-            this.toolConfig[typeof(C.ICxxCompilerTool)] = new Opus.Core.ToolAndOptionType(new CxxCompiler(this), typeof(CxxCompilerOptionCollection));
-            this.toolConfig[typeof(C.IObjCCompilerTool)] = new Opus.Core.ToolAndOptionType(new CCompiler(this), typeof(GccCommon.ObjCCompilerOptionCollection));
+            this.toolConfig[typeof(C.ICompilerTool)]       = new Opus.Core.ToolAndOptionType(new CCompiler(this), typeof(CCompilerOptionCollection));
+            this.toolConfig[typeof(C.ICxxCompilerTool)]    = new Opus.Core.ToolAndOptionType(new CxxCompiler(this), typeof(CxxCompilerOptionCollection));
+            this.toolConfig[typeof(C.IObjCCompilerTool)]   = new Opus.Core.ToolAndOptionType(new CCompiler(this), typeof(GccCommon.ObjCCompilerOptionCollection));
             this.toolConfig[typeof(C.IObjCxxCompilerTool)] = new Opus.Core.ToolAndOptionType(new CxxCompiler(this), typeof(GccCommon.ObjCxxCompilerOptionCollection));
-            this.toolConfig[typeof(C.ILinkerTool)] = new Opus.Core.ToolAndOptionType(new Linker(this), typeof(LinkerOptionCollection));
-            this.toolConfig[typeof(C.IArchiverTool)] = new Opus.Core.ToolAndOptionType(new GccCommon.Archiver(this), typeof(ArchiverOptionCollection));
+            this.toolConfig[typeof(C.ILinkerTool)]         = new Opus.Core.ToolAndOptionType(new Linker(this), typeof(LinkerOptionCollection));
+            this.toolConfig[typeof(C.IArchiverTool)]       = new Opus.Core.ToolAndOptionType(new GccCommon.Archiver(this), typeof(ArchiverOptionCollection));
         }
     }
 }
