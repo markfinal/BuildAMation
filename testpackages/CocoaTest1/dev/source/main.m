9e0cd47237c6cdef3322df89ac9ce10fedc26c42
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-03-30 07:34:46 +0000

    [trunk] Extended CocoaTest1 to build an OSX application bundle and open a window in Objective C

--- testpackages/CocoaTest1/dev/source/main.m
+++ testpackages/CocoaTest1/dev/source/main.m
@@ -1,15 +1,60 @@
 #include <Cocoa/Cocoa.h>
 
+@interface MyApplicationDelegate : NSObject <NSApplicationDelegate, NSWindowDelegate>
+{
+    NSWindow* window;
+}
+@end
+
+@implementation MyApplicationDelegate : NSObject
+- (id)init
+{
+    if (self == [super init])
+    {
+        NSRect frame = NSMakeRect(0, 0, 200, 200);
+        window  = [[[NSWindow alloc] initWithContentRect:frame
+                                     styleMask:(NSTitledWindowMask|NSClosableWindowMask|NSMiniaturizableWindowMask|NSResizableWindowMask)
+                                     backing:NSBackingStoreBuffered /* supports GPU acceleration */
+                                     defer:NO] autorelease];
+        [window setBackgroundColor:[NSColor blueColor]];
+        [window setTitle:@"Hello world"];
+        [window center];
+    }
+    return self;
+}
+
+- (void)applicationWillFinishLaunching:(NSNotification *)notification
+{
+    (void)notification;
+    [window makeKeyAndOrderFront:self];
+}
+
+- (void)dealloc
+{
+    [window release];
+    [super dealloc];
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication {
+    (void)theApplication;
+    return YES;
+}
+
+@end
+
 int main(int argc, char *argv[])
 {
-    NSAutoreleasePool* pool =[[NSAutoreleasePool alloc] init];
-    [NSApplication sharedApplication];
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    NSApplication *application = [NSApplication sharedApplication];
 
+    MyApplicationDelegate *appDelegate = [[[MyApplicationDelegate alloc] init] autorelease];
     (void)argc;
     (void)argv;
-    NSRunAlertPanel(@"Testing Message Box",
-                    @"Hello, world!",
-                    @"OK", NULL, NULL);
-    [pool release];
-    return 0;
+
+    [application setDelegate:appDelegate];
+    [application run];
+
+    [pool drain];
+
+    return EXIT_SUCCESS;
 }
