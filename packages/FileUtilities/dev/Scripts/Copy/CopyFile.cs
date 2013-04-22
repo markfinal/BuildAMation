2f8ff5f87f62b7df9044fb4c43ca8f7d7ff13cd3
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-04-22 08:34:10 +0000

    [050copyfiles] CopyFile module now has a built-in source file, like C.ObjectFile

--- packages/FileUtilities/dev/Scripts/Copy/CopyFile.cs
+++ packages/FileUtilities/dev/Scripts/Copy/CopyFile.cs
@@ -31,6 +31,36 @@ namespace FileUtilities
     [Opus.Core.ModuleToolAssignment(typeof(ICopyFileTool))]
     public class CopyFile : Opus.Core.BaseModule
     {
+        public Opus.Core.File SourceFile
+        {
+            get;
+            private set;
+        }
+
+        public CopyFile()
+        {
+            this.SourceFile = new Opus.Core.File();
+        }
+
+        public void SetRelativePath(object owner, params string[] pathSegments)
+        {
+            this.SourceFile.SetRelativePath(owner, pathSegments);
+        }
+
+        public void SetPackageRelativePath(Opus.Core.PackageInformation package, params string[] pathSegments)
+        {
+            this.SourceFile.SetPackageRelativePath(package, pathSegments);
+        }
+
+        public void SetAbsolutePath(string absolutePath)
+        {
+            this.SourceFile.SetAbsolutePath(absolutePath);
+        }
+
+        public void SetGuaranteedAbsolutePath(string absolutePath)
+        {
+            this.SourceFile.SetGuaranteedAbsolutePath(absolutePath);
+        }
     }
 
     public class CopyFileOptionCollection : Opus.Core.BaseOptionCollection, CommandLineProcessor.ICommandLineSupport
