4b8e1e63793713cd025fbc1a795217044ab365a7
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-06-04 09:08:56 +0000

    [050copyfiles] Gcc-4.0 Objective C and C++ option sets also use the Gcc-4.0 C visibility interface.

---
+++ packages/Gcc/4.0/Scripts/ObjCCompilerOptionCollection.cs
@@ -0,0 +1,29 @@
+// <copyright file="ObjCCompilerOptionCollection.cs" company="Mark Final">
+//  Opus package
+// </copyright>
+// <summary>Gcc package</summary>
+// <author>Mark Final</author>
+namespace Gcc
+{
+    // Not sealed since the ObjC++ compiler inherits from it
+    public partial class ObjCCompilerOptionCollection : GccCommon.ObjCCompilerOptionCollection, ICCompilerOptions
+    {
+        public ObjCCompilerOptionCollection()
+            : base()
+        {
+        }
+
+        public ObjCCompilerOptionCollection(Opus.Core.DependencyNode node)
+            : base(node)
+        {
+        }
+
+        protected override void InitializeDefaults(Opus.Core.DependencyNode node)
+        {
+            base.InitializeDefaults(node);
+
+            // requires gcc 4.0
+            (this as ICCompilerOptions).Visibility = EVisibility.Hidden;
+        }
+    }
+}
