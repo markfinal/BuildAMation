9381367376d73f726ab9b50d0a9356608eee856d
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-06-12 22:33:08 +0000

    [050copyfiles] Adding Qt 4.7.1 QtXml module

---
+++ packages/QtCommon/dev/Scripts/QtXml.cs
@@ -0,0 +1,58 @@
+// <copyright file="QtXml.cs" company="Mark Final">
+//  Opus package
+// </copyright>
+// <summary>QtCommon package</summary>
+// <author>Mark Final</author>
+namespace QtCommon
+{
+    public abstract class Xml : Base
+    {
+        public Xml(System.Type toolsetType, bool includeModule)
+        {
+            this.ToolsetType = toolsetType;
+            this.IncludeModule = includeModule;
+
+            this.UpdateOptions += new Opus.Core.UpdateOptionCollectionDelegate(QtXml_IncludePaths);
+            this.UpdateOptions += new Opus.Core.UpdateOptionCollectionDelegate(QtXml_VisualCWarningLevel);
+            this.UpdateOptions += new Opus.Core.UpdateOptionCollectionDelegate(QtXml_LinkerOptions);
+        }
+
+        public override void RegisterOutputFiles(Opus.Core.BaseOptionCollection options, Opus.Core.Target target)
+        {
+            options.OutputPaths[C.OutputFileFlags.Executable] = this.GetModuleDynamicLibrary(this.ToolsetType, target, "QtXml");
+            base.RegisterOutputFiles(options, target);
+        }
+
+        [C.ExportLinkerOptionsDelegate]
+        void QtXml_LinkerOptions(Opus.Core.IModule module, Opus.Core.Target target)
+        {
+            var options = module.Options as C.ILinkerOptions;
+            if (null != options)
+            {
+                this.AddLibraryPath(this.ToolsetType, options, target);
+                this.AddModuleLibrary(options, target, "QtXml");
+            }
+        }
+
+        [C.ExportCompilerOptionsDelegate]
+        void QtXml_VisualCWarningLevel(Opus.Core.IModule module, Opus.Core.Target target)
+        {
+            var options = module.Options as VisualCCommon.ICCompilerOptions;
+            if (null != options)
+            {
+                // QtXml headers do not compile at warning level 4
+                options.WarningLevel = VisualCCommon.EWarningLevel.Level3;
+            }
+        }
+
+        [C.ExportCompilerOptionsDelegate]
+        void QtXml_IncludePaths(Opus.Core.IModule module, Opus.Core.Target target)
+        {
+            var options = module.Options as C.ICCompilerOptions;
+            if (null != options)
+            {
+                this.AddIncludePath(this.ToolsetType, options, target, "QtXml", this.IncludeModule);
+            }
+        }
+    }
+}
