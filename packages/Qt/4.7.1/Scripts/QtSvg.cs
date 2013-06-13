f79a66822a5ffd506179ae585cbd3e0fe8a43f61
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-06-13 08:44:30 +0000

    [050copyfiles] Adding Qt Svg module

---
+++ packages/Qt/4.7.1/Scripts/QtSvg.cs
@@ -0,0 +1,15 @@
+// <copyright file="QtSvg.cs" company="Mark Final">
+//  Opus package
+// </copyright>
+// <summary>Qt package</summary>
+// <author>Mark Final</author>
+namespace Qt
+{
+    public sealed class Svg : QtCommon.Svg
+    {
+        public Svg()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+}
