1b8f25e7255b006832159855fb20ac29485e9913
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-06-16 13:25:01 +0000

    [050copyfiles] Stubb[050copyfiles] Stubbing out FileUtilities VSSolution support

---
+++ packages/FileUtilities/dev/VSSolutionBuilder/CopyDirectory.cs
@@ -0,0 +1,17 @@
+// <copyright file="CopyDirectory.cs" company="Mark Final">
+//  Opus package
+// </copyright>
+// <summary>FileUtilities package</summary>
+// <author>Mark Final</author>
+namespace VSSolutionBuilder
+{
+    public sealed partial class VSSolutionBuilder
+    {
+        public object Build(FileUtilities.CopyDirectory module, out bool success)
+        {
+            Opus.Core.Log.MessageAll("TODO: Stub function for VSSolution support for {0}", module);
+            success = true;
+            return null;
+        }
+    }
+}
---
+++ packages/FileUtilities/dev/VSSolutionBuilder/SymlinkFile.cs
@@ -0,0 +1,17 @@
+// <copyright file="SymLinkFile.cs" company="Mark Final">
+//  Opus package
+// </copyright>
+// <summary>FileUtilities package</summary>
+// <author>Mark Final</author>
+namespace VSSolutionBuilder
+{
+    public sealed partial class VSSolutionBuilder
+    {
+        public object Build(FileUtilities.SymlinkFile module, out bool success)
+        {
+            Opus.Core.Log.MessageAll("TODO: Stub function for VSSolution support for {0}", module);
+            success = true;
+            return null;
+        }
+    }
+}
