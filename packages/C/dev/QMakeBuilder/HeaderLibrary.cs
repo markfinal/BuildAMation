8282b929fad7a3192bafb5d2ba8fb44d538a5341
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-07-10 20:44:38 +0000

    [050copyfiles] QMake support for header only libraries

--- packages/C/dev/QMakeBuilder/HeaderLibrary.cs
+++ packages/C/dev/QMakeBuilder/HeaderLibrary.cs
@@ -7,58 +7,29 @@ namespace QMakeBuilder
 {
     public sealed partial class QMakeBuilder
     {
-        public object Build(C.HeaderLibrary headerLibrary, out bool success)
+        public object Build(C.HeaderLibrary moduleToBuild, out bool success)
         {
-            var headerLibraryModule = headerLibrary as Opus.Core.BaseModule;
-            var node = headerLibraryModule.OwningNode;
+            var data = new QMakeData(moduleToBuild.OwningNode);
 
-            var nodeData = new NodeData();
-            nodeData.Configuration = GetQtConfiguration(node.Target);
-
-            var proFilePath = QMakeBuilder.GetProFilePath(node);
-            System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(proFilePath));
-            nodeData.ProFilePathName = proFilePath;
-
-            using (var proFileWriter = new System.IO.StreamWriter(proFilePath))
+            // find headers
+            var fieldBindingFlags = System.Reflection.BindingFlags.Instance |
+                                    System.Reflection.BindingFlags.Public |
+                                    System.Reflection.BindingFlags.NonPublic;
+            var fields = moduleToBuild.GetType().GetFields(fieldBindingFlags);
+            foreach (var field in fields)
             {
-                proFileWriter.WriteLine("# --- Written by Opus");
-
+                var headerFileAttributes = field.GetCustomAttributes(typeof(C.HeaderFilesAttribute), false);
+                if (headerFileAttributes.Length > 0)
                 {
-                    var relativePriPathName = Opus.Core.RelativePathUtilities.GetPath(this.DisableQtPriPathName, proFilePath);
-                    proFileWriter.WriteLine("include({0})", relativePriPathName.Replace('\\', '/'));
-                }
-
-                proFileWriter.WriteLine("TEMPLATE = subdirs");
-
-                // headers
-                {
-                    var fieldBindingFlags = System.Reflection.BindingFlags.Instance |
-                                            System.Reflection.BindingFlags.Public |
-                                            System.Reflection.BindingFlags.NonPublic;
-                    var fields = headerLibrary.GetType().GetFields(fieldBindingFlags);
-                    foreach (var field in fields)
-                    {
-                        var headerFileAttributes = field.GetCustomAttributes(typeof(C.HeaderFilesAttribute), false);
-                        if (headerFileAttributes.Length > 0)
-                        {
-                            var headerFileCollection = field.GetValue(headerLibrary) as Opus.Core.FileCollection;
-                            if (headerFileCollection.Count > 0)
-                            {
-                                var headersStatement = new System.Text.StringBuilder();
-                                headersStatement.AppendFormat("{0}:HEADERS += ", nodeData.Configuration);
-                                foreach (string headerPath in headerFileCollection)
-                                {
-                                    headersStatement.AppendFormat("\\\n\t{0}", headerPath.Replace('\\', '/'));
-                                }
-                                proFileWriter.WriteLine(headersStatement.ToString());
-                            }
-                        }
-                    }
+                    var headerFileCollection = field.GetValue(moduleToBuild) as Opus.Core.FileCollection;
+                    data.Headers.AddRangeUnique(headerFileCollection.ToStringArray());
                 }
             }
 
+            data.Output = QMakeData.OutputType.HeaderLibrary;
+
             success = true;
-            return nodeData;
+            return data;
         }
     }
 }
