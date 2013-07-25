109c276216404f6759bcc4759a7f8b635eb08f9c
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-07-25 13:08:07 +0000

    [locations] Reminder to try something

--- Opus.Core/Interfaces/IModuleCollection.cs
+++ Opus.Core/Interfaces/IModuleCollection.cs
@@ -8,6 +8,7 @@ namespace Opus.Core
     public interface IModuleCollection : INestedDependents
     {
 #if true
+        // TODO: consider moving this to BaseModule, because it should be applicable to ANY module
         void RegisterUpdateOptions(UpdateOptionCollectionDelegateArray delegateArray, Location root, params string[] pathSegments);
 #else
         IModule GetChildModule(object owner, params string[] pathSegments);
