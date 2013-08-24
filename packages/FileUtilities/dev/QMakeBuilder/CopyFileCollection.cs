b6a33b649bb3de78c5b9e43588066e4926a04c4a
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-08-24 19:25:02 +0000

    [trunk] Further QMake updates for FileUtilities CopyFile. CopyFileCollection no longer does anything. CopyFile now merges with any modules it needs to, and creates directories before copying.

--- packages/FileUtilities/dev/QMakeBuilder/CopyFileCollection.cs
+++ packages/FileUtilities/dev/QMakeBuilder/CopyFileCollection.cs
@@ -9,28 +9,6 @@ namespace QMakeBuilder
     {
         public object Build(FileUtilities.CopyFileCollection moduleToBuild, out bool success)
         {
-            var node = moduleToBuild.OwningNode;
-            var data = new QMakeData(node);
-            foreach (var child in node.Children)
-            {
-                var childData = child.Data as QMakeData;
-                if (null != childData)
-                {
-                    data.Merge(childData);
-                }
-            }
-            if (null != node.ExternalDependents)
-            {
-                foreach (var dependent in node.ExternalDependents)
-                {
-                    var depData = dependent.Data as QMakeData;
-                    if (null != depData)
-                    {
-                        data.Merge(depData);
-                    }
-                }
-            }
-
             success = true;
             return null;
         }
