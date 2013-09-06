8fdf741b88be3ba87a839221b4fa510c2ced07ab
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-09-06 19:35:48 +0000

    [xcode] ObjectFileCollections are now an empty function

--- packages/C/dev/XCodeBuilder/CObjectFileCollection.cs
+++ packages/C/dev/XCodeBuilder/CObjectFileCollection.cs
@@ -7,12 +7,10 @@ namespace XCodeBuilder
 {
     public sealed partial class XCodeBuilder
     {
+        [Opus.Core.EmptyBuildFunction]
         public object Build(C.ObjectFileCollectionBase moduleToBuild, out bool success)
         {
-            Opus.Core.Log.MessageAll("ObjectFileCollectionBase");
-            //var data = new XCodeNodeData();
             success = true;
-            //return data;
             return null;
         }
     }
