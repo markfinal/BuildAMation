412391638b986328ab733ac32f1197380399cb27
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-09-18 05:19:34 +0000

    [xcode] Split the OptionsDictionary out of XCBuildConfiguration to be reused elsewhere

--- packages/XcodeBuilder/dev/Scripts/XCBuildConfiguration.cs
+++ packages/XcodeBuilder/dev/Scripts/XCBuildConfiguration.cs
@@ -7,42 +7,6 @@ namespace XcodeBuilder
 {
     public sealed class XCBuildConfiguration : XCodeNodeData, IWriteableNode
     {
-        public class OptionsDictionary : System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string, Opus.Core.StringArray>>
-        {
-            private System.Collections.Generic.Dictionary<string, Opus.Core.StringArray> dictionary = new System.Collections.Generic.Dictionary<string, Opus.Core.StringArray>();
-
-            public Opus.Core.StringArray this[string key]
-            {
-                get
-                {
-                    if (!this.dictionary.ContainsKey(key))
-                    {
-                        this.dictionary[key] = new Opus.Core.StringArray();
-                    }
-
-                    return this.dictionary[key];
-                }
-            }
-
-            #region IEnumerable implementation
-
-            System.Collections.Generic.IEnumerator<System.Collections.Generic.KeyValuePair<string, Opus.Core.StringArray>> System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string, Opus.Core.StringArray>>.GetEnumerator ()
-            {
-                return this.dictionary.GetEnumerator();
-            }
-
-            #endregion
-
-            #region IEnumerable implementation
-
-            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-            {
-                return (this as System.Collections.IEnumerable).GetEnumerator();
-            }
-
-            #endregion
-        }
-
         public XCBuildConfiguration(string name, string moduleName)
             : base(name)
         {
