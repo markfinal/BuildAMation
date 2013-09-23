871809f67d08bb9216c38b0a12c99b1de6f78285
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-09-23 21:06:14 +0000

    [xcode] Stubbing out XmlUtilities to build a PList. This needs proper integration into the Xcode builder

---
+++ packages/XmlUtilities/dev/XcodeBuilder/XmlWriter.cs
@@ -0,0 +1,16 @@
+// <copyright file="XmlWriter.cs" company="Mark Final">
+//  Opus package
+// </copyright>
+// <summary>XmlUtilities package</summary>
+// <author>Mark Final</author>
+namespace XcodeBuilder
+{
+    public sealed partial class XcodeBuilder
+    {
+        public object Build(XmlUtilities.XmlModule moduleToBuild, out bool success)
+        {
+            success = true;
+            return null;
+        }
+    }
+}
