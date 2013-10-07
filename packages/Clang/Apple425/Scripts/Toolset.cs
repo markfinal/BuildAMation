93dc752d9b239165c0002a9f466b9af8025f0e19
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-10-07 21:16:43 +0000

    [trunk] Fake the linker and archiver with Gcc, so that clang can be used as a whole toolset for Apple

--- packages/Clang/Apple425/Scripts/Toolset.cs
+++ packages/Clang/Apple425/Scripts/Toolset.cs
@@ -7,6 +7,13 @@ namespace Clang
 {
     public sealed class Toolset : ClangCommon.Toolset
     {
+        public Toolset()
+            : base()
+        {
+            this.toolConfig[typeof(C.ILinkerTool)]   = new Opus.Core.ToolAndOptionType(new Gcc.Linker(this), typeof(Gcc.LinkerOptionCollection));
+            this.toolConfig[typeof(C.IArchiverTool)] = new Opus.Core.ToolAndOptionType(new GccCommon.Archiver(this), typeof(Gcc.ArchiverOptionCollection));
+        }
+
         protected override string SpecificVersion (Opus.Core.BaseTarget baseTarget)
         {
             return "Apple425";
