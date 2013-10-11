3c8d18fd2f6d4af4e81744c9742f6009aaf9bc4e
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-10-11 12:56:34 +0000

    [trunk] Excluded source file names are now per project

--- packages/XcodeBuilder/dev/Scripts/PostExecute.cs
+++ packages/XcodeBuilder/dev/Scripts/PostExecute.cs
@@ -17,33 +17,33 @@ namespace XcodeBuilder
                 return;
             }
 
-            // handle any source file exclusions by diffing the source files attached to configurations
-            // which those in the native target
-            foreach (var target in this.Project.NativeTargets)
-            {
-                var nativeTarget = target as PBXNativeTarget;
-                var allSource = nativeTarget.SourceFilesToBuild;
-                foreach (var config in nativeTarget.BuildConfigurationList.BuildConfigurations)
-                {
-                    var buildConfig = config as XCBuildConfiguration;
-                    if (0 == buildConfig.SourceFiles.Count)
-                    {
-                        continue;
-                    }
-
-                    var complement = allSource.Complement(buildConfig.SourceFiles);
-                    foreach (var source in complement)
-                    {
-                        buildConfig.Options["EXCLUDED_SOURCE_FILE_NAMES"].AddUnique(source.FileReference.ShortPath);
-                    }
-                }
-            }
-
             // cannot write a Byte-Ordering-Mark (BOM) into the project file
             var encoding = new System.Text.UTF8Encoding(false);
 
             foreach (var project in this.Workspace.Projects)
             {
+                // handle any source file exclusions by diffing the source files attached to configurations
+                // which those in the native target
+                foreach (var target in project.NativeTargets)
+                {
+                    var nativeTarget = target as PBXNativeTarget;
+                    var allSource = nativeTarget.SourceFilesToBuild;
+                    foreach (var config in nativeTarget.BuildConfigurationList.BuildConfigurations)
+                    {
+                        var buildConfig = config as XCBuildConfiguration;
+                        if (0 == buildConfig.SourceFiles.Count)
+                        {
+                            continue;
+                        }
+
+                        var complement = allSource.Complement(buildConfig.SourceFiles);
+                        foreach (var source in complement)
+                        {
+                            buildConfig.Options["EXCLUDED_SOURCE_FILE_NAMES"].AddUnique(source.FileReference.ShortPath);
+                        }
+                    }
+                }
+
                 if (!System.IO.Directory.Exists(project.RootUri.AbsolutePath))
                 {
                     System.IO.Directory.CreateDirectory(project.RootUri.AbsolutePath);
