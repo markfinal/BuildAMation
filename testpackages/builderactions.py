dfb07f8a513c2fcf51112589fb7d705659822250
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-10-21 12:41:37 +0000

    [trunk] Running Xcode post actions using the workspace generated. Does have an impact of opening up the Xcode UI in order to cache the project data

--- testpackages/builderactions.py
+++ testpackages/builderactions.py
@@ -71,35 +71,99 @@ def MakeFilePost(package, options, outputMessages, errorMessages):
         return -1
     return exitCode
 
+def _openXcodeWorkspaceInIDE(workspacePath, outputMessages, errorMessages):
+    """Open the Xcode workspace into the IDE. This will cache all of the project data, allowing xcodebuild command line invocations to function."""
+    argList = []
+    argList.append("open")
+    argList.append("-a")
+    argList.append("xcode")
+    argList.append(workspacePath)
+    p = subprocess.Popen(argList, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    import time
+    time.sleep(5) # TODO: this is bad, need to find a better way - Popen only waits for Xcode to start up, not for it to finish parsing the project
+    (outputStream, errorStream) = p.communicate() # this should WAIT
+    outputMessages.write(outputStream)
+    errorMessages.write(errorStream)
+
+def _writeXcodeWorkspaceSettings(workspacePath, outputMessages, errorMessages):
+    """Write the default settings for the current user for the workspace."""
+    realSettingsFilePath = os.path.join(workspacePath, "xcuserdata", "%s.xcuserdatad"%os.environ["USER"],"WorkspaceSettings.xcsettings")
+    tempSettingsFilePath = realSettingsFilePath + ".plist"
+    argList = []
+    argList.append("defaults")
+    argList.append("write")
+    argList.append(tempSettingsFilePath)
+    # build location style must use legacy settings currently, in order to locate dependents
+    argList.append("BuildLocationStyle")
+    argList.append("UseTargetSettings")
+    p = subprocess.Popen(argList, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (outputStream, errorStream) = p.communicate() # this should WAIT
+    outputMessages.write(outputStream)
+    errorMessages.write(errorStream)
+    import shutil
+    shutil.copyfile(tempSettingsFilePath, realSettingsFilePath)
+    # TODO: this next statement causes failures with subsequent builds of the same package
+    #os.remove(tempSettingsFilePath)
+
 def XcodePost(package, options, outputMessages, errorMessages):
     """Post action for testing the Xcode builder"""
     exitCode = 0
     buildRoot = os.path.join(package.GetPath(), options.buildRoot)
-    xcodeProjectPath = os.path.join(buildRoot, package.GetName() + ".xcodeproj")
-    if not os.path.exists(xcodeProjectPath):
+    xcodeWorkspacePath = os.path.join(buildRoot, "*.xcworkspace")
+    import glob
+    workspaces = glob.glob(xcodeWorkspacePath)
+    if not workspaces:
         # TODO: really need something different here - an invalid test result, rather than a failure
-        outputMessages.write("Xcode project expected at %s did not exist" % xcodeProjectPath)
+        outputMessages.write("Xcode workspace expected at %s did not exist" % xcodeWorkspacePath)
         return 0
+    if len(workspaces) > 1:
+        outputMessages.write("More than one Xcode workspace was found")
+        return -1
     try:
-        # TODO: to execute this on a workspace, do something like this
-        # This is because Opus generated projects do not build their "default" setup and must resort to legacy (which doesn't really sound that legacy)
-        # xcodebuild -workspace Test5.xcworkspace -scheme MyDynamicLibTestApp BuildLocationStyle=UseTargetSettings
-        for config in options.configurations:
-            argList = []
-            argList.append("xcodebuild")
-            argList.append("-alltargets")
-            argList.append("-configuration")
-            # capitalize the first letter of the configuration
-            config = config[0].upper() + config[1:]
-            argList.append(config)
-            print "Running '%s' in %s" % (" ".join(argList), buildRoot)
-            p = subprocess.Popen(argList, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=buildRoot)
-            (outputStream, errorStream) = p.communicate() # this should WAIT
-            exitCode |= p.returncode
-            if outputStream:
-                outputMessages.write(outputStream)
-            if errorStream:
-                errorMessages.write(errorStream)
+        _writeXcodeWorkspaceSettings(workspaces[0], outputMessages, errorMessages)
+        _openXcodeWorkspaceInIDE(workspaces[0], outputMessages, errorMessages)
+        # first, list all the schemes available
+        argList = []
+        argList.append("xcodebuild")
+        argList.append("-workspace")
+        argList.append(workspaces[0])
+        argList.append("-list")
+        p = subprocess.Popen(argList, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (outputStream, errorStream) = p.communicate() # this should WAIT
+        outputMessages.write("executing %s\n" % ' '.join(argList))
+        outputMessages.write(outputStream)
+        errorMessages.write(errorStream)
+        # parse the output to get the schemes
+        lines = outputStream.split()
+        schemes = []
+        hasSchemes = False
+        for line in lines:
+            trimmed = line.strip()
+            if hasSchemes:
+                schemes.append(trimmed)
+            elif trimmed.startswith('Schemes:'):
+                hasSchemes = True
+        # iterate over all the schemes and configurations
+        for scheme in schemes:
+            for config in options.configurations:
+                argList = []
+                argList.append("xcodebuild")
+                argList.append("-workspace")
+                argList.append(workspaces[0])
+                argList.append("-scheme")
+                argList.append(scheme)
+                argList.append("-configuration")
+                # capitalize the first letter of the configuration
+                config = config[0].upper() + config[1:]
+                argList.append(config)
+                outputMessages.write("Running '%s' in %s" % (" ".join(argList), buildRoot))
+                p = subprocess.Popen(argList, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=buildRoot)
+                (outputStream, errorStream) = p.communicate() # this should WAIT
+                exitCode |= p.returncode
+                if outputStream:
+                    outputMessages.write(outputStream)
+                if errorStream:
+                    errorMessages.write(errorStream)
     except Exception, e:
         errorMessages.write(str(e))
         return -1
