1d921e8408cb5ca9e48a9cfeee92f6c9bc3183ec
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-11-01 21:31:17 +0000

    [trunk] Simplified the source layout of Qt-4.8.5

---
+++ packages/Qt/4.8.5/Scripts/Modules.cs
@@ -0,0 +1,167 @@
+// <copyright file="Modules.cs" company="Mark Final">
+//  Opus package
+// </copyright>
+// <summary>Qt package</summary>
+// <author>Mark Final</author>
+namespace Qt
+{
+    public sealed class Phonon : QtCommon.Phonon
+    {
+        public Phonon()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class Core : QtCommon.Core
+    {
+        public Core()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class DBus : QtCommon.DBus
+    {
+        public DBus()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class Declarative : QtCommon.Declarative
+    {
+        public Declarative()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class Designer : QtCommon.Designer
+    {
+        public Designer()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class Gui : QtCommon.Gui
+    {
+        public Gui()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class Help : QtCommon.Help
+    {
+        public Help()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class Multimedia : QtCommon.Multimedia
+    {
+        public Multimedia()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class Network : QtCommon.Network
+    {
+        public Network()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class OpenGL : QtCommon.OpenGL
+    {
+        public OpenGL()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class OpenVG : QtCommon.OpenVG
+    {
+        public OpenVG()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class Script : QtCommon.Script
+    {
+        public Script()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class ScriptTools : QtCommon.ScriptTools
+    {
+        public ScriptTools()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class Sql : QtCommon.Sql
+    {
+        public Sql()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class Svg : QtCommon.Svg
+    {
+        public Svg()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class Test : QtCommon.Test
+    {
+        public Test()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class UiTools : QtCommon.UiTools
+    {
+        public UiTools()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class WebKit : QtCommon.WebKit
+    {
+        public WebKit()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class Xml : QtCommon.Xml
+    {
+        public Xml()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+
+    public sealed class XmlPatterns : QtCommon.XmlPatterns
+    {
+        public XmlPatterns()
+            : base(typeof(Qt.Toolset), false)
+        {
+        }
+    }
+}
