a68e2926885e7fd3edcebf9ef999236a4181010a
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-11-02 18:49:47 +0000

    [trunk] Removed obsolete code from Opus.Core.File

--- Opus.Core/File.cs
+++ Opus.Core/File.cs
@@ -16,59 +16,6 @@ namespace Opus.Core
             set;
         }
 
-        private static bool ContainsDirectorySeparators(string pathSegment)
-        {
-            var containsSeparators = pathSegment.Contains(DirectorySeparatorString) || pathSegment.Contains(AltDirectorySeparatorString);
-            return containsSeparators;
-        }
-
-        private static void ValidateFilePart(string pathSegment)
-        {
-            if (ContainsDirectorySeparators(pathSegment))
-            {
-                throw new Exception("Any file part cannot contain a directory separators; '{0}'", pathSegment);
-            }
-        }
-
-        internal static string CombinePaths(ref string baseDirectory, params string[] pathSegments)
-        {
-            var combinedPath = baseDirectory;
-            var canExtendBaseDirectoryWithUps = true;
-            for (int i = 0; i < pathSegments.Length; ++i)
-            {
-                ValidateFilePart(pathSegments[i]);
-                if (null != combinedPath)
-                {
-                    if (pathSegments[i] == "..")
-                    {
-                        if (canExtendBaseDirectoryWithUps)
-                        {
-                            baseDirectory = System.IO.Path.Combine(baseDirectory, pathSegments[i]);
-                            continue;
-                        }
-                    }
-                    else
-                    {
-                        canExtendBaseDirectoryWithUps = false;
-                    }
-
-                    combinedPath = System.IO.Path.Combine(combinedPath, pathSegments[i]);
-                }
-                else
-                {
-                    combinedPath = pathSegments[i];
-                }
-            }
-
-            return combinedPath;
-        }
-
-        public static string CanonicalPath(string path)
-        {
-            var canonicalPath = System.IO.Path.GetFullPath(new System.Uri(path).LocalPath);
-            return canonicalPath;
-        }
-
         public void Include(Location baseLocation, string pattern, ScaffoldLocation.ETypeHint typeHint)
         {
             var location = new ScaffoldLocation(baseLocation, pattern, typeHint);
@@ -78,27 +25,6 @@ namespace Opus.Core
         public void Include(Location baseLocation, string pattern)
         {
             this.Include(baseLocation, pattern, ScaffoldLocation.ETypeHint.File);
-        }
-
-        private static bool IsFileInHiddenHierarchy(System.IO.FileInfo file, string rootDir)
-        {
-            if (System.IO.FileAttributes.Hidden == (file.Attributes & System.IO.FileAttributes.Hidden))
-            {
-                return true;
-            }
-
-            var dirInfo = file.Directory;
-            while (dirInfo.FullName != rootDir)
-            {
-                if (System.IO.FileAttributes.Hidden == (dirInfo.Attributes & System.IO.FileAttributes.Hidden))
-                {
-                    return true;
-                }
-
-                dirInfo = dirInfo.Parent;
-            }
-
-            return false;
         }
 
         public string AbsolutePath
