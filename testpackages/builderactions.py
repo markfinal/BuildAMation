980d84df1433c2a23d9b604731dee7922c58d950
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2013-11-02 15:08:59 +0000

    [trunk] Dial down the MSBUILD output to just normal

--- testpackages/builderactions.py
+++ testpackages/builderactions.py
@@ -37,7 +37,7 @@ def VSSolutionPost(package, options, outputMessages, errorMessages):
         for config in options.configurations:
             argList = []
             argList.append(msBuildPath)
-            argList.append("/verbosity:detailed");
+            argList.append("/verbosity:normal");
             argList.append(slnPath)
             # capitalize the first letter of the configuration
             config = config[0].upper() + config[1:]
