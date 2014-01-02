43b28ca26fec6449c950af93369eb76312696b38
markfinal@hotmail.com <markfinal@hotmail.com@b1343a59-161a-32a7-69ea-2fa55fcaa4e1>
2014-01-02 08:45:50 +0000

    [trunk] Updated copyrights to 2014

--- Opus/Properties/AssemblyInfo.cs
+++ Opus/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Opus")]
-[assembly: AssemblyCopyright("Copyright 2011-2013")]
+[assembly: AssemblyCopyright("Copyright 2011-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- OpusOptionCodeGenerator/Properties/AssemblyInfo.cs
+++ OpusOptionCodeGenerator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OpusOptionCodeGenerator")]
-[assembly: AssemblyCopyright("Copyright 2011-2013")]
+[assembly: AssemblyCopyright("Copyright 2011-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
