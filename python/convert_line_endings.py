1b21da62f81d5f65dc7b0f16fdf999d5182484b4
Mark Final <markfinal@hotmail.com>
2014-05-13 18:31:23 +0100

    [trunk] Fixed tabbing, and an errant return statement

--- python/convert_line_endings.py
+++ python/convert_line_endings.py
@@ -4,26 +4,26 @@ import os
 import sys
 
 def convert_line_endings(file):
-  if '\r\n' in open(file, 'rb').read():
-    print '%s contains DOS line endings. Converting' % file
-    with open(file, 'rb') as infile:
-      text = infile.read()
-      text = text.replace('\r\n', '\n')
-    with open(file, 'wb') as outfile:
-      outfile.write(text)
+    if '\r\n' in open(file, 'rb').read():
+        print '%s contains DOS line endings. Converting' % file
+        with open(file, 'rb') as infile:
+            text = infile.read()
+            text = text.replace('\r\n', '\n')
+        with open(file, 'wb') as outfile:
+            outfile.write(text)
 
 def processPath(dirPath, ext):
-  for dirpath, dirnames, filenames in os.walk(dirPath):
-    for file in filenames:
-      if os.path.splitext(file)[1] == ext:
-        csPath = os.path.join(dirpath, file)
-        convert_line_endings(csPath)
+    for dirpath, dirnames, filenames in os.walk(dirPath):
+        for file in filenames:
+            if os.path.splitext(file)[1] == ext:
+                csPath = os.path.join(dirpath, file)
+                convert_line_endings(csPath)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     convert_line_endings(sys.argv[1])
-    return
-  processPath('.', '.cs')
-  processPath('testpackages', '.h')
-  processPath('testpackages', '.c')
-  processPath('testpackages', '.cpp')
+  else:
+    processPath('.', '.cs')
+    processPath('testpackages', '.h')
+    processPath('testpackages', '.c')
+    processPath('testpackages', '.cpp')
