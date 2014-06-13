4ddd3bf7b43d7cb4b3c1434b0dea643e0a49ee30
Mark Final <markfinal@hotmail.com>
2014-06-13 18:35:25 +0100

    [trunk] VSSolution support for publishing. Dependencies are copied next to the primary module

--- packages/Publisher/dev/VSSolutionBuilder/ProductModule.cs
+++ packages/Publisher/dev/VSSolutionBuilder/ProductModule.cs
@@ -7,7 +7,11 @@ namespace VSSolutionBuilder
 {
     public sealed partial class VSSolutionBuilder
     {
-        private static void CopyNodes(IProject toProject, Opus.Core.DependencyNode toCopy)
+        private static void
+        CopyNodes(
+            Publisher.ProductModule moduleToBuild,
+            IProject toProject,
+            Opus.Core.DependencyNode toCopy)
         {
             var configCollection = toProject.Configurations;
             // TODO: this should be done from the base target!
@@ -22,13 +26,19 @@ namespace VSSolutionBuilder
                 configuration.AddToolIfMissing(vcPostBuildEventTool);
             }
 
+            // TODO: the key needs to be on an options interface or similiar
             var sourceLoc = toCopy.Module.Locations[C.Application.OutputFile];
             var sourcePath = sourceLoc.GetSingleRawPath();
+
             var destinationDir = configuration.OutputDirectory;
+            var destinationDirPath = destinationDir.GetSingleRawPath();
+
+            var newKeyName = Publisher.ProductModuleUtilities.GetPublishedKeyName(toCopy.Module, toCopy.Module, C.Application.OutputFile);
+            var primaryKey = new Opus.Core.LocationKey(newKeyName, Opus.Core.ScaffoldLocation.ETypeHint.File);
+            var destPath = Publisher.ProductModuleUtilities.GenerateDestinationPath(sourcePath, destinationDirPath, moduleToBuild, primaryKey);
 
             var commandLine = new System.Text.StringBuilder();
-            commandLine.AppendFormat("IF NOT EXIST \"{0}\" MKDIR \"{0}\"{1}", destinationDir, System.Environment.NewLine);
-            commandLine.AppendFormat("cmd.exe /c COPY \"{0}\" \"{1}\"{2}", sourcePath, destinationDir, System.Environment.NewLine);
+            commandLine.AppendFormat("cmd.exe /c COPY \"{0}\" \"{1}\"{2}", sourcePath, destPath, System.Environment.NewLine);
 
             {
                 string attributeName = null;
@@ -57,11 +67,39 @@ namespace VSSolutionBuilder
             }
         }
 
-        public object Build(Publisher.ProductModule moduleToBuild, out bool success)
+        public object
+        Build(
+            Publisher.ProductModule moduleToBuild,
+            out bool success)
         {
             var primaryNode = Publisher.ProductModuleUtilities.GetPrimaryNode(moduleToBuild);
             var projectData = primaryNode.Data as IProject;
-            CopyNodes(projectData, primaryNode);
+
+            foreach (var dependency in primaryNode.ExternalDependents)
+            {
+                var module = dependency.Module;
+                var moduleType = module.GetType();
+                var flags = System.Reflection.BindingFlags.Instance |
+                            System.Reflection.BindingFlags.NonPublic;
+                var fields = moduleType.GetFields(flags);
+                foreach (var field in fields)
+                {
+                    var candidates = field.GetCustomAttributes(typeof(Publisher.PublishModuleDependencyAttribute), false);
+                    if (0 == candidates.Length)
+                    {
+                        continue;
+                    }
+                    if (candidates.Length > 1)
+                    {
+                        throw new Opus.Core.Exception("More than one publish module dependency found");
+                    }
+                    var candidateData = field.GetValue(module) as Opus.Core.Array<Opus.Core.LocationKey>;
+                    foreach (var key in candidateData)
+                    {
+                        CopyNodes(moduleToBuild, projectData, module.OwningNode);
+                    }
+                }
+            }
 
             success = true;
             return null;
