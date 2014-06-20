028a80cd9348d0dfedc734166993242d93becf38
Mark Final <markfinal@hotmail.com>
2014-06-20 19:10:33 +0100

    [trunk] Empty build rule for Xcode posix shared library symlinks

---
+++ packages/C/dev/XCodeBuilder/PosixSharedLibrarySymlinks.cs
@@ -0,0 +1,20 @@
+// <copyright file="PosixSharedLibrarySymlinks.cs" company="Mark Final">
+//  Opus package
+// </copyright>
+// <summary>C package</summary>
+// <author>Mark Final</author>
+namespace XcodeBuilder
+{
+    public sealed partial class XcodeBuilder
+    {
+        [Opus.Core.EmptyBuildFunction]
+        public object
+        Build(
+            C.PosixSharedLibrarySymlinks moduleToBuild,
+            out bool success)
+        {
+            success = true;
+            return null;
+        }
+    }
+}
