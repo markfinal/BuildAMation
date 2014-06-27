3296fcd7963874f584e4bccc28e82ae6e0e28535
Mark Final <markfinal@hotmail.com>
2014-06-27 07:31:25 +0100

    [trunk] Adding properties to the shell script build phase

--- packages/XcodeBuilder/dev/Scripts/PBXShellScriptBuildPhase.cs
+++ packages/XcodeBuilder/dev/Scripts/PBXShellScriptBuildPhase.cs
@@ -10,6 +10,34 @@ namespace XcodeBuilder
         public PBXShellScriptBuildPhase(string name, string moduleName)
             : base(name, moduleName)
         {
+            this.InputPaths = new Opus.Core.StringArray();
+            this.OutputPaths = new Opus.Core.StringArray();
+            this.ShellPath = "/bin/sh";
+            this.ShellScriptLines = new Opus.Core.StringArray();
+        }
+
+        public Opus.Core.StringArray InputPaths
+        {
+            get;
+            private set;
+        }
+
+        public Opus.Core.StringArray OutputPaths
+        {
+            get;
+            private set;
+        }
+
+        public string ShellPath
+        {
+            get;
+            set;
+        }
+
+        public Opus.Core.StringArray ShellScriptLines
+        {
+            get;
+            private set;
         }
 
 #region IWriteableNode implementation
@@ -26,7 +54,26 @@ namespace XcodeBuilder
                 writer.WriteLine("\t\t\t\t{0} /* {1} in {2} */,", file.UUID, System.IO.Path.GetFileName(buildFile.FileReference.ShortPath), buildFile.BuildPhase.Name);
             }
             writer.WriteLine("\t\t\t);");
+            writer.WriteLine("\t\t\tinputPaths = (");
+            foreach (var path in this.InputPaths)
+            {
+                writer.WriteLine("\t\t\t\t\"{0}\",", path);
+            }
+            writer.WriteLine("\t\t\t);");
+            writer.WriteLine("\t\t\toutputPaths = (");
+            foreach (var path in this.OutputPaths)
+            {
+                writer.WriteLine("\t\t\t\t\"{0}\",", path);
+            }
+            writer.WriteLine("\t\t\t);");
             writer.WriteLine("\t\t\trunOnlyForDeploymentPostprocessing = 0;");
+            writer.WriteLine("\t\t\tshellPath = {0};", this.ShellPath);
+            var script = new System.Text.StringBuilder();
+            foreach (var line in this.ShellScriptLines)
+            {
+                script.AppendFormat("{0}\n", line);
+            }
+            writer.WriteLine("\t\t\tshellScript = \"{0}\";", script.ToString());
             writer.WriteLine("\t\t};");
         }
 
