ab9062afd9eee460cda2f6d7a15eea8953f09edf
Mark Final <markfinal@hotmail.com>
2014-07-17 13:53:45 +0100

    [trunk] Stubbing out the MakeFile support for publisher

--- packages/Publisher/dev/MakeFileBuilder/ProductModule.cs
+++ packages/Publisher/dev/MakeFileBuilder/ProductModule.cs
@@ -7,6 +7,248 @@ namespace MakeFileBuilder
 {
     public sealed partial class MakeFileBuilder
     {
+#if true
+        private void
+        CopyNodeLocation(
+            Publisher.ProductModule moduleToBuild,
+            Opus.Core.BaseModule moduleToCopy,
+            Opus.Core.LocationKey sourceKey,
+            Publisher.CopyFileLocationsAttribute attribute,
+            string metaSubDirectory,
+            Opus.Core.BaseModule primaryModule,
+            string publishDirPath)
+        {
+#if true
+#else
+            var moduleLocations = moduleToCopy.Locations;
+
+            if (!moduleLocations.Contains(sourceKey))
+            {
+                return;
+            }
+
+            var sourceLoc = moduleLocations[sourceKey];
+            if (!sourceLoc.IsValid)
+            {
+                return;
+            }
+
+            // take the common subdirectory by default, otherwise override on a per Location basis
+            var subDirectory = attribute.CommonSubDirectory;
+            if (!string.IsNullOrEmpty(metaSubDirectory))
+            {
+                subDirectory = metaSubDirectory;
+            }
+
+            var publishedKeyName = Publisher.ProductModuleUtilities.GetPublishedKeyName(
+                primaryModule,
+                moduleToCopy,
+                sourceKey);
+
+            if (sourceKey.IsFileKey)
+            {
+                var publishedKey = new Opus.Core.LocationKey(publishedKeyName, Opus.Core.ScaffoldLocation.ETypeHint.File);
+                Publisher.ProductModuleUtilities.CopyFileToLocation(
+                    sourceLoc,
+                    publishDirPath,
+                    subDirectory,
+                    moduleToBuild,
+                    publishedKey);
+            }
+            else if (sourceKey.IsSymlinkKey)
+            {
+                var publishedKey = new Opus.Core.LocationKey(publishedKeyName, Opus.Core.ScaffoldLocation.ETypeHint.Symlink);
+                Publisher.ProductModuleUtilities.CopySymlinkToLocation(
+                    sourceLoc,
+                    publishDirPath,
+                    subDirectory,
+                    moduleToBuild,
+                    publishedKey);
+            }
+            else if (sourceKey.IsDirectoryKey)
+            {
+                throw new Opus.Core.Exception("Directories cannot be published yet");
+            }
+            else
+            {
+                throw new Opus.Core.Exception("Unsupported Location type");
+            }
+#endif
+        }
+
+        private void
+        CopyAdditionalDirectory(
+            Opus.Core.BaseModule moduleToBuild,
+            Opus.Core.BaseModule primaryModule,
+            Publisher.PublishDirectory directoryInfo,
+            Publisher.AdditionalDirectoriesAttribute attribute,
+            string publishDirPath)
+        {
+#if true
+#else
+            var publishedKeyName = Publisher.ProductModuleUtilities.GetPublishedAdditionalDirectoryKeyName(
+                primaryModule,
+                directoryInfo.Directory);
+            var publishedKey = new Opus.Core.LocationKey(publishedKeyName, Opus.Core.ScaffoldLocation.ETypeHint.Directory);
+            var sourceLoc = directoryInfo.DirectoryLocation;
+            var subdirectory = attribute.CommonSubDirectory;
+            Publisher.ProductModuleUtilities.CopyDirectoryToLocation(
+                sourceLoc,
+                publishDirPath,
+                subdirectory,
+                directoryInfo.RenamedLeaf,
+                moduleToBuild,
+                publishedKey);
+#endif
+        }
+
+        private void
+        CopyInfoPList(
+            Opus.Core.BaseModule moduleToBuild,
+            Opus.Core.BaseModule primaryModule,
+            Opus.Core.BaseModule moduleToCopy,
+            Opus.Core.LocationKey keyToCopy)
+        {
+#if true
+#else
+            var publishedKeyName = Publisher.ProductModuleUtilities.GetPublishedKeyName(
+                primaryModule,
+                moduleToCopy,
+                keyToCopy);
+            var publishedKey = new Opus.Core.LocationKey(publishedKeyName, Opus.Core.ScaffoldLocation.ETypeHint.File);
+            var contentsLoc = moduleToBuild.Locations[Publisher.ProductModule.OSXAppBundleContents].GetSingleRawPath();
+            var plistSourceLoc = moduleToCopy.Locations[keyToCopy];
+            Publisher.ProductModuleUtilities.CopyFileToLocation(
+                plistSourceLoc,
+                contentsLoc,
+                string.Empty,
+                moduleToBuild,
+                publishedKey);
+#endif
+        }
+
+        public object
+        Build(
+            Publisher.ProductModule moduleToBuild,
+            out bool success)
+        {
+            var dirsToCreate = moduleToBuild.Locations.FilterByType(Opus.Core.ScaffoldLocation.ETypeHint.Directory, Opus.Core.Location.EExists.WillExist);
+            var locationMap = moduleToBuild.Locations;
+
+            // Native build publishes to a new location away from the existing module builds
+            var publishDirLoc = locationMap[Publisher.ProductModule.PublishDir];
+            var publishDirPath = publishDirLoc.GetSingleRawPath();
+
+            var primaryNode = Publisher.ProductModuleUtilities.GetPrimaryTarget(moduleToBuild);
+            if (null == primaryNode)
+            {
+                throw new Opus.Core.Exception("Unable to locate the primary target for publishing");
+            }
+            var primaryModule = primaryNode.Module;
+
+            // gather all nodes that will be considered for publication
+            var nodesToPublish = new Opus.Core.DependencyNodeCollection();
+            nodesToPublish.Add(primaryNode);
+            nodesToPublish.Add(moduleToBuild.OwningNode);
+            if (null != primaryNode.ExternalDependents)
+            {
+                nodesToPublish.AddRange(primaryNode.ExternalDependents);
+            }
+            if (null != primaryNode.RequiredDependents)
+            {
+                nodesToPublish.AddRange(primaryNode.RequiredDependents);
+            }
+
+            // gather up the publishing metadata for those nodes
+            var metaData = Publisher.ProductModuleUtilities.GetPublishingMetaData(moduleToBuild.OwningNode.Target, nodesToPublish);
+
+            // for built files to copy...
+            var copyFiles = metaData.FilterByType<Publisher.CopyFileLocationsAttribute>();
+            // TODO: convert to var
+            foreach (Publisher.ProductModuleUtilities.MetaData meta in copyFiles)
+            {
+                var nodeData = meta.Data as Opus.Core.Array<Publisher.PublishDependency>;
+                if (null == nodeData)
+                {
+                    throw new Opus.Core.Exception("Meta data '{0}' in '{1}' was of unexpected type '{2}'. Expected '{3}'",
+                        meta.Name, meta.Node.UniqueModuleName, meta.Data.GetType().ToString(), typeof(Opus.Core.Array<Publisher.PublishDependency>).ToString());
+                }
+
+                var module = meta.Node.Module;
+                var moduleLocations = module.Locations;
+                foreach (var node in nodeData)
+                {
+                    if (node.SubDirectory != null)
+                    {
+                        Opus.Core.Log.MessageAll("Copy file '{0}' : '{1}' -> {2}/{3}", meta.Node.UniqueModuleName, node.Key.ToString(), publishDirPath, node.SubDirectory);
+                    }
+                    else
+                    {
+                        Opus.Core.Log.MessageAll("Copy file '{0}' : '{1}' -> {2}", meta.Node.UniqueModuleName, node.Key.ToString(), publishDirPath);
+                    }
+
+                    this.CopyNodeLocation(
+                        moduleToBuild,
+                        module,
+                        node.Key,
+                        meta.Attribute as Publisher.CopyFileLocationsAttribute,
+                        node.SubDirectory,
+                        primaryModule,
+                        publishDirPath);
+                }
+            }
+
+            // for additional files already on disk...
+            var additionalDirs = metaData.FilterByType<Publisher.AdditionalDirectoriesAttribute>();
+            // TODO: convert to var
+            foreach (Publisher.ProductModuleUtilities.MetaData meta in additionalDirs)
+            {
+                var dirData = meta.Data as Publisher.PublishDirectory;
+                if (null == dirData)
+                {
+                    throw new Opus.Core.Exception("Meta data '{0}' in '{1}' was of unexpected type '{2}'. Expected '{3}'",
+                        meta.Name, meta.Node.UniqueModuleName, meta.Data.GetType().ToString(), typeof(Publisher.PublishDirectory).ToString());
+                }
+
+                Opus.Core.Log.MessageAll("Additional dir '{0}' : '{1}' -> '{2}'", meta.Node.UniqueModuleName, dirData.Directory, publishDirPath);
+                this.CopyAdditionalDirectory(
+                    moduleToBuild,
+                    primaryModule,
+                    dirData,
+                    meta.Attribute as Publisher.AdditionalDirectoriesAttribute,
+                    publishDirPath);
+            }
+
+            var options = moduleToBuild.Options as Publisher.IPublishOptions;
+            if (options.OSXApplicationBundle)
+            {
+                // for built info.plists...
+                var infoPLists = metaData.FilterByType<Publisher.OSXInfoPListAttribute>();
+                // TODO: convert to var
+                foreach (Publisher.ProductModuleUtilities.MetaData meta in infoPLists)
+                {
+                    var nodeData = meta.Data as Publisher.NamedModuleLocation;
+                    if (null == nodeData)
+                    {
+                        throw new Opus.Core.Exception("Meta data '{0}' in '{1}' was of unexpected type '{2}'. Expected '{3}'",
+                            meta.Name, meta.Node.UniqueModuleName, meta.Data.GetType().ToString(), typeof(Publisher.NamedModuleLocation).ToString());
+                    }
+
+                    var plistNode = Opus.Core.ModuleUtilities.GetNode(nodeData.ModuleType, (Opus.Core.BaseTarget)moduleToBuild.OwningNode.Target);
+
+                    Opus.Core.Log.MessageAll("Copy Info.plist file '{0}' : '{1}' -> '{2}'", meta.Node.UniqueModuleName, nodeData.Key, publishDirPath);
+                    this.CopyInfoPList(
+                        moduleToBuild,
+                        primaryModule,
+                        plistNode.Module,
+                        nodeData.Key);
+                }
+            }
+
+            success = true;
+            return null;
+        }
+#else
         private static Opus.Core.StringArray
         MakeCopyFileRecipe(
             string sourcePath,
@@ -433,5 +675,6 @@ namespace MakeFileBuilder
             success = true;
             return returnData;
         }
+#endif
     }
 }
