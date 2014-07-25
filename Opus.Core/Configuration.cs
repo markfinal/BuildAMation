363f589dde3ec0111c118ec6f5de5826d6d566fd
Mark Final <markfinal@hotmail.com>
2014-07-25 07:46:58 +0100

    [Opus.Core,Enumerations] Split out the Configuration class into a separate file

---
+++ Opus.Core/Configuration.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="Configuration.cs" company="Mark Final">
+//  Opus
+// </copyright>
+// <summary>Opus Core</summary>
+// <author>Mark Final</author>
+namespace Opus.Core
+{
+    public static class Configuration
+    {
+        public static EConfiguration
+        FromString(
+            string configurationName)
+        {
+            var configuration = EConfiguration.Invalid;
+            if (0 == System.String.Compare(configurationName, "Debug", true))
+            {
+                configuration = EConfiguration.Debug;
+            }
+            else if (0 == System.String.Compare(configurationName, "Optimized", true))
+            {
+                configuration = EConfiguration.Optimized;
+            }
+            else if (0 == System.String.Compare(configurationName, "Profile", true))
+            {
+                configuration = EConfiguration.Profile;
+            }
+            else if (0 == System.String.Compare(configurationName, "Final", true))
+            {
+                configuration = EConfiguration.Final;
+            }
+            else
+            {
+                throw new Exception("Configuration name '{0}' not recognized", configurationName);
+            }
+            return configuration;
+        }
+    }
+}
--- Opus.Core/Enumerations/EConfiguration.cs
+++ Opus.Core/Enumerations/EConfiguration.cs
@@ -15,36 +15,4 @@ namespace Opus.Core
         Final     = (1 << 3),
         All       = Debug | Optimized | Profile | Final
     }
-
-    // TODO :move to another file
-    public static class Configuration
-    {
-        public static EConfiguration
-        FromString(
-            string configurationName)
-        {
-            var configuration = EConfiguration.Invalid;
-            if (0 == System.String.Compare(configurationName, "Debug", true))
-            {
-                configuration = EConfiguration.Debug;
-            }
-            else if (0 == System.String.Compare(configurationName, "Optimized", true))
-            {
-                configuration = EConfiguration.Optimized;
-            }
-            else if (0 == System.String.Compare(configurationName, "Profile", true))
-            {
-                configuration = EConfiguration.Profile;
-            }
-            else if (0 == System.String.Compare(configurationName, "Final", true))
-            {
-                configuration = EConfiguration.Final;
-            }
-            else
-            {
-                throw new Exception("Configuration name '{0}' not recognized", configurationName);
-            }
-            return configuration;
-        }
-    }
 }
