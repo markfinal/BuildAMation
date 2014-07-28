11979232220b61c09779349b858e326c1e79cef5
Mark Final <markfinal@hotmail.com>
2014-07-28 08:00:40 +0100

    [packages,Qt,4.8.5] Updated for coding standards

--- packages/Qt/4.8.5/Scripts/Modules.cs
+++ packages/Qt/4.8.5/Scripts/Modules.cs
@@ -5,83 +5,83 @@
 // <author>Mark Final</author>
 namespace Qt
 {
-    public sealed class Phonon : QtCommon.Phonon
-    {
-    }
+    public sealed class Phonon :
+        QtCommon.Phonon
+    {}
 
-    public sealed class Core : QtCommon.Core
-    {
-    }
+    public sealed class Core :
+        QtCommon.Core
+    {}
 
-    public sealed class DBus : QtCommon.DBus
-    {
-    }
+    public sealed class DBus :
+        QtCommon.DBus
+    {}
 
-    public sealed class Declarative : QtCommon.Declarative
-    {
-    }
+    public sealed class Declarative :
+        QtCommon.Declarative
+    {}
 
-    public sealed class Designer : QtCommon.Designer
-    {
-    }
+    public sealed class Designer :
+        QtCommon.Designer
+    {}
 
-    public sealed class Gui : QtCommon.Gui
-    {
-    }
+    public sealed class Gui :
+        QtCommon.Gui
+    {}
 
-    public sealed class Help : QtCommon.Help
-    {
-    }
+    public sealed class Help :
+        QtCommon.Help
+    {}
 
-    public sealed class Multimedia : QtCommon.Multimedia
-    {
-    }
+    public sealed class Multimedia :
+        QtCommon.Multimedia
+    {}
 
-    public sealed class Network : QtCommon.Network
-    {
-    }
+    public sealed class Network :
+        QtCommon.Network
+    {}
 
-    public sealed class OpenGL : QtCommon.OpenGL
-    {
-    }
+    public sealed class OpenGL :
+        QtCommon.OpenGL
+    {}
 
-    public sealed class OpenVG : QtCommon.OpenVG
-    {
-    }
+    public sealed class OpenVG :
+        QtCommon.OpenVG
+    {}
 
-    public sealed class Script : QtCommon.Script
-    {
-    }
+    public sealed class Script :
+        QtCommon.Script
+    {}
 
-    public sealed class ScriptTools : QtCommon.ScriptTools
-    {
-    }
+    public sealed class ScriptTools :
+        QtCommon.ScriptTools
+    {}
 
-    public sealed class Sql : QtCommon.Sql
-    {
-    }
+    public sealed class Sql :
+        QtCommon.Sql
+    {}
 
-    public sealed class Svg : QtCommon.Svg
-    {
-    }
+    public sealed class Svg :
+        QtCommon.Svg
+    {}
 
-    public sealed class Test : QtCommon.Test
-    {
-    }
+    public sealed class Test :
+        QtCommon.Test
+    {}
 
-    public sealed class UiTools : QtCommon.UiTools
-    {
-    }
+    public sealed class UiTools :
+        QtCommon.UiTools
+    {}
 
-    public sealed class WebKit : QtCommon.WebKit
-    {
-    }
+    public sealed class WebKit :
+        QtCommon.WebKit
+    {}
 
-    public sealed class Xml : QtCommon.Xml
-    {
-    }
+    public sealed class Xml :
+        QtCommon.Xml
+    {}
 
-    public sealed class XmlPatterns : QtCommon.XmlPatterns
-    {
-    }
+    public sealed class XmlPatterns :
+        QtCommon.XmlPatterns
+    {}
 }
