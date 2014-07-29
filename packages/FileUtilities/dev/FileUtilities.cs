174368b9636b98e61ec9c3c0ebf6bc3bd7651908
Mark Final <markfinal@hotmail.com>
2014-07-29 07:53:11 +0100

    [packages,FileUtilities,*] Removed FileUtilities package. This has been superceded by the Publisher package.

--- packages/FileUtilities/dev/FileUtilities.cs
+++
@@ -1,9 +0,0 @@
-// <copyright file="FileUtilities.cs" company="Mark Final">
-//  Opus package
-// </copyright>
-// <summary>FileUtilities package</summary>
-// <author>Mark Final</author>
-namespace FileUtilities
-{
-    // Add modules here
-}
