71632411d73f0de6c97062c87367a06f74951dae
Mark Final <markfinal@hotmail.com>
2014-08-16 16:03:04 +0100

    [Bam] Removing unused bash scripts

--- convert_line_endings.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-grep -IUrl --color '
-' . --include=*.cs | xargs -ifile fromdos 'file'
-
--- strip_trailing_whitespace.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find . -type f -name '*.cs' | xargs -ifile sed --in-place 's/[[:space:]]\+$//' 'file'
-
