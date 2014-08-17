bd97b30d4f63c5b45519e5ee5323971db9bdb5cf
Mark Final <markfinal@hotmail.com>
2014-08-17 17:15:52 +0100

    [Bam] Adding license text to all source files

--- packages/Clang/Apple425/Scripts/CxxCompilerOptionCollectionDelegates.cs
+++ packages/Clang/Apple425/Scripts/CxxCompilerOptionCollectionDelegates.cs
@@ -1,13 +1,21 @@
-// Automatically generated file from OpusOptionCodeGenerator.
-// Command line arguments:
-//     -i=../../../C/dev/Scripts/ICxxCompilerOptions.cs
-//     -n=Clang
-//     -c=CxxCompilerOptionCollection
-//     -p
-//     -d
-//     -dd=../../../CommandLineProcessor/dev/Scripts/CommandLineDelegate.cs&../../../XcodeProjectProcessor/dev/Scripts/Delegate.cs
-//     -pv=ClangCommon.PrivateData
-//     -e
+#region License
+// Copyright 2010-2014 Mark Final
+//
+// This file is part of BuildAMation.
+//
+// BuildAMation is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// BuildAMation is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with BuildAMation.  If not, see <http://www.gnu.org/licenses/>.
+#endregion
 
 namespace Clang
 {
--- testpackages/CodeGenTest/dev/Scripts/CodeGenTool.cs
+++ testpackages/CodeGenTest/dev/Scripts/CodeGenTool.cs
@@ -1,3 +1,21 @@
+#region License
+// Copyright 2010-2014 Mark Final
+//
+// This file is part of BuildAMation.
+//
+// BuildAMation is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// BuildAMation is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with BuildAMation.  If not, see <http://www.gnu.org/licenses/>.
+#endregion
 [assembly: Bam.Core.RegisterToolset("CodeGenTest", typeof(CodeGenTest.Toolset))]
 
 namespace CodeGenTest
