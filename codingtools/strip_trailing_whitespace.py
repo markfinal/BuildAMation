99c83876d3561e01a4660c4e892fc5080bd4a9ea
Mark Final <markfinal@hotmail.com>
2014-08-18 18:30:48 +0100

    [codingtools] Converted to correct tab spacing

--- codingtools/strip_trailing_whitespace.py
+++ codingtools/strip_trailing_whitespace.py
@@ -6,22 +6,22 @@ import re
 import sys
 
 def strip_trailing_whitespace(file):
-  with open(file, mode='rt') as infile:
-    lines = infile.readlines()
-  with open(file, mode='wt') as outfile:
-    for line in lines:
-      stripped = re.sub('[ \t]+$', '', line)
-      outfile.write(stripped)
-  if sys.platform.startswith("win"):
-    convert_line_endings(file)
+    with open(file, mode='rt') as infile:
+        lines = infile.readlines()
+    with open(file, mode='wt') as outfile:
+        for line in lines:
+            stripped = re.sub('[ \t]+$', '', line)
+            outfile.write(stripped)
+    if sys.platform.startswith("win"):
+        convert_line_endings(file)
 
 def processPath(dirPath, extensionList):
-  for dirpath, dirnames, filenames in os.walk(dirPath):
-    for file in filenames:
-      fileExt = os.path.splitext(file)[1]
-      if fileExt in extensionList:
-        csPath = os.path.join(dirpath, file)
-        strip_trailing_whitespace(csPath)
+    for dirpath, dirnames, filenames in os.walk(dirPath):
+        for file in filenames:
+            fileExt = os.path.splitext(file)[1]
+            if fileExt in extensionList:
+                csPath = os.path.join(dirpath, file)
+                strip_trailing_whitespace(csPath)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
