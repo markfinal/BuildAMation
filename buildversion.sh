adcd60520f858ec8c91db39ef3ad2f3be5a8a328
Mark Final <markfinal@hotmail.com>
2014-08-20 06:50:16 +0100

    [Bam] Updated the buildversion script. Need some more attention, and may actually create git tags

--- buildversion.sh
+++ buildversion.sh
@@ -2,25 +2,35 @@
 
 if [ -n "$1" ]
 then
-  echo "Creating Opus distribution for version $1"
+  echo "Creating BuildAMation distribution for version $1"
 else
   echo "Please supply a version number"
   exit 1
 fi
 
-directory=opus-$1
+directory=BuildAMation-$1
 
-svn export http://opus.googlecode.com/svn/trunk $directory
+git clone https://github.com/markfinal/BuildAMation.git $directory
+# TODO: checkout to a branch or tag
+# even better... test if there is a tag, if not, create it, after the version update below
+# this would allow creating a fixed point of code with the correct version number, but also being able to
+# recreate the exact code again
 rm $directory/.gitignore
+rm $directory/.git
 
+# Update version number to that specified
 find $directory -name "AssemblyInfo.cs" | xargs sed 's/AssemblyInformationalVersion("[0-9.]*")/AssemblyInformationalVersion("'$1'")/g' -i
 
-tar -czf opus-$1-source.tgz $directory
+# tar up the source tree
+tar -czf BuildAMation-$1-source.tgz $directory
 
+# now compile the source tree, optimized
 pushd $directory
-mdtool build --target:Build --configuration:Release Opus.sln
+mdtool build --target:Build --configuration:Release BuildAMation.sln
 popd
 
-tar -czf opus-$1-binary.tgz $directory/bin $directory/Changelog.txt $directory/env.bat $directory/env.sh $directory/License.txt $directory/packages $directory/testpackages
-rm -fr $directory
+# now tar up the 'bin' folder, and any additional files necessary to ship
+tar -czf BuildAMation-$1-binary.tgz $directory/bin $directory/Changelog.txt $directory/COPYING.LESSER $directory/env.bat $directory/env.sh $directory/License.txt $directory/packages $directory/testpackages
 
+# remove checkout folder
+rm -fr $directory
