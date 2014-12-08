c232ae4a257ef2218d99a7fa8673abdc50bc12c1
Mark Final <markfinal@hotmail.com>
2014-12-08 08:13:04 +0000

    [testpackages] Adding a C++11 test packages, Cxx11Test1-dev.

---
+++ testpackages/Cxx11Test1/dev/source/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    auto integer = 42;
+    auto string = "My text";
+    std::cout << "Integer value is " << integer << std::endl;
+    std::cout << "String value is '" << string << "'" << std::endl;
+    return 0;
+}
