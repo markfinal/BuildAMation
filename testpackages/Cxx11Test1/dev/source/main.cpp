83094bfc3ebb10f16f4eab936598590d51615157
Mark Final <markfinal@hotmail.com>
2014-12-08 08:20:00 +0000

    [testpackages,Cxx11Test1,dev] Testing lambda functions

--- testpackages/Cxx11Test1/dev/source/main.cpp
+++ testpackages/Cxx11Test1/dev/source/main.cpp
@@ -2,9 +2,15 @@
 
 int main()
 {
+    // auto variables
     auto integer = 42;
     auto string = "My text";
-    std::cout << "Integer value is " << integer << std::endl;
-    std::cout << "String value is '" << string << "'" << std::endl;
+    std::cout << "Auto integer value is " << integer << std::endl;
+    std::cout << "Auto string value is '" << string << "'" << std::endl;
+
+    // lambda functions
+    auto func = [] () { std::cout << "Hello world, from a lambda function" << std::endl; };
+    func();
+
     return 0;
 }
