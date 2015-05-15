b3ac23dcec6540f665da7c9552355efde890e8a1
Mark Final <markfinal@hotmail.com>
2015-05-15 07:35:41 +0100

    [V2] Prototype implementation for the Apple clang compiler

--- packages/Clang/Apple425/Scripts/CCompilerOptionCollection.cs
+++ packages/Clang/Apple425/Scripts/CCompilerOptionCollection.cs
@@ -16,8 +16,310 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BuildAMation.  If not, see <http://www.gnu.org/licenses/>.
 #endregion // License
+using C.V2.DefaultSettings;
 namespace Clang
 {
+    public static partial class NativeImplementation
+    {
+        public static void
+        Convert(
+            this C.V2.ICommonCompilerOptions options,
+            Bam.Core.V2.Module module,
+            Bam.Core.StringArray commandLine)
+        {
+            var objectFile = module as C.V2.ObjectFile;
+            if (true == options.DebugSymbols)
+            {
+                commandLine.Add("-g");
+            }
+        }
+    }
+
+namespace V2
+{
+    public class CompilerSettings :
+        Bam.Core.V2.Settings,
+        C.V2.ICommonCompilerOptions,
+        C.V2.ICOnlyCompilerOptions,
+        CommandLineProcessor.V2.IConvertToCommandLine
+    {
+        public CompilerSettings(Bam.Core.V2.Module module)
+            : this(module, true)
+        {
+        }
+
+        public CompilerSettings(Bam.Core.V2.Module module, bool useDefaults)
+        {
+            (this as C.V2.ICommonCompilerOptions).Empty();
+            if (useDefaults)
+            {
+                (this as C.V2.ICommonCompilerOptions).Defaults(module);
+            }
+        }
+
+        C.V2.EBit? C.V2.ICommonCompilerOptions.Bits
+        {
+            get;
+            set;
+        }
+
+        C.V2.PreprocessorDefinitions C.V2.ICommonCompilerOptions.PreprocessorDefines
+        {
+            get;
+            set;
+        }
+
+        Bam.Core.Array<Bam.Core.V2.TokenizedString> C.V2.ICommonCompilerOptions.IncludePaths
+        {
+            get;
+            set;
+        }
+
+        Bam.Core.Array<Bam.Core.V2.TokenizedString> C.V2.ICommonCompilerOptions.SystemIncludePaths
+        {
+            get;
+            set;
+        }
+
+        C.ECompilerOutput? C.V2.ICommonCompilerOptions.OutputType
+        {
+            get;
+            set;
+        }
+
+        bool? C.V2.ICommonCompilerOptions.DebugSymbols
+        {
+            get;
+            set;
+        }
+
+        bool? C.V2.ICommonCompilerOptions.WarningsAsErrors
+        {
+            get;
+            set;
+        }
+
+        C.EOptimization? C.V2.ICommonCompilerOptions.Optimization
+        {
+            get;
+            set;
+        }
+
+        C.ETargetLanguage? C.V2.ICommonCompilerOptions.TargetLanguage
+        {
+            get;
+            set;
+        }
+
+        C.ELanguageStandard? C.V2.ICommonCompilerOptions.LanguageStandard
+        {
+            get;
+            set;
+        }
+
+        bool? C.V2.ICommonCompilerOptions.OmitFramePointer
+        {
+            get;
+            set;
+        }
+
+        Bam.Core.StringArray C.V2.ICommonCompilerOptions.DisableWarnings
+        {
+            get;
+            set;
+        }
+
+        Bam.Core.StringArray C.V2.ICommonCompilerOptions.PreprocessorUndefines
+        {
+            get;
+            set;
+        }
+
+        void CommandLineProcessor.V2.IConvertToCommandLine.Convert(Bam.Core.V2.Module module, Bam.Core.StringArray commandLine)
+        {
+            (this as C.V2.ICommonCompilerOptions).Convert(module, commandLine);
+        }
+    }
+
+    public sealed class CxxCompilerSettings :
+        Bam.Core.V2.Settings,
+        C.V2.ICommonCompilerOptions,
+        C.V2.ICxxOnlyCompilerOptions
+    {
+        C.V2.EBit? C.V2.ICommonCompilerOptions.Bits
+        {
+            get;
+            set;
+        }
+
+        C.V2.PreprocessorDefinitions C.V2.ICommonCompilerOptions.PreprocessorDefines
+        {
+            get;
+            set;
+        }
+
+        Bam.Core.Array<Bam.Core.V2.TokenizedString> C.V2.ICommonCompilerOptions.IncludePaths
+        {
+            get;
+            set;
+        }
+
+        Bam.Core.Array<Bam.Core.V2.TokenizedString> C.V2.ICommonCompilerOptions.SystemIncludePaths
+        {
+            get;
+            set;
+        }
+
+        C.ECompilerOutput? C.V2.ICommonCompilerOptions.OutputType
+        {
+            get;
+            set;
+        }
+
+        bool? C.V2.ICommonCompilerOptions.DebugSymbols
+        {
+            get;
+            set;
+        }
+
+        bool? C.V2.ICommonCompilerOptions.WarningsAsErrors
+        {
+            get;
+            set;
+        }
+
+        C.EOptimization? C.V2.ICommonCompilerOptions.Optimization
+        {
+            get;
+            set;
+        }
+
+        C.ETargetLanguage? C.V2.ICommonCompilerOptions.TargetLanguage
+        {
+            get;
+            set;
+        }
+
+        C.ELanguageStandard? C.V2.ICommonCompilerOptions.LanguageStandard
+        {
+            get;
+            set;
+        }
+
+        bool? C.V2.ICommonCompilerOptions.OmitFramePointer
+        {
+            get;
+            set;
+        }
+
+        Bam.Core.StringArray C.V2.ICommonCompilerOptions.DisableWarnings
+        {
+            get;
+            set;
+        }
+
+        Bam.Core.StringArray C.V2.ICommonCompilerOptions.PreprocessorUndefines
+        {
+            get;
+            set;
+        }
+
+        C.Cxx.EExceptionHandler C.V2.ICxxOnlyCompilerOptions.ExceptionHandler
+        {
+            get;
+            set;
+        }
+    }
+
+    public static class Configure
+    {
+        static Configure()
+        {
+            InstallPath = Bam.Core.V2.TokenizedString.Create(@"/usr/bin", null);
+        }
+
+        public static Bam.Core.V2.TokenizedString InstallPath
+        {
+            get;
+            private set;
+        }
+    }
+
+    public abstract class CompilerBase :
+        C.V2.CompilerTool
+    {
+        protected CompilerBase()
+        {
+            this.Macros.Add("InstallPath", Configure.InstallPath);
+            this.Macros.Add("objext", ".o");
+        }
+
+        public override Bam.Core.V2.TokenizedString Executable
+        {
+            get
+            {
+                return this.Macros["CompilerPath"];
+            }
+        }
+
+        public override Bam.Core.V2.Settings CreateDefaultSettings<T>(T module)
+        {
+            if (typeof(C.Cxx.V2.ObjectFile).IsInstanceOfType(module) ||
+                typeof(C.Cxx.V2.ObjectFileCollection).IsInstanceOfType(module))
+            {
+                var settings = new CxxCompilerSettings();
+                this.OverrideDefaultSettings(settings);
+                return settings;
+            }
+            else if (typeof(C.V2.ObjectFile).IsInstanceOfType(module) ||
+                     typeof(C.V2.CObjectFileCollection).IsInstanceOfType(module))
+            {
+                var settings = new CompilerSettings(module);
+                this.OverrideDefaultSettings(settings);
+                return settings;
+            }
+            // TODO: objective C
+            else
+            {
+                throw new Bam.Core.Exception("Could not determine type of module {0}", typeof(T).ToString());
+            }
+        }
+
+        protected abstract void OverrideDefaultSettings(Bam.Core.V2.Settings settings);
+    }
+
+    [C.V2.RegisterCompiler("Clang", Bam.Core.EPlatform.OSX)]
+    public sealed class CCompiler :
+        CompilerBase
+    {
+        public CCompiler()
+        {
+            this.Macros.Add("CompilerPath", Bam.Core.V2.TokenizedString.Create("$(InstallPath)/clang", this));
+        }
+
+        protected override void OverrideDefaultSettings(Bam.Core.V2.Settings settings)
+        {
+            var cSettings = settings as C.V2.ICommonCompilerOptions;
+            cSettings.TargetLanguage = C.ETargetLanguage.C;
+        }
+    }
+
+    public sealed class CxxCompiler :
+        CompilerBase
+    {
+        public CxxCompiler()
+        {
+            this.Macros.Add("CompilerPath", Bam.Core.V2.TokenizedString.Create("$(InstallPath)/clang++", this));
+        }
+
+        protected override void OverrideDefaultSettings(Bam.Core.V2.Settings settings)
+        {
+            var cSettings = settings as C.V2.ICommonCompilerOptions;
+            cSettings.TargetLanguage = C.ETargetLanguage.Cxx;
+        }
+    }
+}
+
     public partial class CCompilerOptionCollection :
         ClangCommon.CCompilerOptionCollection,
         C.ICCompilerOptionsOSX
