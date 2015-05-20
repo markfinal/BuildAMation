f4a8498b5aed1cdd15eca6b630316fc0e30e77f1
Mark Final <markfinal@hotmail.com>
2015-05-20 07:24:08 +0100

    [V2] Borrowed the Workspace Settings code from V1, in order for product outputs to be written relative to the workspace

--- packages/XcodeBuilder/dev/XcodeBuilder.cs
+++ packages/XcodeBuilder/dev/XcodeBuilder.cs
@@ -1108,6 +1108,9 @@ namespace V2
             {
                 workspaceContents.WriteTo(xmlwriter);
             }
+
+            var workspaceSettings = new WorkspaceSettings(workspaceDir);
+            workspaceSettings.Serialize();
         }
     }
 
@@ -1233,6 +1236,103 @@ namespace V2
             this.Project.FileReferences.Add(copyOfLibFileRef);
             this.Project.BuildFiles.Add(libraryBuildFile);
             this.Frameworks.BuildFiles.Add(libraryBuildFile);
+        }
+    }
+
+    public class WorkspaceSettings
+    {
+        public
+        WorkspaceSettings(
+            string workspaceDir)
+        {
+            this.Path = workspaceDir;
+            this.Path = System.IO.Path.Combine(this.Path, "xcuserdata");
+            this.Path = System.IO.Path.Combine(this.Path, System.Environment.GetEnvironmentVariable("USER") + ".xcuserdatad");
+            this.Path = System.IO.Path.Combine(this.Path, "WorkspaceSettings.xcsettings");
+            this.CreatePlist();
+        }
+
+        private void
+        CreateKeyValuePair(
+            System.Xml.XmlDocument doc,
+            System.Xml.XmlElement parent,
+            string key,
+            string value)
+        {
+            var keyEl = doc.CreateElement("key");
+            keyEl.InnerText = key;
+            var valueEl = doc.CreateElement("string");
+            valueEl.InnerText = value;
+            parent.AppendChild(keyEl);
+            parent.AppendChild(valueEl);
+        }
+
+        private void
+        CreatePlist()
+        {
+            var doc = new System.Xml.XmlDocument();
+            // don't resolve any URLs, or if there is no internet, the process will pause for some time
+            doc.XmlResolver = null;
+
+            {
+                var type = doc.CreateDocumentType("plist", "-//Apple Computer//DTD PLIST 1.0//EN", "http://www.apple.com/DTDs/PropertyList-1.0.dtd", null);
+                doc.AppendChild(type);
+            }
+            var plistEl = doc.CreateElement("plist");
+            {
+                var versionAttr = doc.CreateAttribute("version");
+                versionAttr.Value = "1.0";
+                plistEl.Attributes.Append(versionAttr);
+            }
+
+            var dictEl = doc.CreateElement("dict");
+            plistEl.AppendChild(dictEl);
+            doc.AppendChild(plistEl);
+
+            // build and intermediate file locations
+            CreateKeyValuePair(doc, dictEl, "BuildLocationStyle", "CustomLocation");
+            CreateKeyValuePair(doc, dictEl, "CustomBuildIntermediatesPath", "XcodeIntermediates"); // where xxx.build folders are stored
+            CreateKeyValuePair(doc, dictEl, "CustomBuildLocationType", "RelativeToWorkspace");
+            CreateKeyValuePair(doc, dictEl, "CustomBuildProductsPath", "."); // has to be the workspace folder, in order to write files to expected locations
+
+            // derived data
+            CreateKeyValuePair(doc, dictEl, "DerivedDataCustomLocation", "XcodeDerivedData");
+            CreateKeyValuePair(doc, dictEl, "DerivedDataLocationStyle", "WorkspaceRelativePath");
+
+            this.Document = doc;
+        }
+
+        private string Path
+        {
+            get;
+            set;
+        }
+
+        private System.Xml.XmlDocument Document
+        {
+            get;
+            set;
+        }
+
+        public void
+        Serialize()
+        {
+            // do not write a Byte-Ordering-Mark (BOM)
+            var encoding = new System.Text.UTF8Encoding(false);
+
+            System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(this.Path));
+            using (var writer = new System.IO.StreamWriter(this.Path, false, encoding))
+            {
+                var settings = new System.Xml.XmlWriterSettings();
+                settings.OmitXmlDeclaration = false;
+                settings.NewLineChars = "\n";
+                settings.Indent = true;
+                using (var xmlWriter = System.Xml.XmlWriter.Create(writer, settings))
+                {
+                    this.Document.WriteTo(xmlWriter);
+                    xmlWriter.WriteWhitespace(settings.NewLineChars);
+                }
+            }
         }
     }
 }
