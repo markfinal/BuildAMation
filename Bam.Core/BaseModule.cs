1e3ee6e9b4818c5cb1b2e5079a77deed22403675
Mark Final <markfinal@hotmail.com>
2015-05-27 07:45:38 +0100

    [V2] Adding the module's package name into the macros, as packagename

--- Bam.Core/BaseModule.cs
+++ Bam.Core/BaseModule.cs
@@ -63,6 +63,7 @@ namespace V2
             var packageRoot = packageInfo.Identifier.Location.AbsolutePath;
             this.Macros.Add("pkgroot", packageRoot);
             this.Macros.Add("modulename", this.GetType().Name);
+            this.Macros.Add("packagename", packageInfo.Name);
             this.Macros.Add("pkgbuilddir", packageInfo.BuildDirectory);
 
             this.OwningRank = null;
