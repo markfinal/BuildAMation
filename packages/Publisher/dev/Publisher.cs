9bdec1798e2973c1b4ad34ebf2b9bc5208c14fd2
Mark Final <markfinal@hotmail.com>
2015-06-24 18:34:39 +0100

    [V2] Prototype for running InnoSetup compiler

--- packages/Publisher/dev/Publisher.cs
+++ packages/Publisher/dev/Publisher.cs
@@ -84,5 +84,135 @@ namespace V2
         }
     }
 }
+namespace V2
+{
+    class InnoSetupScript :
+        Bam.Core.V2.Module
+    {
+        private System.Collections.Generic.Dictionary<Bam.Core.V2.Module, Bam.Core.V2.FileKey> Files = new System.Collections.Generic.Dictionary<Bam.Core.V2.Module, Bam.Core.V2.FileKey>();
+
+        public InnoSetupScript()
+        {
+            this.ScriptPath = Bam.Core.V2.TokenizedString.Create("$(buildroot)/$(modulename)/script.iss", this);
+        }
+
+        public Bam.Core.V2.TokenizedString ScriptPath
+        {
+            get;
+            private set;
+        }
+
+        public void
+        AddFile(
+            Bam.Core.V2.Module module,
+            Bam.Core.V2.FileKey key)
+        {
+            this.DependsOn(module);
+            this.Files.Add(module, key);
+        }
+
+        public override void Evaluate()
+        {
+            // do nothing
+        }
+
+        protected override void ExecuteInternal()
+        {
+            var path = this.ScriptPath.Parse();
+            var dir = System.IO.Path.GetDirectoryName(path);
+            if (!System.IO.Directory.Exists(dir))
+            {
+                System.IO.Directory.CreateDirectory(dir);
+            }
+            using (var scriptWriter = new System.IO.StreamWriter(path))
+            {
+                scriptWriter.WriteLine("[Setup]");
+                scriptWriter.WriteLine("AppName={0}", this.GetType().ToString());
+                scriptWriter.WriteLine("AppVersion=1.0");
+                scriptWriter.WriteLine("DefaultDirName={{sd}}\\{0}", this.GetType().ToString());
+                scriptWriter.WriteLine("ArchitecturesAllowed=x64");
+                scriptWriter.WriteLine("ArchitecturesInstallIn64BitMode=x64");
+                scriptWriter.WriteLine("[Files]");
+                foreach (var dep in this.Files)
+                {
+                    scriptWriter.Write(System.String.Format("Source: \"{0}\"; ", dep.Key.GeneratedPaths[dep.Value]));
+                    scriptWriter.Write("DestDir: \"{app}\"; ");
+                    scriptWriter.Write("DestName: \"Test\"");
+                }
+            }
+        }
+
+        protected override void GetExecutionPolicy(string mode)
+        {
+            // do nothing
+        }
+    }
+
+    public sealed class InnoSetupCompilerSettings :
+        Bam.Core.V2.Settings
+    {
+    }
+
+    public sealed class InnoSetupCompiler :
+        Bam.Core.V2.Tool
+    {
+        public override Bam.Core.V2.Settings CreateDefaultSettings<T>(T module)
+        {
+            return new InnoSetupCompilerSettings();
+        }
+
+        public override Bam.Core.V2.TokenizedString Executable
+        {
+            get
+            {
+                return Bam.Core.V2.TokenizedString.Create(@"C:\Program Files (x86)\Inno Setup 5\ISCC.exe", null);
+            }
+        }
+    }
+
+    public abstract class InnoSetupInstaller :
+        Bam.Core.V2.Module
+    {
+        private InnoSetupScript ScriptModule;
+        private Bam.Core.V2.Tool Compiler;
+
+        public InnoSetupInstaller()
+        {
+            // TODO: this actually needs to be a new class each time, otherwise multiple installers won't work
+            // need to find a way to instantiate a non-abstract instance of an abstract class
+            // looks like emit is needed
+            this.ScriptModule = Bam.Core.V2.Graph.Instance.FindReferencedModule<InnoSetupScript>();
+            this.DependsOn(this.ScriptModule);
+
+            this.Compiler = Bam.Core.V2.Graph.Instance.FindReferencedModule<InnoSetupCompiler>();
+            this.Requires(this.Compiler);
+        }
+
+        public void
+        Include<DependentModule>(
+            Bam.Core.V2.FileKey key) where DependentModule : Bam.Core.V2.Module, new()
+        {
+            var dependent = Bam.Core.V2.Graph.Instance.FindReferencedModule<DependentModule>();
+            this.ScriptModule.AddFile(dependent, key);
+        }
+
+        public override void Evaluate()
+        {
+            // do nothing
+        }
+
+        protected override void ExecuteInternal()
+        {
+            var args = new Bam.Core.StringArray();
+            args.Add(this.ScriptModule.ScriptPath.Parse());
+            CommandLineProcessor.V2.Processor.Execute(this.Compiler, args);
+        }
+
+        protected override void GetExecutionPolicy(string mode)
+        {
+            // do nothing
+        }
+    }
+}
     // Add modules here
 }
