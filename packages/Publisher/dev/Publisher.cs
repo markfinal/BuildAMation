ad937bae9f7d15db60b9b7f9f89a934537e8721d
Mark Final <markfinal@hotmail.com>
2015-06-26 06:45:15 +0100

    [V2] Prototype of the OSX disk image creator. Doesn't contain anything yet

--- packages/Publisher/dev/Publisher.cs
+++ packages/Publisher/dev/Publisher.cs
@@ -488,5 +488,191 @@ namespace V2
         }
     }
 }
+namespace V2
+{
+    class DiskImageInputFiles :
+        Bam.Core.V2.Module
+    {
+        private System.Collections.Generic.Dictionary<Bam.Core.V2.Module, Bam.Core.V2.FileKey> Files = new System.Collections.Generic.Dictionary<Bam.Core.V2.Module, Bam.Core.V2.FileKey>();
+
+        public DiskImageInputFiles()
+        {
+            this.ScriptPath = Bam.Core.V2.TokenizedString.Create("$(buildroot)/$(modulename)/diskimageinput.txt", this);
+        }
+
+        public Bam.Core.V2.TokenizedString ScriptPath
+        {
+            get;
+            private set;
+        }
+
+        public void
+        AddFile(
+            Bam.Core.V2.Module module,
+            Bam.Core.V2.FileKey key)
+        {
+            this.DependsOn(module);
+            this.Files.Add(module, key);
+        }
+
+        public override void Evaluate()
+        {
+            // do nothing
+        }
+
+        protected override void ExecuteInternal()
+        {
+            var path = this.ScriptPath.Parse();
+            var dir = System.IO.Path.GetDirectoryName(path);
+            if (!System.IO.Directory.Exists(dir))
+            {
+                System.IO.Directory.CreateDirectory(dir);
+            }
+            using (var scriptWriter = new System.IO.StreamWriter(path))
+            {
+                foreach (var dep in this.Files)
+                {
+                    var filePath = dep.Key.GeneratedPaths[dep.Value].ToString();
+                    var fileDir = System.IO.Path.GetDirectoryName(filePath);
+                    // TODO: this should probably be a setting
+                    if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.OSX))
+                    {
+                        scriptWriter.WriteLine("-C");
+                        scriptWriter.WriteLine(fileDir);
+                    }
+                    else
+                    {
+                        scriptWriter.WriteLine("-C {0}", fileDir);
+                    }
+                    scriptWriter.WriteLine(System.IO.Path.GetFileName(filePath));
+                }
+            }
+        }
+
+        protected override void GetExecutionPolicy(string mode)
+        {
+            // do nothing
+        }
+    }
+
+    public sealed class DiskImageSettings :
+        Bam.Core.V2.Settings
+    {
+    }
+
+    public sealed class DiskImageCompiler :
+        Bam.Core.V2.Tool
+    {
+        public override Bam.Core.V2.Settings CreateDefaultSettings<T>(T module)
+        {
+            return new TarSettings();
+        }
+
+        public override Bam.Core.V2.TokenizedString Executable
+        {
+            get
+            {
+                return Bam.Core.V2.TokenizedString.Create("hdiutil", null);
+            }
+        }
+    }
+
+    [Bam.Core.V2.PlatformFilter(Bam.Core.EPlatform.OSX)]
+    public abstract class DiskImage :
+        Bam.Core.V2.Module
+    {
+        public static Bam.Core.V2.FileKey Key = Bam.Core.V2.FileKey.Generate("Installer");
+
+        private DiskImageInputFiles InputFiles;
+        private Bam.Core.V2.Tool Compiler;
+
+        public DiskImage()
+        {
+            this.RegisterGeneratedFile(Key, Bam.Core.V2.TokenizedString.Create("$(buildroot)/installer.dmg", this));
+
+            // TODO: this actually needs to be a new class each time, otherwise multiple installers won't work
+            // need to find a way to instantiate a non-abstract instance of an abstract class
+            // looks like emit is needed
+            this.InputFiles = Bam.Core.V2.Graph.Instance.FindReferencedModule<DiskImageInputFiles>();
+            this.DependsOn(this.InputFiles);
+
+            this.Compiler = Bam.Core.V2.Graph.Instance.FindReferencedModule<DiskImageCompiler>();
+            this.Requires(this.Compiler);
+        }
+
+        public void
+        Include<DependentModule>(
+            Bam.Core.V2.FileKey key) where DependentModule : Bam.Core.V2.Module, new()
+        {
+            var dependent = Bam.Core.V2.Graph.Instance.FindReferencedModule<DependentModule>();
+            this.InputFiles.AddFile(dependent, key);
+        }
+
+        public override void Evaluate()
+        {
+            // do nothing
+        }
+
+        protected override void ExecuteInternal()
+        {
+            var volumeName = "My Volume";
+            var diskImagePathName = this.GeneratedPaths[Key].ToString();
+
+            // create the disk image
+            {
+                var args = new Bam.Core.StringArray();
+                args.Add("create");
+                args.Add("-quiet");
+                args.Add("-size");
+                args.Add("32m");
+                args.Add("-fs");
+                args.Add("HFS+");
+                args.Add("-volname");
+                args.Add(System.String.Format("\"{0}\"", volumeName));
+                args.Add(diskImagePathName);
+                CommandLineProcessor.V2.Processor.Execute(this.Compiler, args);
+            }
+
+            // mount disk image
+            {
+                var args = new Bam.Core.StringArray();
+                args.Add("attach");
+                args.Add("-quiet");
+                args.Add(diskImagePathName);
+                CommandLineProcessor.V2.Processor.Execute(this.Compiler, args);
+            }
+
+                // TODO
+                /// do a copy
+
+            // unmount disk image
+            {
+                var args = new Bam.Core.StringArray();
+                args.Add("detach");
+                args.Add("-quiet");
+                args.Add(System.String.Format("\"/Volumes/{0}\"", volumeName));
+                CommandLineProcessor.V2.Processor.Execute(this.Compiler, args);
+            }
+
+            // hdiutil convert myimg.dmg -format UDZO -o myoutputimg.dmg
+            {
+                var args = new Bam.Core.StringArray();
+                args.Add("convert");
+                args.Add("-quiet");
+                args.Add(diskImagePathName);
+                args.Add("-format");
+                args.Add("UDZ0");
+                args.Add("-o");
+                args.Add(diskImagePathName);
+                CommandLineProcessor.V2.Processor.Execute(this.Compiler, args);
+            }
+        }
+
+        protected override void GetExecutionPolicy(string mode)
+        {
+            // do nothing
+        }
+    }
+}
     // Add modules here
 }
