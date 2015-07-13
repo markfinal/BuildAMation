8972ec1207c1dbcf59c6a4f1bb3148b3ba015fff
Mark Final <markfinal@hotmail.com>
2015-07-13 18:23:34 +0100

    [V2] Implemented the HeaderLibraryTest test case

--- testpackages/HeaderLibraryTest/dev/HeaderLibraryTest.cs
+++ testpackages/HeaderLibraryTest/dev/HeaderLibraryTest.cs
@@ -16,8 +16,47 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BuildAMation.  If not, see <http://www.gnu.org/licenses/>.
 #endregion // License
+using Bam.Core.V2; // for EPlatform.PlatformExtensions
 namespace HeaderLibraryTest
 {
+    public sealed class HeaderLibraryV2 :
+        C.V2.HeaderLibrary
+    {
+        protected override void Init(Bam.Core.V2.Module parent)
+        {
+            base.Init(parent);
+
+            this.PublicPatch((settings, appliedTo) =>
+                {
+                    var compiler = settings as C.V2.ICommonCompilerOptions;
+                    if (null != compiler)
+                    {
+                        compiler.IncludePaths.Add(Bam.Core.V2.TokenizedString.Create("$(pkgroot)/include", this));
+                    }
+                });
+        }
+    }
+
+    public sealed class ApplicationV2 :
+        C.V2.ConsoleApplication
+    {
+        protected override void Init(Bam.Core.V2.Module parent)
+        {
+            base.Init(parent);
+
+            var source = this.CreateCxxSourceContainer();
+            source.AddFile("$(pkgroot)/source/main.c");
+
+            this.CompileAgainst<HeaderLibraryV2>(source);
+
+            if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Windows) &&
+                this.Linker is VisualC.V2.LinkerBase)
+            {
+                this.LinkAgainst<WindowsSDK.WindowsSDKV2>();
+            }
+        }
+    }
+
     class HeaderLibrary :
         C.HeaderLibrary
     {
