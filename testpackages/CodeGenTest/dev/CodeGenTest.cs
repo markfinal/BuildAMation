f6c24153db276202d20859eadfc8bc0a7ee77c22
Mark Final <markfinal@hotmail.com>
2015-07-13 08:05:07 +0100

    [V2] Converted CodeGenTest, which is much simpler than it used to be

--- testpackages/CodeGenTest/dev/CodeGenTest.cs
+++ testpackages/CodeGenTest/dev/CodeGenTest.cs
@@ -16,8 +16,130 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BuildAMation.  If not, see <http://www.gnu.org/licenses/>.
 #endregion // License
+using Bam.Core.V2; // for EPlatform.PlatformExtensions
+using CodeGenTest.V2.CodeGenExtension;
 namespace CodeGenTest
 {
+namespace V2
+{
+    namespace CodeGenExtension
+    {
+        public static class CodeGenExtension
+        {
+            public static System.Tuple<Bam.Core.V2.Module, Bam.Core.V2.Module>
+            GenerateSource(
+                this C.V2.CObjectFileCollection module)
+            {
+                // generated source file
+                var generatedSourceFile = Bam.Core.V2.Module.Create<GeneratedSourceModule>(module);
+
+                // compile source
+                var objFile = module.AddFile(GeneratedSourceModule.Key, generatedSourceFile);
+
+                return new System.Tuple<Bam.Core.V2.Module, Bam.Core.V2.Module>(generatedSourceFile, objFile);
+            }
+        }
+    }
+
+    public sealed class BuildCodeGenTool :
+        C.V2.ConsoleApplication
+    {
+        protected override void Init(Bam.Core.V2.Module parent)
+        {
+            base.Init(parent);
+
+            var source = this.CreateCSourceContainer();
+            source.AddFile("$(pkgroot)/source/codegentool/main.c");
+
+            if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Windows) &&
+                this.Linker is VisualC.V2.LinkerBase)
+            {
+                this.LinkAgainst<WindowsSDK.WindowsSDKV2>();
+            }
+        }
+    }
+
+    public sealed class GeneratedSourceSettings :
+        Bam.Core.V2.Settings
+    {
+    }
+
+    public sealed class GeneratedSourceTool :
+        Bam.Core.V2.Tool
+    {
+        private C.V2.CModule buildTool;
+
+        protected override void Init(Bam.Core.V2.Module parent)
+        {
+            base.Init(parent);
+
+            this.buildTool = Bam.Core.V2.Graph.Instance.FindReferencedModule<BuildCodeGenTool>();
+            this.Requires(this.buildTool);
+        }
+
+        public override Bam.Core.V2.Settings CreateDefaultSettings<T>(T module)
+        {
+            return new GeneratedSourceSettings();
+        }
+
+        public override Bam.Core.V2.TokenizedString Executable
+        {
+            get
+            {
+                return this.buildTool.GeneratedPaths[C.V2.ConsoleApplication.Key];
+            }
+        }
+    }
+
+    public class GeneratedSourceModule :
+        C.V2.SourceFile
+    {
+        private Bam.Core.V2.Tool Compiler;
+
+        public GeneratedSourceModule()
+        {
+            this.RegisterGeneratedFile(Key, Bam.Core.V2.TokenizedString.Create("$(buildroot)/Generated.c", this));
+            this.Compiler = Bam.Core.V2.Graph.Instance.FindReferencedModule<GeneratedSourceTool>();
+            this.Requires(this.Compiler);
+        }
+
+        public override void
+        Evaluate()
+        {
+            this.IsUpToDate = false;
+        }
+
+        protected override void
+        ExecuteInternal(
+            Bam.Core.V2.ExecutionContext context)
+        {
+            var args = new Bam.Core.StringArray();
+            args.Add(Bam.Core.V2.TokenizedString.Create("$(buildroot)", this).Parse());
+            args.Add("Generated");
+            CommandLineProcessor.V2.Processor.Execute(context, this.Compiler, args);
+        }
+    }
+}
+    public sealed class TestAppV2 :
+        C.V2.ConsoleApplication
+    {
+        protected override void Init(Bam.Core.V2.Module parent)
+        {
+            base.Init(parent);
+
+            var source = this.CreateCSourceContainer();
+            source.AddFile("$(pkgroot)/source/testapp/main.c");
+
+            var generatedSourceTuple = source.GenerateSource();
+
+            if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Windows) &&
+                this.Linker is VisualC.V2.LinkerBase)
+            {
+                this.LinkAgainst<WindowsSDK.WindowsSDKV2>();
+            }
+        }
+    }
+
     // Define module classes here
     class TestAppGeneratedSource :
         CodeGenModule
