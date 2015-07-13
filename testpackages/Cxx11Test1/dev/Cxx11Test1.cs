c88f2c66019c6aaa5320e233ca936d4c8e6c43d4
Mark Final <markfinal@hotmail.com>
2015-07-13 07:34:21 +0100

    [V2] Updated the C++11 test

--- testpackages/Cxx11Test1/dev/Cxx11Test1.cs
+++ testpackages/Cxx11Test1/dev/Cxx11Test1.cs
@@ -16,10 +16,37 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BuildAMation.  If not, see <http://www.gnu.org/licenses/>.
 #endregion // License
-[assembly:Bam.Core.GlobalOptionCollectionOverride(typeof(Cxx11Test1.GlobalSettings))]
-
+using Bam.Core.V2; // for EPlatform.PlatformExtensions
+[assembly: Bam.Core.GlobalOptionCollectionOverride(typeof(Cxx11Test1.GlobalSettings))]
 namespace Cxx11Test1
 {
+    public sealed class TestProgV2 :
+        C.V2.ConsoleApplication
+    {
+        protected override void Init(Bam.Core.V2.Module parent)
+        {
+            base.Init(parent);
+
+            var source = this.CreateCxxSourceContainer();
+            source.AddFile("$(pkgroot)/source/main.cpp");
+
+            source.PrivatePatch(settings =>
+                {
+                    var compiler = settings as C.V2.ICommonCompilerOptions;
+                    compiler.LanguageStandard = C.ELanguageStandard.Cxx11;
+
+                    var cxxCompiler = settings as C.V2.ICxxOnlyCompilerOptions;
+                    cxxCompiler.ExceptionHandler = C.Cxx.EExceptionHandler.Synchronous;
+                });
+
+            if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Windows) &&
+                this.Linker is VisualC.V2.LinkerBase)
+            {
+                this.LinkAgainst<WindowsSDK.WindowsSDKV2>();
+            }
+        }
+    }
+
     class GlobalSettings : Bam.Core.IGlobalOptionCollectionOverride
     {
         #region IGlobalOptionCollectionOverride implementation
