4cdffb6dfe20d50df073655eb92997b9b4416ee9
Mark Final <markfinal@hotmail.com>
2015-08-09 15:07:10 +0100

    [V2] Using a Microsoft example for a throttled Task scheduler. Haven't plugged it into the system yet, as it was throwing some erroneous exceptions

--- Bam.Core/BuildManager.cs
+++ Bam.Core/BuildManager.cs
@@ -27,6 +27,128 @@ namespace V2
     /// </summary>
     public sealed class Executor
     {
+        // Provides a task scheduler that ensures a maximum concurrency level while
+        // running on top of the thread pool.
+        // https://msdn.microsoft.com/en-us/library/ee789351.aspx
+        public class LimitedConcurrencyLevelTaskScheduler : System.Threading.Tasks.TaskScheduler
+        {
+            // Indicates whether the current thread is processing work items.
+            [System.ThreadStatic]
+            private static bool _currentThreadIsProcessingItems;
+
+            // The list of tasks to be executed
+            private readonly System.Collections.Generic.LinkedList<System.Threading.Tasks.Task> _tasks = new System.Collections.Generic.LinkedList<System.Threading.Tasks.Task>(); // protected by lock(_tasks)
+
+            // The maximum concurrency level allowed by this scheduler.
+            private readonly int _maxDegreeOfParallelism;
+
+            // Indicates whether the scheduler is currently processing work items.
+            private int _delegatesQueuedOrRunning = 0;
+
+            // Creates a new instance with the specified degree of parallelism.
+            public LimitedConcurrencyLevelTaskScheduler(int maxDegreeOfParallelism)
+            {
+                if (maxDegreeOfParallelism < 1) throw new System.ArgumentOutOfRangeException("maxDegreeOfParallelism");
+                _maxDegreeOfParallelism = maxDegreeOfParallelism;
+            }
+
+            // Queues a task to the scheduler.
+            protected sealed override void QueueTask(System.Threading.Tasks.Task task)
+            {
+                // Add the task to the list of tasks to be processed.  If there aren't enough
+                // delegates currently queued or running to process tasks, schedule another.
+                lock (_tasks)
+                {
+                    _tasks.AddLast(task);
+                    if (_delegatesQueuedOrRunning < _maxDegreeOfParallelism)
+                    {
+                        ++_delegatesQueuedOrRunning;
+                        NotifyThreadPoolOfPendingWork();
+                    }
+                }
+            }
+
+            // Inform the ThreadPool that there's work to be executed for this scheduler.
+            private void NotifyThreadPoolOfPendingWork()
+            {
+                System.Threading.ThreadPool.UnsafeQueueUserWorkItem(_ =>
+                {
+                    // Note that the current thread is now processing work items.
+                    // This is necessary to enable inlining of tasks into this thread.
+                    _currentThreadIsProcessingItems = true;
+                    try
+                    {
+                        // Process all available items in the queue.
+                        while (true)
+                        {
+                            System.Threading.Tasks.Task item;
+                            lock (_tasks)
+                            {
+                                // When there are no more items to be processed,
+                                // note that we're done processing, and get out.
+                                if (_tasks.Count == 0)
+                                {
+                                    --_delegatesQueuedOrRunning;
+                                    break;
+                                }
+
+                                // Get the next item from the queue
+                                item = _tasks.First.Value;
+                                _tasks.RemoveFirst();
+                            }
+
+                            // Execute the task we pulled out of the queue
+                            base.TryExecuteTask(item);
+                        }
+                    }
+                    // We're done processing items on the current thread
+                    finally { _currentThreadIsProcessingItems = false; }
+                }, null);
+            }
+
+            // Attempts to execute the specified task on the current thread.
+            protected sealed override bool TryExecuteTaskInline(System.Threading.Tasks.Task task, bool taskWasPreviouslyQueued)
+            {
+                // If this thread isn't already processing a task, we don't support inlining
+                if (!_currentThreadIsProcessingItems) return false;
+
+                // If the task was previously queued, remove it from the queue
+                if (taskWasPreviouslyQueued)
+                    // Try to run the task.
+                if (TryDequeue(task))
+                    return base.TryExecuteTask(task);
+                else
+                    return false;
+                else
+                    return base.TryExecuteTask(task);
+            }
+
+            // Attempt to remove a previously scheduled task from the scheduler.
+            protected sealed override bool TryDequeue(System.Threading.Tasks.Task task)
+            {
+                lock (_tasks) return _tasks.Remove(task);
+            }
+
+            // Gets the maximum concurrency level supported by this scheduler.
+            public sealed override int MaximumConcurrencyLevel { get { return _maxDegreeOfParallelism; } }
+
+            // Gets an enumerable of the tasks currently scheduled on this scheduler.
+            protected sealed override System.Collections.Generic.IEnumerable<System.Threading.Tasks.Task> GetScheduledTasks()
+            {
+                bool lockTaken = false;
+                try
+                {
+                    System.Threading.Monitor.TryEnter(_tasks, ref lockTaken);
+                    if (lockTaken) return _tasks;
+                    else throw new System.NotSupportedException();
+                }
+                finally
+                {
+                    if (lockTaken) System.Threading.Monitor.Exit(_tasks);
+                }
+            }
+        }
+
         public void Run()
         {
             // TODO: should the rank collections be sorted, so that modules with fewest dependencies are first?
@@ -67,6 +189,23 @@ namespace V2
 
             if (threaded)
             {
+                var cancellationSource = new System.Threading.CancellationTokenSource();
+                var cancellationToken = cancellationSource.Token;
+
+                // LongRunning is absolutely necessary in order to achieve paralleism
+                var creationOpts = System.Threading.Tasks.TaskCreationOptions.LongRunning;
+                var continuationOpts = System.Threading.Tasks.TaskContinuationOptions.LongRunning;
+
+                //var scheduler = new LimitedConcurrencyLevelTaskScheduler(System.Environment.ProcessorCount);
+                //var scheduler = new LimitedConcurrencyLevelTaskScheduler(1);
+                var scheduler = System.Threading.Tasks.TaskScheduler.Default;
+
+                var factory = new System.Threading.Tasks.TaskFactory(
+                        cancellationToken,
+                        creationOpts,
+                        continuationOpts,
+                        scheduler);
+
                 var tasks = new Array<System.Threading.Tasks.Task>();
                 foreach (var rank in graph.Reverse())
                 {
@@ -81,7 +220,7 @@ namespace V2
                         Log.DebugMessage("Module {0} requires building", module.ToString());
                         var context = new ExecutionContext();
 
-                        var task = System.Threading.Tasks.Task.Factory.StartNew(() =>
+                        var task = factory.StartNew(() =>
                             {
                                 var depTasks = new Array<System.Threading.Tasks.Task>();
                                 foreach (var dep in module.Dependents)
@@ -109,8 +248,7 @@ namespace V2
                                 {
                                     Log.ErrorMessage(context.ErrorStringBuilder.ToString());
                                 }
-                            },
-                            System.Threading.Tasks.TaskCreationOptions.LongRunning); // LongRunning is absolutely necessary in order to achieve paralleism
+                            });
                         tasks.Add(task);
                         module.ExecutionTask = task;
                     }
