3a28e9ee5a6e7e1e2dc67e1dbf1cb27796984b2b
Mark Final <markfinal@hotmail.com>
2015-09-18 16:47:03 +0100

    [Bam] bam --version now reports the directory containing the Bam core assembly.

--- Bam/CommandLineArgumentHelper.cs
+++ Bam/CommandLineArgumentHelper.cs
@@ -110,7 +110,7 @@ namespace Bam
             Core.Log.Info("(c) Mark Final, 2010-2015");
             Core.Log.Info("Licensed under BSD 3-clause. See License file.");
             var clrVersion = System.Environment.Version;
-            Core.Log.Info("Using C# compiler v{0}.{1}", clrVersion.Major, clrVersion.Minor);
+            Core.Log.Info("Using C# compiler v{0}.{1} with assemblies in {2}", clrVersion.Major, clrVersion.Minor, Core.State.ExecutableDirectory);
         }
     }
 }
