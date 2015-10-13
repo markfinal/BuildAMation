4999e137b37e239883db1184e087c15b854019d6
Mark Final <markfinal@hotmail.com>
2015-10-13 17:03:09 +0100

    [packages,C] Plugins, since they load at runtime, do not need an SO name

--- packages/C/bam/Scripts/CxxPlugin.cs
+++ packages/C/bam/Scripts/CxxPlugin.cs
@@ -27,10 +27,21 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endregion // License
+using Bam.Core;
 namespace C.Cxx
 {
     public abstract class Plugin :
         DynamicLibrary
     {
+        protected override void
+        Init(
+            Bam.Core.Module parent)
+        {
+            base.Init(parent);
+            if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Linux))
+            {
+                this.Macros.Remove("SOName");
+            }
+        }
     }
 }
