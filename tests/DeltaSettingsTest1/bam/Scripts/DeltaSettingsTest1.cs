8f890999f1f85585f99dc04d55de3a63af1be4de
Mark Final <markfinal@hotmail.com>
2015-10-21 09:53:21 +0100

    [tests,DeltaSettingsTest1] Updated to test for both Value and Reference types in the Settings

--- tests/DeltaSettingsTest1/bam/Scripts/DeltaSettingsTest1.cs
+++ tests/DeltaSettingsTest1/bam/Scripts/DeltaSettingsTest1.cs
@@ -12,12 +12,18 @@ namespace DeltaSettingsTest1
             base.Init(parent);
 
             var source = this.CreateCSourceContainer("$(packagedir)/source/*.c");
+            source.PrivatePatch(settings =>
+                {
+                    var compiler = settings as C.ICommonCompilerSettings;
+                    compiler.PreprocessorDefines.Add("D_COMMON");
+                });
 
             source.Children.Where(item => (item as C.ObjectFile).InputPath.Parse().Contains("c.c")).ToList().ForEach(item =>
                 item.PrivatePatch(settings =>
                     {
                         var compiler = settings as C.ICommonCompilerSettings;
-                        compiler.DebugSymbols = false;
+                        compiler.TargetLanguage = C.ETargetLanguage.Cxx;
+                        compiler.PreprocessorDefines.Remove("D_COMMON");
                     }));
         }
     }
