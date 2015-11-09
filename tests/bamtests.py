e2842ce71a325fa664e42ba8429dd7f35a4c68f1
Mark Final <markfinal@hotmail.com>
2015-11-09 15:08:57 +0000

    [tests] Adding InstallerTest1 to the Linux tests, for Native and MakeFile build modes

--- tests/bamtests.py
+++ tests/bamtests.py
@@ -69,5 +69,6 @@ def ConfigureRepository():
                                               linux={"Native":[gcc64]},
                                               osx={"Native":[clang64],"Xcode":[clang64]})
     configs["InstallerTest1"] = TestSetup(win={"Native":[visualc64],"VSSolution":[visualc64]},
+                                          linux={"Native":[gcc64],"MakeFile":[gcc64]},
                                           osx={"Native":[clang64],"MakeFile":[clang64],"Xcode":[clang64]})
     return configs
