65078468a33e95cdf451b10066b2e808a08926d1
Mark Final <markfinal@hotmail.com>
2016-03-24 15:24:26 +0000

    [Core] Fixes #178. Added Bam.Core.ExecuteReasoning.EReason.DeferredEvaluation, and utility function Bam.Core.ExecuteReasoning.DeferredUntilBuild, which can be used to identify modules that must defer their evaluation until after the build starts, e.g. those modules that rely on part of the build to generate their data. It is up to the policy writers of the build mode to handle deferred evaluation.

--- Bam.Core/ExecuteReasoning.cs
+++ Bam.Core/ExecuteReasoning.cs
@@ -53,7 +53,12 @@ namespace Bam.Core
             /// <summary>
             /// A (re)build is required as the sources to the modules are newer than the output files.
             /// </summary>
-            InputFileIsNewer
+            InputFileIsNewer,
+
+            /// <summary>
+            /// Evaluation has been deferred because it cannot be evaluated before the build starts, e.g. console output of a built module is used as the contents of another file.
+            /// </summary>
+            DeferredEvaluation
         }
 
         private ExecuteReasoning(
@@ -102,6 +107,19 @@ namespace Bam.Core
         }
 
         /// <summary>
+        /// Utility method to create an instance of deferred-evaluation (re)build, of the specified output path.
+        /// </summary>
+        /// <returns>An instance of ExecuteReasoning.</returns>
+        /// <param name="outputPath">Output path whose state must be deferred until the build.</param>
+        /// <returns></returns>
+        public static ExecuteReasoning
+        DeferredUntilBuild(
+            TokenizedString outputPath)
+        {
+            return new ExecuteReasoning(EReason.DeferredEvaluation, outputPath);
+        }
+
+        /// <summary>
         /// Convert the reasoning to a meaningful description. Used by the --explain command line option.
         /// </summary>
         /// <returns>A description of the (re)build reason.</returns>
@@ -123,6 +141,12 @@ namespace Bam.Core
                             return "member(s) of the module collection were updated";
                         }
                         return System.String.Format("{0} is newer than {1}", this.InputFilePath.Parse(), this.OutputFilePath.Parse());
+                    }
+
+                case EReason.DeferredEvaluation:
+                    {
+                        return System.String.Format("there is insufficient information during evaluation to determine the state of {0}",
+                            this.OutputFilePath.Parse());
                     }
 
                 default:
