afb6a3cdbbf60ab302ab6febfa8596d7c993300b
Mark Final <markfinal@hotmail.com>
2016-04-05 11:56:01 +0100

    [codingtools] Fixes #202. Corrected Python syntax errors

--- codingtools/make_release.py
+++ codingtools/make_release.py
@@ -80,8 +80,8 @@ def update_version_numbers(options):
                       line.rstrip())
         print line
     doxyconfig_path = os.path.join(os.getcwd(), "docsrc", "BuildAMationDoxy")
-    for line in fileinput.input(common_assembly_info_path, inplace=1):  # , backup='.bk'):
-        if line.starts_with('PROJECT_NUMBER'):
+    for line in fileinput.input(doxyconfig_path, inplace=1):  # , backup='.bk'):
+        if line.startswith('PROJECT_NUMBER'):
             print "PROJECT_NUMBER = %s" % options.version
         else:
             print line
