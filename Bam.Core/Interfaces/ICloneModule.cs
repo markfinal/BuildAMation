789f0a5e76bd7366463b793dbc0bed45f91fa6a5
Mark Final <markfinal@hotmail.com>
2016-04-06 12:05:46 +0100

    [Core] Issue #197. Added Bam.Core.ICloneModule, which modules can implement if Bam.Core.Module.CloneWithPrivatePatches is insufficient. For example, if the module has properties that must be assigned in a particular order.

---
+++ Bam.Core/Interfaces/ICloneModule.cs
@@ -0,0 +1,50 @@
+#region License
+// Copyright (c) 2010-2016, Mark Final
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of BuildAMation nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#endregion // License
+namespace Bam.Core
+{
+    /// <summary>
+    /// Implement this interface if cloning a module requires custom setup scripts.
+    /// </summary>
+    public interface ICloneModule
+    {
+        /// <summary>
+        /// Implementing this function infers that Bam.Core.Module.CloneWithPrivatePatches is insufficient.
+        /// The minimum expected of this function is to invoke CloneWithPrivatePatches, and honour the parent and post clone delegate.
+        /// Anything after this is specific to the module.
+        /// </summary>
+        /// <param name="parent">Optional Module of which this clone is a child of.</param>
+        /// <param name="postInit">Optional callback, invoked after the call to CloneWithPrivatePatches has completed.</param>
+        /// <returns>The cloned module</returns>
+        Module
+        Clone(
+            Module parent = null,
+            Module.PostInitDelegate postInit = null);
+    }
+}
