672c94dadaeac7dec640083df3a7687e235fd33c
Mark Final <markfinal@hotmail.com>
2016-07-01 10:35:39 +0100

    [tests,CyclicDependenciesTest1] Added license and used coding style

--- tests/CyclicDependenciesTest1/bam/Scripts/CyclicDependenciesTest1.cs
+++ tests/CyclicDependenciesTest1/bam/Scripts/CyclicDependenciesTest1.cs
@@ -1,10 +1,41 @@
+#region License
+// Copyright (c) 2010-2016, Mark Final
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of BuildAMation nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#endregion // License
 using Bam.Core;
 namespace CyclicDependenciesTest1
 {
     class A :
         Bam.Core.Module
     {
-        protected override void Init(Module parent)
+        protected override void
+        Init(
+            Bam.Core.Module parent)
         {
             base.Init(parent);
 
@@ -12,15 +43,20 @@ namespace CyclicDependenciesTest1
             this.DependsOn(depB);
         }
 
-        public override void Evaluate()
+        public override void
+        Evaluate()
         {
         }
 
-        protected override void ExecuteInternal(ExecutionContext context)
+        protected override void
+        ExecuteInternal(
+            Bam.Core.ExecutionContext context)
         {
         }
 
-        protected override void GetExecutionPolicy(string mode)
+        protected override void
+        GetExecutionPolicy(
+            string mode)
         {
         }
     }
@@ -28,7 +64,9 @@ namespace CyclicDependenciesTest1
     class B :
         Bam.Core.Module
     {
-        protected override void Init(Module parent)
+        protected override void
+        Init(
+            Bam.Core.Module parent)
         {
             base.Init(parent);
 
@@ -36,15 +74,20 @@ namespace CyclicDependenciesTest1
             this.DependsOn(depA);
         }
 
-        public override void Evaluate()
+        public override void
+        Evaluate()
         {
         }
 
-        protected override void ExecuteInternal(ExecutionContext context)
+        protected override void
+        ExecuteInternal(
+            Bam.Core.ExecutionContext context)
         {
         }
 
-        protected override void GetExecutionPolicy(string mode)
+        protected override void
+        GetExecutionPolicy(
+            string mode)
         {
         }
     }
@@ -52,7 +95,9 @@ namespace CyclicDependenciesTest1
     sealed class C :
         Bam.Core.Module
     {
-        protected override void Init(Module parent)
+        protected override void
+        Init(
+            Bam.Core.Module parent)
         {
             base.Init(parent);
 
@@ -60,15 +105,20 @@ namespace CyclicDependenciesTest1
             this.DependsOn(depB);
         }
 
-        public override void Evaluate()
+        public override void
+        Evaluate()
         {
         }
 
-        protected override void ExecuteInternal(ExecutionContext context)
+        protected override void
+        ExecuteInternal(
+            Bam.Core.ExecutionContext context)
         {
         }
 
-        protected override void GetExecutionPolicy(string mode)
+        protected override void
+        GetExecutionPolicy(
+            string mode)
         {
         }
     }
