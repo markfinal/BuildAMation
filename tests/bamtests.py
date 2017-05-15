563be05727d18dd4118b6c6c6cf0f7c98cabd967
Mark Final <markfinal@hotmail.com>
2017-05-15 06:30:02 +0100

    [tests] Fixes #334. ObjectiveCTest1 is now tested on Linux in Native and MakeFile

--- tests/bamtests.py
+++ tests/bamtests.py
@@ -53,7 +53,8 @@ def configure_repository():
                                        linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
                                        osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
     configs["CocoaTest1"] = TestSetup(osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
-    configs["ObjectiveCTest1"] = TestSetup(osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+    configs["ObjectiveCTest1"] = TestSetup(linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                           osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
     configs["ProxyTest"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
                                      linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
                                      osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32]})
