2228a59554e1a73d022110fb6c3d675eb62bf902
Mark Final <markfinal@hotmail.com>
2017-05-15 07:43:16 +0100

    [tests] Fixes #334. ProceduralHeaderTest1 is now tested in MakeFile build mode

--- tests/bamtests.py
+++ tests/bamtests.py
@@ -89,8 +89,8 @@ def configure_repository():
                                            linux={"Native": [gcc64, gcc32]},
                                            osx={"Native": [clang64, clang32]})
     configs["ProceduralHeaderTest1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32]},
-                                                 linux={"Native": [gcc64, gcc32]},
-                                                 osx={"Native": [clang64, clang32]})
+                                                 linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                                 osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32})
     configs["EmbedStaticIntoDynamicLibrary"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32]},
                                                          linux={"Native": [gcc64, gcc32]},
                                                          osx={"Native": [clang64, clang32]})
