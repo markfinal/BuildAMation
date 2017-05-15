d9cb0791165905c6acbf685e94e885a2bb22aa63
Mark Final <markfinal@hotmail.com>
2017-05-15 08:39:14 +0100

    [tests] Fixes #334. PublishingTest1 and PublishingTest2 now tested with the MakeFile build mode

--- tests/bamtests.py
+++ tests/bamtests.py
@@ -83,11 +83,11 @@ def configure_repository():
                                                    linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
                                                    osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
     configs["PublishingTest1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32]},
-                                           linux={"Native": [gcc64, gcc32]},
-                                           osx={"Native": [clang64, clang32], "Xcode": [clang64, clang32]})
+                                           linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                           osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
     configs["PublishingTest2"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32]},
-                                           linux={"Native": [gcc64, gcc32]},
-                                           osx={"Native": [clang64, clang32]})
+                                           linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                           osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32]})
     configs["ProceduralHeaderTest1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32]},
                                                  linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
                                                  osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32]})
