12ee3a7b038e6fd044e9c3c04eb5c997c28e6d81
Mark Final <markfinal@hotmail.com>
2017-09-23 08:26:36 +0100

    [packages] Fixes #366. Added Clang-Xcode9 package for Xcode9 support.

---
+++ packages/Clang-Xcode9/bam/Scripts/CxxCompilerSettings.cs
@@ -0,0 +1,219 @@
+#region License
+// Copyright (c) 2010-2017, Mark Final
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of BuildAMation nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#endregion // License
+namespace Clang
+{
+    public sealed class CxxCompilerSettings :
+        C.SettingsBase,
+        CommandLineProcessor.IConvertToCommandLine,
+        XcodeProjectProcessor.IConvertToProject,
+        C.ICommonCompilerSettings,
+        C.ICxxOnlyCompilerSettings,
+        C.ICommonCompilerSettingsOSX,
+        C.IAdditionalSettings,
+        ClangCommon.ICommonCompilerSettings
+    {
+        public CxxCompilerSettings(
+            Bam.Core.Module module)
+            : this(module, true)
+        {
+            (this as C.ICommonCompilerSettings).TargetLanguage = C.ETargetLanguage.Cxx;
+        }
+
+        public CxxCompilerSettings(
+            Bam.Core.Module module,
+            bool useDefaults)
+        {
+            this.InitializeAllInterfaces(module, true, useDefaults);
+
+            // not in the defaults in the C package to avoid a compile-time dependency on the Clang package
+            (this as C.ICommonCompilerSettingsOSX).MinimumVersionSupported =
+                Bam.Core.Graph.Instance.PackageMetaData<Clang.MetaData>("Clang").MinimumVersionSupported;
+        }
+
+        void
+        CommandLineProcessor.IConvertToCommandLine.Convert(
+            Bam.Core.StringArray commandLine)
+        {
+            CommandLineProcessor.Conversion.Convert(typeof(ClangCommon.CommandLineCompilerImplementation), this, commandLine);
+        }
+
+        void
+        XcodeProjectProcessor.IConvertToProject.Convert(
+            Bam.Core.Module module,
+            XcodeBuilder.Configuration configuration)
+        {
+            XcodeProjectProcessor.Conversion.Convert(typeof(ClangCommon.XcodeCompilerImplementation), this, module, configuration);
+        }
+
+        C.EBit? C.ICommonCompilerSettings.Bits
+        {
+            get;
+            set;
+        }
+
+        C.PreprocessorDefinitions C.ICommonCompilerSettings.PreprocessorDefines
+        {
+            get;
+            set;
+        }
+
+        Bam.Core.TokenizedStringArray C.ICommonCompilerSettings.IncludePaths
+        {
+            get;
+            set;
+        }
+
+        Bam.Core.TokenizedStringArray C.ICommonCompilerSettings.SystemIncludePaths
+        {
+            get;
+            set;
+        }
+
+        C.ECompilerOutput? C.ICommonCompilerSettings.OutputType
+        {
+            get;
+            set;
+        }
+
+        bool? C.ICommonCompilerSettings.DebugSymbols
+        {
+            get;
+            set;
+        }
+
+        bool? C.ICommonCompilerSettings.WarningsAsErrors
+        {
+            get;
+            set;
+        }
+
+        C.EOptimization? C.ICommonCompilerSettings.Optimization
+        {
+            get;
+            set;
+        }
+
+        C.ETargetLanguage? C.ICommonCompilerSettings.TargetLanguage
+        {
+            get;
+            set;
+        }
+
+        bool? C.ICommonCompilerSettings.OmitFramePointer
+        {
+            get;
+            set;
+        }
+
+        Bam.Core.StringArray C.ICommonCompilerSettings.DisableWarnings
+        {
+            get;
+            set;
+        }
+
+        Bam.Core.StringArray C.ICommonCompilerSettings.PreprocessorUndefines
+        {
+            get;
+            set;
+        }
+
+        C.Cxx.EExceptionHandler? C.ICxxOnlyCompilerSettings.ExceptionHandler
+        {
+            get;
+            set;
+        }
+
+        bool? C.ICxxOnlyCompilerSettings.EnableRunTimeTypeInfo
+        {
+            get;
+            set;
+        }
+
+        C.Cxx.ELanguageStandard? C.ICxxOnlyCompilerSettings.LanguageStandard
+        {
+            get;
+            set;
+        }
+
+        C.Cxx.EStandardLibrary? C.ICxxOnlyCompilerSettings.StandardLibrary
+        {
+            get;
+            set;
+        }
+
+        Bam.Core.TokenizedStringArray C.ICommonCompilerSettingsOSX.FrameworkSearchPaths
+        {
+            get;
+            set;
+        }
+
+        string C.ICommonCompilerSettingsOSX.MinimumVersionSupported
+        {
+            get;
+            set;
+        }
+
+        Bam.Core.StringArray C.IAdditionalSettings.AdditionalSettings
+        {
+            get;
+            set;
+        }
+
+        bool? ClangCommon.ICommonCompilerSettings.AllWarnings
+        {
+            get;
+            set;
+        }
+
+        bool? ClangCommon.ICommonCompilerSettings.ExtraWarnings
+        {
+            get;
+            set;
+        }
+
+        bool? ClangCommon.ICommonCompilerSettings.Pedantic
+        {
+            get;
+            set;
+        }
+
+        ClangCommon.EVisibility? ClangCommon.ICommonCompilerSettings.Visibility
+        {
+            get;
+            set;
+        }
+
+        bool? ClangCommon.ICommonCompilerSettings.StrictAliasing
+        {
+            get;
+            set;
+        }
+    }
+}
