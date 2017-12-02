1a9d9f202b10c1bb20eaeb0cd3b2201ee44f91dd
Mark Final <markfinal@hotmail.com>
2017-12-02 21:05:42 +0000

    Add NUnit support (#390)
    Fixes #389. NUnit is added via NuGet packages. This is supported on VisualStudio, VisualStudio for Mac and MonoDevelop.
    Note that VS 2013 is a little backward (tries to modify the .sln), and VS for Mac at least seems to follow the VS 2017 support for NuGet.
    Added a Bam.Core.Test assembly, which uses NUnit markup.
    NuGet packages added for VS to explore and run NUnit tests.
    Also, used NuGet packages in order to run tests from the command line, which are integrated into the release process.

---
+++ codingtools/test_bam.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import os
+import platform
+import subprocess
+import sys
+
+def test_bam(build_dir, configuration='Release'):
+    current_dir = os.getcwd()
+    try:
+        os.chdir(build_dir)
+        print >>sys.stdout, "Starting tests in %s" % build_dir
+        sys.stdout.flush()
+        test_args = []
+        test_args.append('./NuGetPackages/NUnit.ConsoleRunner.3.7.0/tools/nunit3-console.exe')
+        test_args.append('--stoponerror')
+        test_args.append('--noresult')
+        test_args.append('Bam.Core.Test/bin/%s/Bam.Core.Test.dll' % configuration)
+        if platform.system() != "Windows":
+            test_args.insert(0, 'mono')
+        print >>sys.stdout, "Running command: %s" % ' '.join(test_args)
+        sys.stdout.flush()
+        subprocess.check_call(test_args)
+        print >>sys.stdout, "Finished tests"
+        sys.stdout.flush()
+    finally:
+        os.chdir(current_dir)
+
+
+if __name__ == "__main__":
+    try:
+        test_bam(os.getcwd())
+    except Exception, e:
+        print >>sys.stdout, "*** Test failure reason: %s" % str(e)
+        sys.stdout.flush()
+    print >>sys.stdout, "Done"
+    sys.stdout.flush()
