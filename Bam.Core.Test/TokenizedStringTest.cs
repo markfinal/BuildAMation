9c231a05a16f4938e29c7e2d2112dd1762cad9ba
Mark Final <markfinal@hotmail.com>
2017-12-03 09:15:48 +0000

    Adding more TokenizedString unit tests

--- Bam.Core.Test/TokenizedStringTest.cs
+++ Bam.Core.Test/TokenizedStringTest.cs
@@ -11,5 +11,22 @@ namespace Bam.Core.Test
             NUnit.Framework.Assert.That(() => inline.Parse(),
                 NUnit.Framework.Throws.Exception.TypeOf<Bam.Core.Exception>());
         }
+
+        [NUnit.Framework.Test]
+        public void
+        VerbatimStringIsAlreadyParsed()
+        {
+            var verbatim = Bam.Core.TokenizedString.CreateVerbatim("Hello World");
+            NUnit.Framework.Assert.IsTrue(verbatim.IsParsed);
+        }
+
+        [NUnit.Framework.Test]
+        public void
+        ParsingAParsedStringThrows()
+        {
+            var verbatim = Bam.Core.TokenizedString.CreateVerbatim("Hello World");
+            NUnit.Framework.Assert.That(() => verbatim.Parse(),
+                NUnit.Framework.Throws.Exception.TypeOf<Bam.Core.Exception>());
+        }
     }
 }
