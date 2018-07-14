8dcf7f2679478eb99b9c0da494cd94f48fb74903
Mark Final <markfinal@hotmail.com>
2018-07-14 13:04:59 +0100

    ExternalSourceGeneratorTest* now runnable for MakeFiles on Windows

--- tests/bamtests.py
+++ tests/bamtests.py
@@ -130,10 +130,10 @@ def configure_repository():
     configs["PublishingTest3"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32]},
                                            linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
                                            osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
-    configs["ExternalSourceGeneratorTest1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32]},
+    configs["ExternalSourceGeneratorTest1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32]},
                                                         linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
                                                         osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
-    configs["ExternalSourceGeneratorTest2"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32]},
+    configs["ExternalSourceGeneratorTest2"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32]},
                                                         linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
                                                         osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
     return configs
