f879151b11ccfb4c06ff580b641b4970c81c7f69
Mark Final <markfinal@hotmail.com>
2018-07-15 12:54:17 +0100

    Predicating out the conversion interfaces

--- packages/VisualStudioProcessor/bam/Scripts/IConvertToProject.cs
+++ packages/VisualStudioProcessor/bam/Scripts/IConvertToProject.cs
@@ -27,6 +27,8 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endregion // License
+#if BAM_V2
+#else
 namespace VisualStudioProcessor
 {
     public interface IConvertToProject
@@ -40,3 +42,4 @@ namespace VisualStudioProcessor
             string condition = null);
     }
 }
+#endif
