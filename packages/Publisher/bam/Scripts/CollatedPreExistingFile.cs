833e275b1daa68d6fa201ceb9b1c557ded02794c
Mark Final <markfinal@hotmail.com>
2018-07-28 15:33:43 +0100

    No longer require CollatedPreExistingFile

--- packages/Publisher/bam/Scripts/CollatedPreExistingFile.cs
+++ packages/Publisher/bam/Scripts/CollatedPreExistingFile.cs
@@ -27,6 +27,8 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endregion // License
+#if BAM_V2
+#else
 namespace Publisher
 {
     public class CollatedPreExistingFile :
@@ -55,3 +57,4 @@ namespace Publisher
         }
     }
 }
+#endif
