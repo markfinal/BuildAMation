91f4e7b013f73effcfe79bac5bd3264be79fc37e
Mark Final <markfinal@hotmail.com>
2018-08-01 19:57:33 +0100

    Updated assembler code

--- tests/AssemblerTest2/source/clang/helloworld32.s
+++ tests/AssemblerTest2/source/clang/helloworld32.s
@@ -1,11 +1,18 @@
 	.section	__TEXT,__text,regular,pure_instructions
 	.macosx_version_min 10, 6
-	.globl	_main
+	.globl	_main                   ## -- Begin function main
 	.p2align	4, 0x90
 _main:                                  ## @main
+	.cfi_startproc
 ## BB#0:
 	pushl	%ebp
+Lcfi0:
+	.cfi_def_cfa_offset 8
+Lcfi1:
+	.cfi_offset %ebp, -8
 	movl	%esp, %ebp
+Lcfi2:
+	.cfi_def_cfa_register %ebp
 	subl	$8, %esp
 	calll	L0$pb
 L0$pb:
@@ -13,11 +20,13 @@ L0$pb:
 	leal	L_str-L0$pb(%eax), %eax
 	movl	%eax, (%esp)
 	calll	_puts
+	calll	_CFunction
 	xorl	%eax, %eax
 	addl	$8, %esp
 	popl	%ebp
 	retl
-
+	.cfi_endproc
+                                        ## -- End function
 	.section	__TEXT,__cstring,cstring_literals
 	.p2align	4               ## @str
 L_str:
--- tests/AssemblerTest2/source/clang/helloworld64.s
+++ tests/AssemblerTest2/source/clang/helloworld64.s
@@ -1,25 +1,27 @@
 	.section	__TEXT,__text,regular,pure_instructions
 	.macosx_version_min 10, 6
-	.globl	_main
+	.globl	_main                   ## -- Begin function main
 	.p2align	4, 0x90
 _main:                                  ## @main
 	.cfi_startproc
 ## BB#0:
 	pushq	%rbp
-Ltmp0:
+Lcfi0:
 	.cfi_def_cfa_offset 16
-Ltmp1:
+Lcfi1:
 	.cfi_offset %rbp, -16
 	movq	%rsp, %rbp
-Ltmp2:
+Lcfi2:
 	.cfi_def_cfa_register %rbp
 	leaq	L_str(%rip), %rdi
 	callq	_puts
 	xorl	%eax, %eax
+	callq	_CFunction
+	xorl	%eax, %eax
 	popq	%rbp
 	retq
 	.cfi_endproc
-
+                                        ## -- End function
 	.section	__TEXT,__cstring,cstring_literals
 	.p2align	4               ## @str
 L_str:
