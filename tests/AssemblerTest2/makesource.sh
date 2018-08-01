e61f264aea117af815326a1c1fa57b861a35ddbe
Mark Final <markfinal@hotmail.com>
2018-08-01 19:57:12 +0100

    Fixing include path; clang sdk; and printing messages and stopping on the first error

--- tests/AssemblerTest2/makesource.sh
+++ tests/AssemblerTest2/makesource.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+set -x
+set -e
+
 sysinfo=$( uname )
 if [[ $sysinfo == Darwin ]]
 then
-xcrun --sdk macosx10.12 clang -mmacosx-version-min=10.6 -O2 -arch i386 -S -o source/clang/helloworld32.s -DBUILD32 reference/helloworld.c
-xcrun --sdk macosx10.12 clang -mmacosx-version-min=10.6 -O2 -arch x86_64 -S -o source/clang/helloworld64.s reference/helloworld.c
+sdkversion=$(xcrun --show-sdk-version)
+xcrun --sdk macosx${sdkversion} clang -mmacosx-version-min=10.6 -O2 -arch i386 -S -o source/clang/helloworld32.s -DBUILD32 -Isource reference/helloworld.c
+xcrun --sdk macosx${sdkversion} clang -mmacosx-version-min=10.6 -O2 -arch x86_64 -S -o source/clang/helloworld64.s -Isource reference/helloworld.c
 fi
 
 if [[ $sysinfo == Linux ]]
 then
-gcc -O2 -m32 -S -o source/gcc/helloworld32.S -DBUILD32 reference/helloworld.c
-gcc -O2 -m64 -S -o source/gcc/helloworld64.S reference/helloworld.c
+gcc -O2 -m32 -S -o source/gcc/helloworld32.S -DBUILD32 -Isource reference/helloworld.c
+gcc -O2 -m64 -S -o source/gcc/helloworld64.S -Isource reference/helloworld.c
 fi
 
 if [[ $sysinfo == 'MINGW32_NT-6.2' ]]
 then
 # requires Mingw to be pathed in
-gcc -O2 -m32 -S -o source/mingw/helloworld32.S -DBUILD32 reference/helloworld.c
+gcc -O2 -m32 -S -o source/mingw/helloworld32.S -DBUILD32 -Isource reference/helloworld.c
 fi
