9e18da123b59fead6f75fe1a34ffb37a8d3a962a
Mark Final <markfinal@hotmail.com>
2018-08-15 20:50:21 +0100

    Add file reference for metal shader source files

--- packages/XcodeBuilder/bam/Scripts/FileReference.cs
+++ packages/XcodeBuilder/bam/Scripts/FileReference.cs
@@ -88,6 +88,9 @@ namespace XcodeBuilder
             case FileReference.EFileType.ZipArchive:
                 return "archive.zip";
 
+            case FileReference.EFileType.MetalShaderSource:
+                return "sourcecode.metal";
+
             default:
                 throw new Bam.Core.Exception("Unrecognized file type {0}", type.ToString());
             }
@@ -147,7 +150,8 @@ namespace XcodeBuilder
             TextBasedDylibDefinition,
             TextFile,
             Assembler,
-            ZipArchive
+            ZipArchive,
+            MetalShaderSource
         }
 
         public enum ESourceTree
