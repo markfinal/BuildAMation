c6d5a9fa0a555e9202ab4ea532330eb2e4e710c2
Mark Final <markfinal@hotmail.com>
2018-08-30 07:03:17 +0100

    Simple test case for testing whether preprocessor defines gets passed through to dotnet publish

---
+++ BuildTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BuildTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !CUSTOM_DEFINE
+#error CUSTOM_DEFINE was not defined
+#endif
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
