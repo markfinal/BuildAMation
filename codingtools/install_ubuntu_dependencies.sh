c5d2626f90f268ddcc01020645e82537c51e8a97
Mark Final <markfinal@hotmail.com>
2018-10-10 09:52:24 +0100

    [codingtools|CI] Adding a bash script to install Ubuntu dependencies. Useful for setting up new machines as well as CI

---
+++ codingtools/install_ubuntu_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# GCC ObjectiveC
+# GNUStep
+# 32-bit GCC and G++
+# 32 and 64-bit X11 development files for Test12
+# chrpath for ChrpathTest1
+# doxygen for doc generation
+sudo apt-get install gobjc gnustep-devel gcc-multilib g++-multilib libx11-dev libx11-dev:i386 chrpath doxygen
