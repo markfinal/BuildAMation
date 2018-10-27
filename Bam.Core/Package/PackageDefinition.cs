144b27aeaae54f834e1e24eb9e603ade9d38859c
Mark Final <markfinal@hotmail.com>
2018-10-27 21:43:31 +0100

    Reminder to only do the source execution when in build mode

--- Bam.Core/Package/PackageDefinition.cs
+++ Bam.Core/Package/PackageDefinition.cs
@@ -489,6 +489,7 @@ namespace Bam.Core
             var packageDefinition = this.GetPackageDefinitionName();
             this.Definitions.AddUnique(packageDefinition);
 
+            // TODO: Only do this on a real build - not debug project generation
             this.Source?.Execute(this.Name);
         }
 
