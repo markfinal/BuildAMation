e01a0d9a62a31f12d7c404824fb399edaf8a8fee
Mark Final <markfinal@hotmail.com>
2018-11-14 20:27:03 +0000

    Don't need to tell everyone there are tasks outstanding

--- Bam.Core/BamState.cs
+++ Bam.Core/BamState.cs
@@ -247,7 +247,7 @@ namespace Bam.Core
             {
                 return;
             }
-            Log.Info($"Waiting on package source download tasks to finish before build starts...");
+            Log.Info($"Waiting on package source downloads to finish before build starts...");
             System.Threading.Tasks.Task.WaitAll(this.preBuildTasks.ToArray());
             Log.DebugMessage($"*** FINISHED WAITING ON TASKS");
         }
