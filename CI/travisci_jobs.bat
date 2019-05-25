5d8f3838891178fbaf0b5d5fb2af057fe16678f4
Mark Final <markfinal@hotmail.com>
2019-05-25 21:07:40 +0100

    [CI] Trying again with Travis CI

--- CI/travisci_jobs.bat
+++ CI/travisci_jobs.bat
@@ -1,6 +1,3 @@
-set MASTER_BRANCH_BODY='{^
- "request": {^
- "branch":"master"^
- }}'
+set MASTER_BRANCH_REQUEST="{^"request^": {^"branch^":^"master^"}}"
 
-curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token %TRAVIS_API_TOKEN%" -d "%MASTER_BRANCH_BODY%" https://api.travis-ci.com/repo/markfinal%2Fbam-imageformats/requests
+curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token %TRAVIS_API_TOKEN%" -d "%MASTER_BRANCH_REQUEST%" https://api.travis-ci.org/repo/markfinal%2Fbam-imageformats/requests
