0ee64738b7f28cc6c3a34ee76c30e03b1eca3712
Mark Final <markfinal@hotmail.com>
2019-05-26 08:39:35 +0100

    [CI] Script cleanup

--- CI/travisci_jobs.bat
+++ CI/travisci_jobs.bat
@@ -1,4 +1,2 @@
-REM set MASTER_BRANCH_REQUEST={""request"": {""branch"":""master""}}
-REM curl.exe -v -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token %TRAVIS_API_TOKEN%" -d "%MASTER_BRANCH_REQUEST%"  https://api.travis-ci.org/repo/markfinal%%2Fbam-imageformats/requests
 curl.exe -v -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token %TRAVIS_API_TOKEN%" -d "{\"request\": {\"branch\":\"master\"}}"  https://api.travis-ci.org/repo/markfinal%%2Fbam-imageformats/requests
 echo Finished launching TravisCI jobs
