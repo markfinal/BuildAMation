6797f872c5966fbc4c47906b828d026ad4a5bdb2
Mark Final <markfinal@hotmail.com>
2019-05-26 07:15:27 +0100

    [CI] Diagnostic: where is curl

--- CI/travisci_jobs.bat
+++ CI/travisci_jobs.bat
@@ -1,3 +1,3 @@
 set MASTER_BRANCH_REQUEST={""request"": {""branch"":""master""}}
-
+where curl
 curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token %TRAVIS_API_TOKEN%" -d "%MASTER_BRANCH_REQUEST%"  https://api.travis-ci.org/repo/markfinal%%2Fbam-imageformats/requests
