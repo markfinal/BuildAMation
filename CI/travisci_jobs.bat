a0606faba670724275d4c45e56fcf0abdbad6b24
Mark Final <markfinal@hotmail.com>
2019-05-26 08:27:11 +0100

    [CI] Verbose curl

--- CI/travisci_jobs.bat
+++ CI/travisci_jobs.bat
@@ -3,5 +3,5 @@ where curl
 echo Try curl
 curl.exe -V
 echo Running curl
-curl.exe -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token %TRAVIS_API_TOKEN%" -d "%MASTER_BRANCH_REQUEST%"  https://api.travis-ci.org/repo/markfinal%%2Fbam-imageformats/requests
+curl.exe -v -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token %TRAVIS_API_TOKEN%" -d "%MASTER_BRANCH_REQUEST%"  https://api.travis-ci.org/repo/markfinal%%2Fbam-imageformats/requests
 echo All done
