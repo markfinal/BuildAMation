345ba2199edee23cafb9e5404e52e83b3ba32329
Mark Final <markfinal@hotmail.com>
2019-06-11 08:13:55 +0100

    [CI] Removing sudo from inside scripts

--- CI/ubuntu_install_scripts/gcc6.sh
+++ CI/ubuntu_install_scripts/gcc6.sh
@@ -3,8 +3,8 @@
 ubuntu_version=`lsb_release -rs`
 echo Installing Gcc 6 dependencies for Ubuntu $ubuntu_version
 
-sudo apt-get -q update
-sudo apt-get -q -y --no-install-recommends install g++-6 g++-6-multilib g++-multilib
-sudo update-alternatives --remove-all gcc
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 10 --slave /usr/bin/g++ g++ /usr/bin/g++-6 --slave /usr/bin/gcov gcov /usr/bin/gcov-6 --slave /usr/bin/gcov-dump gcov-dump /usr/bin/gcov-dump-6 --slave /usr/bin/gcov-tool gcov-tool /usr/bin/gcov-tool-6 --slave /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-6 --slave /usr/bin/gcc-nm gcc-nm /usr/bin/gcc-nm-6 --slave /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-6
+apt-get -q update
+apt-get -q -y --no-install-recommends install g++-6 g++-6-multilib g++-multilib
+update-alternatives --remove-all gcc
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 10 --slave /usr/bin/g++ g++ /usr/bin/g++-6 --slave /usr/bin/gcov gcov /usr/bin/gcov-6 --slave /usr/bin/gcov-dump gcov-dump /usr/bin/gcov-dump-6 --slave /usr/bin/gcov-tool gcov-tool /usr/bin/gcov-tool-6 --slave /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-6 --slave /usr/bin/gcc-nm gcc-nm /usr/bin/gcc-nm-6 --slave /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-6
 sudo update-alternatives --set gcc /usr/bin/gcc-6
