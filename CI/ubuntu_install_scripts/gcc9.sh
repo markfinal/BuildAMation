46752165a16e118888548f2b68c57a9bfce5dcab
Mark Final <markfinal@hotmail.com>
2019-06-11 13:02:41 +0100

    [CI] Need compiler specific versions of objective C it seems

--- CI/ubuntu_install_scripts/gcc9.sh
+++ CI/ubuntu_install_scripts/gcc9.sh
@@ -7,7 +7,7 @@ echo Installing Gcc 9 dependencies for Ubuntu $ubuntu_version
 add-apt-repository ppa:ubuntu-toolchain-r/test > /dev/null
 
 apt-get -q update > /dev/null
-apt-get -q -y --no-install-recommends install g++-9 g++-9-multilib g++-multilib > /dev/null
+apt-get -q -y --no-install-recommends install g++-9 g++-9-multilib g++-multilib gobjc++-9 gobjc++-9-multilib > /dev/null
 update-alternatives --remove-all gcc
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 10 --slave /usr/bin/g++ g++ /usr/bin/g++-9 --slave /usr/bin/gcov gcov /usr/bin/gcov-9 --slave /usr/bin/gcov-dump gcov-dump /usr/bin/gcov-dump-9 --slave /usr/bin/gcov-tool gcov-tool /usr/bin/gcov-tool-9 --slave /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-9 --slave /usr/bin/gcc-nm gcc-nm /usr/bin/gcc-nm-9 --slave /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-9
 sudo update-alternatives --set gcc /usr/bin/gcc-9
