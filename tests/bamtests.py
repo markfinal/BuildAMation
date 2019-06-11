b72067a95e97457a2bf63194891cda22e817e8c8
Mark Final <markfinal@hotmail.com>
2019-06-11 21:34:33 +0100

    [tests] Automating C++14 and C++17 tests

--- tests/bamtests.py
+++ tests/bamtests.py
@@ -64,6 +64,12 @@ def configure_repository():
     configs["Cxx11Test1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
                                       linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
                                       osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+    configs["Cxx14Test1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                      linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                      osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+    configs["Cxx17Test1"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                      linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                      osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
     configs["PluginTest"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
                                       linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
                                       osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
