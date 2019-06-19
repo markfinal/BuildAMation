5f0cff60a00c55cb265a2c2369547c38e4cdc780
Mark Final <markfinal@hotmail.com>
2019-06-19 21:01:14 +0100

    [CI] Ensure that scripts are run with elevated permissions

--- CI/ubuntu_install_scripts/enable_gcc.sh
+++ CI/ubuntu_install_scripts/enable_gcc.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
 gcc_version=$1
 ubuntu_version=`lsb_release -rs`
 echo Enabling Gcc ${gcc_version} for Ubuntu $ubuntu_version
