4cfcadb2d994bdc818d7cbe549862577f696fac9
Mark Final <markfinal@hotmail.com>
2019-06-26 22:12:15 +0100

    [CI] Try to parameterise the job launching. Also test with bam-compress

--- CI/appveyor_jobs.ps1
+++ CI/appveyor_jobs.ps1
@@ -1,2 +1,8 @@
+function launch_job([String] $jobName) {
+    Start-AppveyorBuild -ApiKey $env:ACCOUNT_API_KEY -ProjectSlug $jobName -EnvironmentVariables @{ BAM_BRANCH = $env:APPVEYOR_REPO_BRANCH }
+}
+
+launch_job 'bam-compress'
+
 #Start-AppveyorBuild -ApiKey $env:ACCOUNT_API_KEY -ProjectSlug 'bam-imageformats' -EnvironmentVariables @{ BAM_BRANCH = $env:APPVEYOR_REPO_BRANCH }
 #Start-AppveyorBuild -ApiKey $env:ACCOUNT_API_KEY -ProjectSlug 'bam-python' -EnvironmentVariables @{ BAM_BRANCH = $env:APPVEYOR_REPO_BRANCH }
--- CI/travisci_jobs.bat
+++ CI/travisci_jobs.bat
@@ -1,4 +1,11 @@
-curl.exe -v -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token %TRAVIS_API_TOKEN%" -d "{\"request\": {\"branch\":\"master\", \"config\":{\"merge_mode\": \"deep_merge\", \"env\":{\"global\": {\"BAM_BRANCH\":\"%APPVEYOR_REPO_BRANCH%\"}}}}}"  https://api.travis-ci.org/repo/markfinal%%2FBuildAMation/requests
+call :launch_job master https://api.travis-ci.org/repo/markfinal%%2FBuildAMation/requests
+call :launch_job master https://api.travis-ci.org/repo/markfinal%%2Fbam-compress/requests
+
 rem curl.exe -v -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token %TRAVIS_API_TOKEN%" -d "{\"request\": {\"branch\":\"master\"}}"  https://api.travis-ci.org/repo/markfinal%%2Fbam-imageformats/requests
 rem curl.exe -v -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token %TRAVIS_API_TOKEN%" -d "{\"request\": {\"branch\":\"master\"}}"  https://api.travis-ci.org/repo/markfinal%%2Fbam-python/requests
 echo Finished launching TravisCI jobs
+goto :eof
+
+:launch_job
+curl.exe -v -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token %TRAVIS_API_TOKEN%" -d "{\"request\": {\"branch\":\"%1\", \"config\":{\"merge_mode\": \"deep_merge\", \"env\":{\"global\": {\"BAM_BRANCH\":\"%APPVEYOR_REPO_BRANCH%\"}}}}}" %2
+goto :eof
