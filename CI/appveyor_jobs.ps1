dfe442852829474111300a08f36eeb128415594c
Mark Final <markfinal@hotmail.com>
2019-06-26 19:44:01 +0100

    [CI] Testing out determining the branch of BAM that is remotely starting test jobs

--- CI/appveyor_jobs.ps1
+++ CI/appveyor_jobs.ps1
@@ -1,2 +1,2 @@
-Start-AppveyorBuild -ApiKey $env:ACCOUNT_API_KEY -ProjectSlug 'bam-imageformats' -EnvironmentVariables @{ BAM_branch = $env:APPVEYOR_REPO_BRANCH }
-Start-AppveyorBuild -ApiKey $env:ACCOUNT_API_KEY -ProjectSlug 'bam-python' -EnvironmentVariables @{ BAM_branch = $env:APPVEYOR_REPO_BRANCH }
+#Start-AppveyorBuild -ApiKey $env:ACCOUNT_API_KEY -ProjectSlug 'bam-imageformats' -EnvironmentVariables @{ BAM_branch = $env:APPVEYOR_REPO_BRANCH }
+#Start-AppveyorBuild -ApiKey $env:ACCOUNT_API_KEY -ProjectSlug 'bam-python' -EnvironmentVariables @{ BAM_branch = $env:APPVEYOR_REPO_BRANCH }
