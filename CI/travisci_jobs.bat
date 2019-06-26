367b28f3ff7033fcdcd623a6935017889e04e930
Mark Final <markfinal@hotmail.com>
2019-06-26 22:18:43 +0100

    [CI] Diagnostics

--- CI/travisci_jobs.bat
+++ CI/travisci_jobs.bat
@@ -7,5 +7,9 @@ echo Finished launching TravisCI jobs
 goto :eof
 
 :launch_job
+echo Launching job:
+echo  Branch name '%1'
+echo  Repo        '%2'
+echo curl.exe -v -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token %TRAVIS_API_TOKEN%" -d "{\"request\": {\"branch\":\"%1\", \"config\":{\"merge_mode\": \"deep_merge\", \"env\":{\"global\": {\"BAM_BRANCH\":\"%APPVEYOR_REPO_BRANCH%\"}}}}}" %2
 curl.exe -v -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token %TRAVIS_API_TOKEN%" -d "{\"request\": {\"branch\":\"%1\", \"config\":{\"merge_mode\": \"deep_merge\", \"env\":{\"global\": {\"BAM_BRANCH\":\"%APPVEYOR_REPO_BRANCH%\"}}}}}" %2
 goto :eof
