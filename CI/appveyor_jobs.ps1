91010f10e949aafcb792198defdaa97c92794c4d
Mark Final <markfinal@hotmail.com>
2019-06-27 06:22:01 +0100

    [CI] Removing the function in the batch file, as I think the special URL syntax was getting confused with it

--- CI/appveyor_jobs.ps1
+++ CI/appveyor_jobs.ps1
@@ -1,8 +1,9 @@
-function launch_job([String] $jobName) {
-    Start-AppveyorBuild -ApiKey $env:ACCOUNT_API_KEY -ProjectSlug $jobName -EnvironmentVariables @{ BAM_BRANCH = $env:APPVEYOR_REPO_BRANCH }
+function launch_project([String] $projectName) {
+    "Launching AppVeyor project: $projectName"
+    Start-AppveyorBuild -ApiKey $env:ACCOUNT_API_KEY -ProjectSlug $projectName -EnvironmentVariables @{ BAM_BRANCH = $env:APPVEYOR_REPO_BRANCH }
 }
 
-launch_job 'bam-compress'
+launch_project 'bam-compress'
 
 #Start-AppveyorBuild -ApiKey $env:ACCOUNT_API_KEY -ProjectSlug 'bam-imageformats' -EnvironmentVariables @{ BAM_BRANCH = $env:APPVEYOR_REPO_BRANCH }
 #Start-AppveyorBuild -ApiKey $env:ACCOUNT_API_KEY -ProjectSlug 'bam-python' -EnvironmentVariables @{ BAM_BRANCH = $env:APPVEYOR_REPO_BRANCH }
