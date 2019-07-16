f96d54f8c05b2070070cc904ec1804b2e451452a
Mark Final <markfinal@hotmail.com>
2019-07-16 22:20:49 +0100

    [packages,WindowsSDKCommon] Fixes #580. Added documentation

--- packages/WindowsSDKCommon/bam/Scripts/WinResourceCompilerSettings/Defaults/WindowsSDK/ICommonWinResourceCompilerSettings.cs
+++ packages/WindowsSDKCommon/bam/Scripts/WinResourceCompilerSettings/Defaults/WindowsSDK/ICommonWinResourceCompilerSettings.cs
@@ -29,8 +29,16 @@
 #endregion // License
 namespace WindowsSDKCommon.DefaultSettings
 {
+    /// <summary>
+    /// Extension class for default settings
+    /// </summary>
     static partial class DefaultSettingsExtensions
     {
+        /// <summary>
+        /// Set the defaults for ICommonWinResourceCompilerSettings
+        /// </summary>
+        /// <param name="settings">Settings instance</param>
+        /// <param name="module">Module associated with Settings</param>
         public static void
         Defaults(
             this ICommonWinResourceCompilerSettings settings,
